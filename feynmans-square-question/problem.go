a6acfcb487d91af8dc46d0af3ad0a45065c0f924
t-nielzon <t-nielzon@users.noreply.github.com>
2026-02-09 11:11:48 +0800

    Add go solution: feynmans-square-question

---
+++ feynmans-square-question/problem.go
@@ -0,0 +1,24 @@
+/*
+Feynman's squares
+
+Richard Phillips Feynman was a well-known American physicist and a recipient
+of the Nobel Prize in Physics. He worked in theoretical physics and pioneered
+the field of quantum computing.
+
+How many different squares are there in a grid of NxN squares?
+
+For example, when N=2, the answer is 5: the 2x2 square itself, plus the four
+1x1 squares in its corners.
+
+Complete the function that solves Feynman's question in general. The input to
+your function will always be a positive integer.
+
+Examples:
+  1  -->   1
+  2  -->   5
+  3  -->  14
+*/
+package kata
+
+func Squares(n int) int {
+}
---
+++ feynmans-square-question/solution.go
@@ -0,0 +1,6 @@
+package kata
+
+// sum of squares 1² + 2² + ... + n² = n(n+1)(2n+1)/6
+func Squares(n int) int {
+	return n * (n + 1) * (2*n + 1) / 6
+}
