aaa317d310fad16ab47d0566f35ce0f02a6f7454
t-nielzon <t-nielzon@users.noreply.github.com>
2026-02-10 12:24:28 +0800

    Add javascript solution: string-transformations

--- string-transformations/solution.js
+++ string-transformations/solution.js
@@ -1,22 +1,22 @@
-function stringTransformation(s, pattern) {
+function stringTransformation(s, transformations) {
   let result = s;
-  
-  for (const op of pattern) {
+  for (const op of transformations) {
     switch (op) {
-      case 'U':
+      case "U":
         result = result.toUpperCase();
         break;
-      case 'L':
+      case "L":
         result = result.toLowerCase();
         break;
-      case 'R':
-        result = result.split('').reverse().join('');
+      case "R":
+        result = result.split("").reverse().join("");
         break;
-      case 'D':
-        result = result.split('').map(c => c + c).join('');
+      case "D":
+        result = result.replace(/./g, "$&$&");
         break;
     }
   }
-  
   return result;
 }
+
+module.exports = { stringTransformation };
