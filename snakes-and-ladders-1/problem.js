2c6d90d37e516ca24eca019cd3c8adde817082a8
t-nielzon <t-nielzon@users.noreply.github.com>
2026-02-11 16:11:11 +0800

    Add javascript solution: snakes-and-ladders-1

---
+++ snakes-and-ladders-1/problem.js
@@ -0,0 +1,28 @@
+/*
+ * Snakes and Ladders
+ * 5 kyu
+ *
+ * Your task is to create a simple class called SnakesLadders.
+ * The test cases will call the method play(die1, die2) independently
+ * of the state of the game or the player turn.
+ *
+ * Rules:
+ * 1. Two players start on square 0.
+ * 2. Player 1 starts and alternates with player 2.
+ * 3. Follow numbers 1-100.
+ * 4. If both dice are the same, that player gets another turn.
+ * 5. Land on bottom of ladder -> move to top.
+ * 6. Land on top of snake -> slide to bottom.
+ * 7. Must land exactly on 100 to win; excess moves bounce back.
+ * 8. If player rolls double and lands on 100, they win (no extra turn).
+ *
+ * Returns:
+ * - "Player n Wins!" if player n lands on 100
+ * - "Game over!" if a move is attempted after someone has won
+ * - "Player n is on square x" otherwise
+ */
+
+class SnakesLadders {
+  play(die1, die2) {
+  }
+}
