1671a5b870be6ff293a6c35b95099e89e9cc1959
t-nielzon <t-nielzon@users.noreply.github.com>
2026-02-12 09:37:14 +0800

    Add go solution: calculate-the-area-of-a-regular-n-sides-polygon-inside-a-circle-of-radius-r

--- calculate-the-area-of-a-regular-n-sides-polygon-inside-a-circle-of-radius-r/problem.go
+++ calculate-the-area-of-a-regular-n-sides-polygon-inside-a-circle-of-radius-r/problem.go
@@ -7,14 +7,15 @@ func AreaOfPolygonInsideCircle(circleRadius float64, numberOfSides int) float64
 
 It should calculate the area of a regular polygon of numberOfSides sides
 inside a circle of radius circleRadius which passes through all the vertices
-of the polygon (circumscribed circle or circumcircle).
+of the polygon (such circle is called circumscribed circle or circumcircle).
 
 The answer should be a number rounded to 3 decimal places.
 
-Examples:
-  AreaOfPolygonInsideCircle(3, 3)   // returns 11.691
-  AreaOfPolygonInsideCircle(5.8, 7) // returns 92.053
-  AreaOfPolygonInsideCircle(4, 5)   // returns 38.042
+Input :: Output Examples
+
+AreaOfPolygonInsideCircle(3, 3)   // returns 11.691
+AreaOfPolygonInsideCircle(5.8, 7) // returns 92.053
+AreaOfPolygonInsideCircle(4, 5)   // returns 38.042
 */
 package kata
 
