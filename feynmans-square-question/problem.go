66a3f9733ab8020b2c108dbb616a2a4d8089921b
t-nielzon <t-nielzon@users.noreply.github.com>
2026-02-16 09:57:09 +0800

    Add go solution: feynmans-square-question

--- feynmans-square-question/problem.go
+++ feynmans-square-question/problem.go
@@ -5,13 +5,16 @@ Richard Phillips Feynman was a well-known American physicist and a recipient
 of the Nobel Prize in Physics. He worked in theoretical physics and pioneered
 the field of quantum computing.
 
-How many different squares are there in a grid of NxN squares?
+Recently, an old farmer found some papers and notes that are believed to have
+belonged to Feynman. Among notes about mesons and electromagnetism, there was
+a napkin where he wrote a simple puzzle: "how many different squares are there
+in a grid of NxN squares?".
 
 For example, when N=2, the answer is 5: the 2x2 square itself, plus the four
 1x1 squares in its corners.
 
-Complete the function that solves Feynman's question in general. The input to
-your function will always be a positive integer.
+Task: Complete the function that solves Feynman's question in general.
+The input to your function will always be a positive integer.
 
 Examples:
   1  -->   1
