59e878151ab27455a22364889298682d4cb8c4fa
t-nielzon <t-nielzon@users.noreply.github.com>
2026-02-21 08:37:44 +0800

    Add go solution: integers-recreation-one

--- integers-recreation-one/solution.go
+++ integers-recreation-one/solution.go
@@ -3,39 +3,27 @@ package kata
 import "math"
 
 func ListSquared(m, n int) [][]int {
-	result := [][]int{}
-
-	for num := m; num <= n; num++ {
-		sumSquares := sumOfSquaredDivisors(num)
-		if isPerfectSquare(sumSquares) {
-			result = append(result, []int{num, sumSquares})
+	var result [][]int
+	for i := m; i <= n; i++ {
+		sum := sumOfSquaredDivisors(i)
+		root := int(math.Sqrt(float64(sum)))
+		if root*root == sum {
+			result = append(result, []int{i, sum})
 		}
 	}
-
 	return result
 }
 
 func sumOfSquaredDivisors(n int) int {
 	sum := 0
-	sqrt := int(math.Sqrt(float64(n)))
-
-	for i := 1; i <= sqrt; i++ {
+	for i := 1; i*i <= n; i++ {
 		if n%i == 0 {
 			sum += i * i
-			other := n / i
-			if other != i {
-				sum += other * other
+			j := n / i
+			if j != i {
+				sum += j * j
 			}
 		}
 	}
-
 	return sum
-}
-
-func isPerfectSquare(n int) bool {
-	if n < 0 {
-		return false
-	}
-	sqrt := int(math.Sqrt(float64(n)))
-	return sqrt*sqrt == n
 }
