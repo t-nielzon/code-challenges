b1235a6a5e50c58b31f9723bda309b290c400793
t-nielzon <t-nielzon@users.noreply.github.com>
2026-03-08 10:56:48 +0800

    Add go solution: cat-kata-part-1

--- cat-kata-part-1/solution.go
+++ cat-kata-part-1/solution.go
@@ -2,14 +2,14 @@ package kata
 
 import "math"
 
-func PeaceableKingdom(yard []string, minDistance int) bool {
-	type point struct{ r, c int }
-	var cats []point
+func Peaceful(yard []string, minDistance int) bool {
+	type pos struct{ r, c int }
+	var cats []pos
 
 	for r, row := range yard {
 		for c, ch := range row {
 			if ch == 'L' || ch == 'M' || ch == 'R' {
-				cats = append(cats, point{r, c})
+				cats = append(cats, pos{r, c})
 			}
 		}
 	}
@@ -18,16 +18,15 @@ func PeaceableKingdom(yard []string, minDistance int) bool {
 		return true
 	}
 
+	md := float64(minDistance)
 	for i := 0; i < len(cats); i++ {
 		for j := i + 1; j < len(cats); j++ {
 			dr := float64(cats[i].r - cats[j].r)
 			dc := float64(cats[i].c - cats[j].c)
-			dist := math.Sqrt(dr*dr + dc*dc)
-			if dist < float64(minDistance) {
+			if math.Sqrt(dr*dr+dc*dc) < md {
 				return false
 			}
 		}
 	}
-
 	return true
 }
