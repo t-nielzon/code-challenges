de8f8ff5cfdd036c4565cea292ec98a69a2f1561
t-nielzon <t-nielzon@users.noreply.github.com>
2026-03-12 21:59:13 +0800

    Add go solution: find-the-unique-number-1

--- find-the-unique-number-1/solution.go
+++ find-the-unique-number-1/solution.go
@@ -1,19 +1,16 @@
 package kata
 
 func FindUniq(arr []float32) float32 {
-	// compare first three to determine which value is the common one
-	if arr[0] == arr[1] {
-		for _, v := range arr[2:] {
-			if v != arr[0] {
-				return v
-			}
-		}
-	} else {
-		// arr[0] != arr[1], so one of them is unique; arr[2] breaks the tie
+	if arr[0] != arr[1] {
 		if arr[0] == arr[2] {
 			return arr[1]
 		}
 		return arr[0]
+	}
+	for i := 2; i < len(arr); i++ {
+		if arr[i] != arr[0] {
+			return arr[i]
+		}
 	}
 	return arr[0]
 }
