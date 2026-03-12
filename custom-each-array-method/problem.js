5527894c5b5bffb4aaeb9dc237e5769bb575ce3a
t-nielzon <t-nielzon@users.noreply.github.com>
2026-03-12 15:58:59 +0800

    Add javascript solution: custom-each-array-method

---
+++ custom-each-array-method/problem.js
@@ -0,0 +1,13 @@
+/*
+ * Custom each() Array method
+ *
+ * JavaScript provides an Array.prototype.forEach method that allows you to
+ * iterate over array values. For this exercise you will create your own array
+ * method called 'each'. It will be similar to the forEach method, except for
+ * one difference. If the callback function returns true then the loop will
+ * stop and no additional values will be iterated.
+ */
+
+Array.prototype.each = function(callback) {
+
+};
---
+++ custom-each-array-method/solution.js
@@ -0,0 +1,7 @@
+Array.prototype.each = function(callback) {
+  for (let i = 0; i < this.length; i++) {
+    if (callback(this[i], i) === true) {
+      break;
+    }
+  }
+};
