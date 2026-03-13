79f9df8d79c5d6c2a277834592d8edb3ca788c6a
t-nielzon <t-nielzon@users.noreply.github.com>
2026-03-14 01:43:44 +0800

    Add javascript solution: land-perimeter

--- land-perimeter/problem.js
+++ land-perimeter/problem.js
@@ -1,27 +1,28 @@
 /*
  * Land perimeter
- * 
+ *
  * Given an array arr of strings, complete the function by calculating the total
  * perimeter of all the islands. Each piece of land will be marked with 'X' while
  * the water fields are represented as 'O'. Consider each tile being a perfect
  * 1 x 1 piece of land.
- * 
+ *
  * Example:
- * ['XOOXO',
- *  'XOOXO',
- *  'OOOXO',
- *  'XXOXO',
- *  'OXOOO']
- * should return: "Total land perimeter: 24"
- * 
- * ['XOOO',
- *  'XOXO',
- *  'XOXO',
- *  'OOXX',
- *  'OOOO']
- * should return: "Total land perimeter: 18"
+ *   ['XOOXO',
+ *    'XOOXO',
+ *    'OOOXO',
+ *    'XXOXO',
+ *    'OXOOO']
+ *   => "Total land perimeter: 24"
+ *
+ *   ['XOOO',
+ *    'XOXO',
+ *    'XOXO',
+ *    'OOXX',
+ *    'OOOO']
+ *   => "Total land perimeter: 18"
  */
 
 function landPerimeter(arr) {
-
 }
+
+module.exports = { landPerimeter };
--- land-perimeter/solution.js
+++ land-perimeter/solution.js
@@ -1,36 +1,16 @@
 function landPerimeter(arr) {
   let perimeter = 0;
-  
-  for (let row = 0; row < arr.length; row++) {
-    for (let col = 0; col < arr[row].length; col++) {
-      if (arr[row][col] === 'X') {
-        // Each land tile starts with 4 sides
-        let sides = 4;
-        
-        // Check top neighbor
-        if (row > 0 && arr[row - 1][col] === 'X') {
-          sides--;
-        }
-        
-        // Check bottom neighbor
-        if (row < arr.length - 1 && arr[row + 1][col] === 'X') {
-          sides--;
-        }
-        
-        // Check left neighbor
-        if (col > 0 && arr[row][col - 1] === 'X') {
-          sides--;
-        }
-        
-        // Check right neighbor
-        if (col < arr[row].length - 1 && arr[row][col + 1] === 'X') {
-          sides--;
-        }
-        
-        perimeter += sides;
+  for (let r = 0; r < arr.length; r++) {
+    for (let c = 0; c < arr[r].length; c++) {
+      if (arr[r][c] === 'X') {
+        // each land tile starts with 4 edges; subtract shared edges with neighbors
+        perimeter += 4;
+        if (r > 0 && arr[r - 1][c] === 'X') perimeter -= 2;
+        if (c > 0 && arr[r][c - 1] === 'X') perimeter -= 2;
       }
     }
   }
-  
   return `Total land perimeter: ${perimeter}`;
 }
+
+module.exports = { landPerimeter };
