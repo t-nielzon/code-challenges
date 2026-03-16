639c6ac7d24bbb7394f51100aec7fca70f48def8
t-nielzon <t-nielzon@users.noreply.github.com>
2026-03-16 16:06:09 +0800

    Add go solution: palindrome-integer-composition

--- palindrome-integer-composition/problem.go
+++ palindrome-integer-composition/problem.go
@@ -1,17 +1,17 @@
 /*
-The palindromic number 595 is interesting because it can be written as the sum
-of consecutive squares: 6^2 + 7^2 + 8^2 + 9^2 + 10^2 + 11^2 + 12^2 = 595.
+The palindromic number 595 is interesting because it can be written as the
+sum of consecutive squares: 6^2 + 7^2 + 8^2 + 9^2 + 10^2 + 11^2 + 12^2 = 595.
 
-There are exactly eleven palindromes below one-thousand that can be written as
-consecutive square sums. Note that 1 = 0^2 + 1^2 has not been included as this
-problem is concerned with the squares of positive integers.
+There are exactly eleven palindromes below one-thousand that can be written
+as consecutive square sums. Note that 1 = 0^2 + 1^2 has not been included
+as this problem is concerned with the squares of positive integers.
 
-Given an input n, find the count of all the numbers less than n that are both
-palindromic and can be written as the sum of consecutive squares.
+Given an input n, find the count of all the numbers less than n that are
+both palindromic and can be written as the sum of consecutive squares.
 
-For instance: values(1000) = 11. See test examples for more cases.
+For instance: values(1000) = 11.
 
-This Kata is borrowed from Project Euler #125
+This Kata is borrowed from Project Euler #125.
 */
 package kata
 
--- palindrome-integer-composition/solution.go
+++ palindrome-integer-composition/solution.go
@@ -1,34 +1,29 @@
 package kata
 
-func Values(n int) int {
-	palindromes := make(map[int]bool)
+import "strconv"
 
-	// Find all sums of consecutive squares less than n
-	// Start from each positive integer i
+func Values(n int) int {
+	seen := make(map[int]bool)
+
 	for i := 1; i*i < n; i++ {
 		sum := 0
-		// Add consecutive squares starting from i
 		for j := i; sum < n; j++ {
 			sum += j * j
-			// We need at least 2 consecutive squares (j > i means we have i and at least one more)
 			if j > i && sum < n && isPalindrome(sum) {
-				palindromes[sum] = true
+				seen[sum] = true
 			}
 		}
 	}
 
-	return len(palindromes)
+	return len(seen)
 }
 
 func isPalindrome(n int) bool {
-	if n < 0 {
-		return false
+	s := strconv.Itoa(n)
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		if s[i] != s[j] {
+			return false
+		}
 	}
-	original := n
-	reversed := 0
-	for n > 0 {
-		reversed = reversed*10 + n%10
-		n /= 10
-	}
-	return original == reversed
+	return true
 }
