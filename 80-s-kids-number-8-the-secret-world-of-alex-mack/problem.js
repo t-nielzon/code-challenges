638b6fb364d1b18124a442ecce7145b7a83260d8
t-nielzon <t-nielzon@users.noreply.github.com>
2026-03-17 09:31:55 +0800

    Add javascript solution: 80-s-kids-number-8-the-secret-world-of-alex-mack

--- 80-s-kids-number-8-the-secret-world-of-alex-mack/problem.js
+++ 80-s-kids-number-8-the-secret-world-of-alex-mack/problem.js
@@ -2,13 +2,11 @@
  * 80's Kids #8: The Secret World of Alex Mack
  *
  * Top executives at Paradise Valley Chemical Plant are close to finding
- * the superhero's true identity. They know that someone who works at
- * the plant is related to her (Shhh, it's her dad!). Danielle Atron
- * has hired you to obtain the badge number of this person.
+ * the superhero's true identity. They know that someone who works at the
+ * plant is related to her. Danielle Atron has hired you to obtain the
+ * badge number of this person.
  *
  * Can you find the Secret badge number of Alex Mack's dad?
- *
- * Note: This is a puzzle. The initial solution is intentionally vague.
  */
 
 function findSecret(employees) {
--- 80-s-kids-number-8-the-secret-world-of-alex-mack/solution.js
+++ 80-s-kids-number-8-the-secret-world-of-alex-mack/solution.js
@@ -1,3 +1,7 @@
+/*
+ * Alex Mack's father in the show is George Mack.
+ * Find his employee record and return his badge number.
+ */
 function findSecret(employees) {
-  return employees.find(e => e.lastName === 'Mack').badgeNumber;
+  return employees.find(e => e.lastName === 'Mack' && e.firstName === 'George').badgeNumber;
 }
