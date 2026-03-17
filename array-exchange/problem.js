923e6fcb57c5b2e0239d78ba123697c983439702
t-nielzon <t-nielzon@users.noreply.github.com>
2026-03-17 09:32:11 +0800

    Add javascript solution: array-exchange

--- array-exchange/problem.js
+++ array-exchange/problem.js
@@ -17,4 +17,5 @@
  */
 
 function exchangeArrays(a, b) {
+
 }
--- array-exchange/solution.js
+++ array-exchange/solution.js
@@ -1,8 +1,10 @@
 function exchangeArrays(a, b) {
   const aCopy = a.slice().reverse();
   const bCopy = b.slice().reverse();
+
   a.length = 0;
   b.length = 0;
+
   a.push(...bCopy);
   b.push(...aCopy);
 }
