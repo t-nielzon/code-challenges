338b6cb3d333173ba373807a39c0734a065fccbf
t-nielzon <t-nielzon@users.noreply.github.com>
2026-03-22 14:54:21 +0800

    Add go solution: roman-numerals-decoder

--- roman-numerals-decoder/solution.go
+++ roman-numerals-decoder/solution.go
@@ -2,23 +2,16 @@ package kata
 
 func Decode(roman string) int {
 	values := map[byte]int{
-		'I': 1,
-		'V': 5,
-		'X': 10,
-		'L': 50,
-		'C': 100,
-		'D': 500,
-		'M': 1000,
+		'I': 1, 'V': 5, 'X': 10, 'L': 50,
+		'C': 100, 'D': 500, 'M': 1000,
 	}
-
-	total := 0
+	result := 0
 	for i := 0; i < len(roman); i++ {
-		curr := values[roman[i]]
-		if i+1 < len(roman) && curr < values[roman[i+1]] {
-			total -= curr
+		if i+1 < len(roman) && values[roman[i]] < values[roman[i+1]] {
+			result -= values[roman[i]]
 		} else {
-			total += curr
+			result += values[roman[i]]
 		}
 	}
-	return total
+	return result
 }
