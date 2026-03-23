ea10e80270cdd5aa2a1edc551123ffef2e5ab405
t-nielzon <t-nielzon@users.noreply.github.com>
2026-03-23 09:00:56 +0800

    Add go solution: split-strings

--- split-strings/solution.go
+++ split-strings/solution.go
@@ -1,19 +1,12 @@
 package kata
 
 func Solution(str string) []string {
-	if len(str) == 0 {
-		return []string{}
-	}
-
-	// Pad with underscore if odd length
 	if len(str)%2 != 0 {
 		str += "_"
 	}
-
-	result := make([]string, len(str)/2)
+	result := make([]string, 0, len(str)/2)
 	for i := 0; i < len(str); i += 2 {
-		result[i/2] = str[i : i+2]
+		result = append(result, str[i:i+2])
 	}
-
 	return result
 }
