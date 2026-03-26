b676f9ee54bb87fa1f19459db95490fddeb6b0f4
t-nielzon <t-nielzon@users.noreply.github.com>
2026-03-26 20:57:50 +0800

    Add go solution: weight-for-weight

--- weight-for-weight/problem.go
+++ weight-for-weight/problem.go
@@ -17,8 +17,7 @@ Example:
 "56 65 74 100 99 68 86 180 90" ordered by numbers weights becomes:
 "100 180 90 56 65 74 68 86 99"
 
-When two numbers have the same "weight", let us class them as if they were strings (alphabetical ordering) and not numbers:
-180 is before 90 since, having the same "weight" (9), it comes before as a string.
+When two numbers have the same "weight", let us class them as if they were strings (alphabetical ordering) and not numbers.
 
 All numbers in the list are positive numbers and the list can be empty.
 
--- weight-for-weight/solution.go
+++ weight-for-weight/solution.go
@@ -5,22 +5,22 @@ import (
 	"strings"
 )
 
-func digitWeight(s string) int {
-	w := 0
-	for _, c := range s {
-		w += int(c - '0')
-	}
-	return w
-}
-
 func OrderWeight(strng string) string {
 	fields := strings.Fields(strng)
 	sort.SliceStable(fields, func(i, j int) bool {
-		wi, wj := digitWeight(fields[i]), digitWeight(fields[j])
+		wi, wj := digitSum(fields[i]), digitSum(fields[j])
 		if wi != wj {
 			return wi < wj
 		}
 		return fields[i] < fields[j]
 	})
 	return strings.Join(fields, " ")
+}
+
+func digitSum(s string) int {
+	sum := 0
+	for _, c := range s {
+		sum += int(c - '0')
+	}
+	return sum
 }
