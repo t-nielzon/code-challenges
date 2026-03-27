ac97d5180c676d9a8c919473bade0b0e827581df
t-nielzon <t-nielzon@users.noreply.github.com>
2026-03-27 22:50:59 +0800

    Add javascript solution: maximum-subarray-sum

---
+++ maximum-subarray-sum/problem.js
@@ -0,0 +1,23 @@
+/*
+ * Maximum Subarray Sum (5 kyu)
+ *
+ * The maximum sum subarray problem consists in finding the maximum sum
+ * of a contiguous subsequence in an array or list of integers.
+ *
+ * Example:
+ *   Input: [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+ *   Output: 6 (Sum of [4, -1, 2, 1])
+ *
+ * Easy case is when the list is made up of only positive numbers and
+ * the maximum sum is the sum of the whole array. If the list is made
+ * up of only negative numbers, return 0 instead.
+ *
+ * Empty list is considered to have zero greatest sum. Note that the
+ * empty list or array is also a valid sublist/subarray.
+ */
+
+var maxSequence = function(arr) {
+
+};
+
+module.exports = maxSequence;
---
+++ maximum-subarray-sum/solution.js
@@ -0,0 +1,15 @@
+var maxSequence = function(arr) {
+  if (!arr.length) return 0;
+
+  let maxSum = 0;
+  let currentSum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    currentSum = Math.max(0, currentSum + arr[i]);
+    maxSum = Math.max(maxSum, currentSum);
+  }
+
+  return maxSum;
+};
+
+module.exports = maxSequence;
