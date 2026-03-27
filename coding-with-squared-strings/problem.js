aac229dceff9afc55643bb77003a30a013839d7b
t-nielzon <t-nielzon@users.noreply.github.com>
2026-03-27 09:18:16 +0800

    Add javascript solution: coding-with-squared-strings

--- coding-with-squared-strings/problem.js
+++ coding-with-squared-strings/problem.js
@@ -5,19 +5,16 @@
  * e.g. s = "abcd\nefgh\nijkl\nmnop" is a squared string of size 4.
  *
  * Coding:
- * - Given text t of length l, find smallest n where n*n >= l
+ * - Take text t of length l
+ * - Find smallest n where n*n >= l
  * - Pad t with char code 11 until length is n*n
  * - Form squared string of size n
  * - Apply clockwise 90° rotation
  *
  * Decoding:
+ * - Take squared string s from coding
  * - Apply counter-clockwise 90° rotation
- * - Remove padding chars (char code 11)
- *
- * Example:
- * t = "I.was.going.fishing.that.morning.at.ten.o'clock"
- * code(t) -> "c.nhsoI\nltiahi.\noentinw\ncng.nga\nk..mg.s\n\voao.f.\n\v'trtig"
- * decode(code(t)) == "I.was.going.fishing.that.morning.at.ten.o'clock"
+ * - Clean padding to recover original text
  */
 
 function code(s) {
