c4adf7ca81a40c2d3dbebfd05782ddcc4646fdb1
t-nielzon <t-nielzon@users.noreply.github.com>
2026-03-28 20:42:19 +0800

    Add go solution: longest-common-subsequence

---
+++ longest-common-subsequence/problem.go
@@ -0,0 +1,24 @@
+/*
+Write a function called LCS that accepts two sequences and returns the
+longest subsequence common to the passed in sequences.
+
+Subsequence:
+A subsequence is different from a substring. The terms of a subsequence
+need not be consecutive terms of the original sequence.
+
+Example subsequences of "abc" = "a", "b", "c", "ab", "ac", "bc" and "abc".
+
+LCS examples:
+  LCS( "abcdef", "abc" ) => returns "abc"
+  LCS( "abcdef", "acf" ) => returns "acf"
+  LCS( "132535365", "123456789" ) => returns "12356"
+
+Notes:
+- Both arguments will be strings
+- Return value must be a string
+- Return an empty string if there exists no common subsequence
+*/
+package kata
+
+func LCS(s1, s2 string) string {
+}
---
+++ longest-common-subsequence/solution.go
@@ -0,0 +1,40 @@
+package kata
+
+func LCS(s1, s2 string) string {
+	m, n := len(s1), len(s2)
+	dp := make([][]int, m+1)
+	for i := range dp {
+		dp[i] = make([]int, n+1)
+	}
+
+	for i := 1; i <= m; i++ {
+		for j := 1; j <= n; j++ {
+			if s1[i-1] == s2[j-1] {
+				dp[i][j] = dp[i-1][j-1] + 1
+			} else if dp[i-1][j] >= dp[i][j-1] {
+				dp[i][j] = dp[i-1][j]
+			} else {
+				dp[i][j] = dp[i][j-1]
+			}
+		}
+	}
+
+	result := make([]byte, 0, dp[m][n])
+	i, j := m, n
+	for i > 0 && j > 0 {
+		if s1[i-1] == s2[j-1] {
+			result = append(result, s1[i-1])
+			i--
+			j--
+		} else if dp[i-1][j] >= dp[i][j-1] {
+			i--
+		} else {
+			j--
+		}
+	}
+
+	for l, r := 0, len(result)-1; l < r; l, r = l+1, r-1 {
+		result[l], result[r] = result[r], result[l]
+	}
+	return string(result)
+}
