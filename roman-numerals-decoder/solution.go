191300b76c51b37c3d796eed713ec895e1e2939a
t-nielzon <t-nielzon@users.noreply.github.com>
2026-03-28 20:39:37 +0800

    Add go solution: roman-numerals-decoder

--- roman-numerals-decoder/solution.go
+++ roman-numerals-decoder/solution.go
@@ -5,6 +5,7 @@ func Decode(roman string) int {
 		'I': 1, 'V': 5, 'X': 10, 'L': 50,
 		'C': 100, 'D': 500, 'M': 1000,
 	}
+
 	result := 0
 	for i := 0; i < len(roman); i++ {
 		if i+1 < len(roman) && values[roman[i]] < values[roman[i+1]] {
