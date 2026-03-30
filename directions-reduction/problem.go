23ee7b2032516bb42547d9aeb6b3ed720735b427
t-nielzon <t-nielzon@users.noreply.github.com>
2026-03-30 09:27:26 +0800

    Add go solution: directions-reduction

--- directions-reduction/problem.go
+++ directions-reduction/problem.go
@@ -1,24 +1,22 @@
 /*
-Directions Reduction
+Once upon a time, on a way through the old wild mountainous west,
+a man was given directions to go from one point to another.
+The directions were "NORTH", "SOUTH", "WEST", "EAST".
+Clearly "NORTH" and "SOUTH" are opposite, "WEST" and "EAST" too.
 
-Once upon a time, on a way through the old wild mountainous west,…
+Going to one direction and coming back the opposite direction right away
+is a needless effort. Since this is the wild west, with dreadful weather
+and not much water, it's important to save yourself some energy, otherwise
+you might die of thirst!
 
-… a man was given directions to go from one point to another. The directions were
-"NORTH", "SOUTH", "WEST", "EAST". Clearly "NORTH" and "SOUTH" are opposite, "WEST"
-and "EAST" too.
+Write a function dirReduc which will take an array of strings and returns
+an array of strings with the needless directions removed (W<->E or S<->N
+side by side).
 
-Going to one direction and coming back the opposite direction right away is a
-needless effort. Since this is the wild west, with dreadful weather and not much
-water, it's important to save yourself some energy, otherwise you might die of thirst!
-
-The task is to give to the man a simplified version of the plan by removing needless
-directions (W<->E or S<->N side by side).
-
-Examples:
-- ["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"] -> ["WEST"]
-- ["NORTH", "SOUTH", "EAST", "WEST"] -> []
-- ["NORTH", "EAST", "WEST", "SOUTH", "WEST", "WEST"] -> ["WEST", "WEST"]
-- ["NORTH", "WEST", "SOUTH", "EAST"] -> ["NORTH", "WEST", "SOUTH", "EAST"] (not reducible)
+Not all paths can be made simpler. The path ["NORTH", "WEST", "SOUTH", "EAST"]
+is not reducible. "NORTH" and "WEST", "WEST" and "SOUTH", "SOUTH" and "EAST"
+are not directly opposite of each other and can't become such.
+Hence the result path is itself : ["NORTH", "WEST", "SOUTH", "EAST"].
 */
 package kata
 
--- directions-reduction/solution.go
+++ directions-reduction/solution.go
@@ -1,22 +1,20 @@
 package kata
 
 func DirReduc(arr []string) []string {
-	opposites := map[string]string{
+	opposite := map[string]string{
 		"NORTH": "SOUTH",
 		"SOUTH": "NORTH",
 		"EAST":  "WEST",
 		"WEST":  "EAST",
 	}
 
-	stack := []string{}
-
+	stack := make([]string, 0, len(arr))
 	for _, dir := range arr {
-		if len(stack) > 0 && opposites[dir] == stack[len(stack)-1] {
+		if len(stack) > 0 && opposite[stack[len(stack)-1]] == dir {
 			stack = stack[:len(stack)-1]
 		} else {
 			stack = append(stack, dir)
 		}
 	}
-
 	return stack
 }
