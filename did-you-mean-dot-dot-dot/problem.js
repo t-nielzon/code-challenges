be864a20db2c1baa3c2b5184f6a329b6ee23e8f3
t-nielzon <t-nielzon@users.noreply.github.com>
2026-03-31 09:30:26 +0800

    Add javascript solution: did-you-mean-dot-dot-dot

--- did-you-mean-dot-dot-dot/problem.js
+++ did-you-mean-dot-dot-dot/problem.js
@@ -1,23 +1,11 @@
 /*
- * Did you mean ...?
- * 
- * I'm sure, you know Google's "Did you mean ...?", when you entered a search
- * term and mistyped a word. In this kata we want to implement something similar.
- * 
+ * Did you mean ...? (5 kyu)
+ *
  * You'll get an entered term (lowercase string) and an array of known words
- * (also lowercase strings). Your task is to find out, which word from the
+ * (also lowercase strings). Your task is to find out which word from the
  * dictionary is most similar to the entered one. The similarity is described
- * by the minimum number of letters you have to add, remove or replace in order
- * to get from the entered word to one of the dictionary. The lower the number
- * of required changes, the higher the similarity between each two words.
- * 
- * Same words are obviously the most similar ones. A word that needs one letter
- * to be changed is more similar to another word that needs 2 (or more) letters
- * to be changed. E.g. the mistyped term `berr` is more similar to `beer`
- * (1 letter to be replaced) than to `barrel` (3 letters to be changed in total).
- * 
- * Extend the dictionary in a way, that it is able to return you the most similar
- * word from the list of known words.
+ * by the minimum number of letters you have to add, remove or replace in
+ * order to get from the entered word to one of the dictionary (Levenshtein distance).
  */
 
 function Dictionary(words) {
@@ -25,5 +13,5 @@ function Dictionary(words) {
 }
 
 Dictionary.prototype.findMostSimilar = function(term) {
-  
+
 };
--- did-you-mean-dot-dot-dot/solution.js
+++ did-you-mean-dot-dot-dot/solution.js
@@ -3,49 +3,38 @@ function Dictionary(words) {
 }
 
 Dictionary.prototype.findMostSimilar = function(term) {
-  // uses levenshtein distance to find minimum edit distance
-  const levenshtein = (a, b) => {
-    if (a === b) return 0;
-    if (a.length === 0) return b.length;
-    if (b.length === 0) return a.length;
-    
-    const matrix = [];
-    
-    for (let i = 0; i <= b.length; i++) {
-      matrix[i] = [i];
-    }
-    
-    for (let j = 0; j <= a.length; j++) {
-      matrix[0][j] = j;
-    }
-    
-    for (let i = 1; i <= b.length; i++) {
-      for (let j = 1; j <= a.length; j++) {
-        if (b[i - 1] === a[j - 1]) {
-          matrix[i][j] = matrix[i - 1][j - 1];
-        } else {
-          matrix[i][j] = Math.min(
-            matrix[i - 1][j - 1] + 1, // substitution
-            matrix[i][j - 1] + 1,     // insertion
-            matrix[i - 1][j] + 1      // deletion
-          );
-        }
-      }
-    }
-    
-    return matrix[b.length][a.length];
-  };
-  
-  let mostSimilar = this.words[0];
-  let minDistance = levenshtein(term, this.words[0]);
-  
-  for (let i = 1; i < this.words.length; i++) {
-    const distance = levenshtein(term, this.words[i]);
-    if (distance < minDistance) {
-      minDistance = distance;
-      mostSimilar = this.words[i];
+  let bestWord = '';
+  let bestDist = Infinity;
+
+  for (const word of this.words) {
+    const dist = levenshtein(term, word);
+    if (dist < bestDist) {
+      bestDist = dist;
+      bestWord = word;
+      if (dist === 0) return word;
     }
   }
-  
-  return mostSimilar;
+
+  return bestWord;
 };
+
+function levenshtein(a, b) {
+  const m = a.length, n = b.length;
+  const dp = Array(n + 1);
+
+  for (let j = 0; j <= n; j++) dp[j] = j;
+
+  for (let i = 1; i <= m; i++) {
+    let prev = dp[0];
+    dp[0] = i;
+    for (let j = 1; j <= n; j++) {
+      const temp = dp[j];
+      dp[j] = a[i - 1] === b[j - 1]
+        ? prev
+        : 1 + Math.min(prev, dp[j], dp[j - 1]);
+      prev = temp;
+    }
+  }
+
+  return dp[n];
+}
