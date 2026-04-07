691f80c68985776dbe31b5fc30a5c9f31b7ca8ee
t-nielzon <t-nielzon@users.noreply.github.com>
2026-04-07 09:24:52 +0800

    Add go solution: simple-fun-number-178-faulty-odometer

--- simple-fun-number-178-faulty-odometer/solution.go
+++ simple-fun-number-178-faulty-odometer/solution.go
@@ -2,14 +2,14 @@ package kata
 
 func FaultyOdometer(n int) int {
 	result := 0
-	base := 1
+	multiplier := 1
 	for n > 0 {
 		d := n % 10
-		if d >= 5 {
+		if d > 4 {
 			d--
 		}
-		result += d * base
-		base *= 9
+		result += d * multiplier
+		multiplier *= 9
 		n /= 10
 	}
 	return result
