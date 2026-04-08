aab6acb41da4057f337d7171745789e227fee59d
t-nielzon <t-nielzon@users.noreply.github.com>
2026-04-08 09:55:58 +0800

    Add go solution: is-a-number-prime

---
+++ is-a-number-prime/solution.go
@@ -0,0 +1,19 @@
+package kata
+
+func IsPrime(n int) bool {
+	if n <= 1 {
+		return false
+	}
+	if n <= 3 {
+		return true
+	}
+	if n%2 == 0 || n%3 == 0 {
+		return false
+	}
+	for i := 5; i*i <= n; i += 6 {
+		if n%i == 0 || n%(i+2) == 0 {
+			return false
+		}
+	}
+	return true
+}
