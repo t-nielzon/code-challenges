50ede7b2d5384ec2e890f759011101fbb5190e81
t-nielzon <t-nielzon@users.noreply.github.com>
2026-04-08 09:55:40 +0800

    Add go solution: directions-reduction

--- directions-reduction/problem.go
+++ directions-reduction/problem.go
@@ -1,22 +1,20 @@
 /*
 Once upon a time, on a way through the old wild mountainous west,
-a man was given directions to go from one point to another.
-The directions were "NORTH", "SOUTH", "WEST", "EAST".
-Clearly "NORTH" and "SOUTH" are opposite, "WEST" and "EAST" too.
+a man was given directions to go from one point to another. The
+directions were "NORTH", "SOUTH", "WEST", "EAST". Clearly "NORTH"
+and "SOUTH" are opposite, "WEST" and "EAST" too.
 
-Going to one direction and coming back the opposite direction right away
-is a needless effort. Since this is the wild west, with dreadful weather
-and not much water, it's important to save yourself some energy, otherwise
-you might die of thirst!
+Going to one direction and coming back the opposite direction right
+away is a needless effort. Since this is the wild west, with dreadful
+weather and not much water, it's important to save yourself some
+energy, otherwise you might die of thirst!
 
-Write a function dirReduc which will take an array of strings and returns
-an array of strings with the needless directions removed (W<->E or S<->N
-side by side).
+Write a function dirReduc which will take an array of strings and
+returns an array of strings with the needless directions removed
+(W<->E or S<->N side by side).
 
-Not all paths can be made simpler. The path ["NORTH", "WEST", "SOUTH", "EAST"]
-is not reducible. "NORTH" and "WEST", "WEST" and "SOUTH", "SOUTH" and "EAST"
-are not directly opposite of each other and can't become such.
-Hence the result path is itself : ["NORTH", "WEST", "SOUTH", "EAST"].
+Not all paths can be made simpler. The path
+["NORTH", "WEST", "SOUTH", "EAST"] is not reducible.
 */
 package kata
 
