064e3f914a27f0d74162e5678a5345b62fa72a66
t-nielzon <t-nielzon@users.noreply.github.com>
2026-04-11 11:27:36 +0800

    Add go solution: camelcase-method

--- camelcase-method/solution.go
+++ camelcase-method/solution.go
@@ -3,9 +3,11 @@ package kata
 import "strings"
 
 func CamelCase(s string) string {
-	var b strings.Builder
-	for _, word := range strings.Fields(s) {
-		b.WriteString(strings.ToUpper(word[:1]) + word[1:])
+	words := strings.Fields(s)
+	for i, w := range words {
+		if len(w) > 0 {
+			words[i] = strings.ToUpper(w[:1]) + w[1:]
+		}
 	}
-	return b.String()
+	return strings.Join(words, "")
 }
