d47139de629c272bbf28ddf327dc7886f6d25c77
t-nielzon <t-nielzon@users.noreply.github.com>
2026-04-14 08:55:26 +0800

    Add go solution: convert-string-to-camel-case

--- convert-string-to-camel-case/problem.go
+++ convert-string-to-camel-case/problem.go
@@ -1,6 +1,4 @@
 /*
-Convert string to camel case
-
 Complete the method/function so that it converts dash/underscore delimited
 words into camel casing. The first word within the output should be
 capitalized only if the original word was capitalized (known as Upper Camel
@@ -8,10 +6,9 @@ Case, also often referred to as Pascal case). The next words should be
 always capitalized.
 
 Examples:
-
-"the-stealth-warrior" gets converted to "theStealthWarrior"
-"The_Stealth_Warrior" gets converted to "TheStealthWarrior"
-"The_Stealth-Warrior" gets converted to "TheStealthWarrior"
+  "the-stealth-warrior" gets converted to "theStealthWarrior"
+  "The_Stealth_Warrior" gets converted to "TheStealthWarrior"
+  "The_Stealth-Warrior" gets converted to "TheStealthWarrior"
 */
 package kata
 
--- convert-string-to-camel-case/solution.go
+++ convert-string-to-camel-case/solution.go
@@ -3,15 +3,11 @@ package kata
 import "strings"
 
 func ToCamelCase(s string) string {
-	if s == "" {
-		return ""
-	}
-	s = strings.ReplaceAll(s, "_", "-")
-	words := strings.Split(s, "-")
+	words := strings.FieldsFunc(s, func(r rune) bool {
+		return r == '-' || r == '_'
+	})
 	for i := 1; i < len(words); i++ {
-		if len(words[i]) > 0 {
-			words[i] = strings.ToUpper(words[i][:1]) + words[i][1:]
-		}
+		words[i] = strings.ToUpper(words[i][:1]) + words[i][1:]
 	}
 	return strings.Join(words, "")
 }
