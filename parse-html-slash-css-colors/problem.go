5d41129413071df2139bec95bea8c21b285165fc
t-nielzon <t-nielzon@users.noreply.github.com>
2026-04-16 12:52:36 +0800

    Add go solution: parse-html-slash-css-colors

--- parse-html-slash-css-colors/problem.go
+++ parse-html-slash-css-colors/problem.go
@@ -3,12 +3,25 @@ Parse HTML/CSS Colors
 
 In this kata you parse RGB colors represented by strings. The formats are
 primarily used in HTML and CSS. Your task is to implement a function which
-takes a color as a string and returns the parsed color as a map.
+takes a color as a string and returns the parsed color as a Color struct.
 
 Input:
-- 6-digit hexadecimal - "#RRGGBB" e.g. "#012345", "#789abc", "#FFA077"
-- 3-digit hexadecimal - "#RGB" e.g. "#012", "#aaa", "#F5A"
-- Preset color name e.g. "red", "BLUE", "LimeGreen"
+The input string represents one of the following:
+
+- 6-digit hexadecimal - "#RRGGBB"
+  e.g. "#012345", "#789abc", "#FFA077"
+  Each pair of digits represents a value of the channel in hexadecimal: 00 to FF
+
+- 3-digit hexadecimal - "#RGB"
+  e.g. "#012", "#aaa", "#F5A"
+  Each digit represents a value 0 to F which translates to 2-digit hexadecimal:
+  0->00, 1->11, 2->22, and so on.
+
+- Preset color name
+  e.g. "red", "BLUE", "LimeGreen"
+  You have to use the predefined map PresetColors. The keys are the names of
+  preset colors in lower-case and the values are the corresponding colors in
+  6-digit hexadecimal (same as "#RRGGBB").
 
 Examples:
   Parse("#80FFA0")   == Color{128, 255, 160}
@@ -17,10 +30,11 @@ Examples:
 */
 package kata
 
-// Color struct is defined as follows:
+// Color struct is predefined:
 // type Color struct{ R, G, B byte }
 
-// PresetColors is a predefined map with lower-case keys and "#RRGGBB" values.
+// PresetColors is a predefined map[string]string with lowercase keys
+// and "#RRGGBB" values.
 
 func Parse(color string) Color {
 }
