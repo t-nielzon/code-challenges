fdd86656581125df7f681eae6dd255859c01f9d7
t-nielzon <t-nielzon@users.noreply.github.com>
2026-04-16 09:04:28 +0800

    Add go solution: best-travel

--- best-travel/problem.go
+++ best-travel/problem.go
@@ -1,16 +1,19 @@
 /*
-John and Mary want to travel between a few towns A, B, C ... Mary has on a sheet of paper a list of distances between these towns.
+John and Mary want to travel between a few towns A, B, C ... Mary has on a
+sheet of paper a list of distances between these towns.
 ls = [50, 55, 57, 58, 60].
-John is tired of driving and he says to Mary that he doesn't want to drive more than t = 174 miles and he
-will visit only 3 towns.
+John is tired of driving and he says to Mary that he doesn't want to drive
+more than t = 174 miles and he will visit only 3 towns.
 
-Which distances, hence which towns, they will choose so that the sum of the distances is the biggest possible
-to please Mary and John?
+Which distances, hence which towns, they will choose so that the sum of the
+distances is the biggest possible to please Mary and John?
 
-The function chooseBestSum will take as parameters t (maximum sum of distances, integer >= 0), k (number of
-towns to visit, k >= 1) and ls (list of distances, all distances are positive or zero integers and this list
-has at least one element). The function returns the "best" sum ie the biggest possible sum of k distances less
-than or equal to the given limit t, if that sum exists, or otherwise -1.
+The function chooseBestSum will take as parameters t (maximum sum of
+distances, integer >= 0), k (number of towns to visit, k >= 1) and ls (list
+of distances, all distances are positive or zero integers and this list has
+at least one element). The function returns the "best" sum ie the biggest
+possible sum of k distances less than or equal to the given limit t, if that
+sum exists, or otherwise -1.
 */
 package kata
 
