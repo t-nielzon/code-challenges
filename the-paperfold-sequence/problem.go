04ada65775ebaaaee79b90c6a3e6ea842fa51efe
t-nielzon <t-nielzon@users.noreply.github.com>
2026-04-20 09:22:26 +0800

    Add go solution: the-paperfold-sequence

--- the-paperfold-sequence/problem.go
+++ the-paperfold-sequence/problem.go
@@ -1,15 +1,15 @@
 /*
-The PaperFold sequence
+The regular paperfolding sequence (dragon curve sequence) is an infinite
+automatic sequence of 0s and 1s defined as the limit of inserting an
+alternating sequence of 1s and 0s around and between the terms of the
+previous sequence.
 
-The regular paperfolding sequence, also known as the dragon curve sequence,
-is an infinite automatic sequence of 0s and 1s defined as the limit of
-inserting an alternating sequence of 1s and 0s around and between the terms
-of the previous sequence.
-
-Define a generator PaperFold that sequentially generates the values of this
-sequence. It will be tested for up to 1,000,000 values.
+Define a generator PaperFold that sequentially generates the values of
+this sequence. It will be tested for up to 1,000,000 values.
 */
+
 package kata
 
 func PaperFold() func() int {
+	// TODO: implement
 }
--- the-paperfold-sequence/solution.go
+++ the-paperfold-sequence/solution.go
@@ -1,18 +1,14 @@
 package kata
 
 func PaperFold() func() int {
-	// use the bit-trick: the value at 1-indexed position n is determined by
-	// removing trailing zeros from n, then checking the bit above them
 	n := 0
 	return func() int {
 		n++
-		// find the highest power of 2 dividing n
-		v := n
-		for v&1 == 0 {
-			v >>= 1
+		m := n
+		for m&1 == 0 {
+			m >>= 1
 		}
-		// the result depends on v mod 4
-		if v%4 == 1 {
+		if m&2 == 0 {
 			return 1
 		}
 		return 0
