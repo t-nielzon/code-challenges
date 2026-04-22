bd4a6cb73e875a46fbdfffed3f33c19047117da4
t-nielzon <t-nielzon@users.noreply.github.com>
2026-04-22 13:02:32 +0800

    Add go solution: camelcase-method

--- camelcase-method/problem.go
+++ camelcase-method/problem.go
@@ -1,14 +1,16 @@
+package kata
+
 /*
-Write a method (or function, depending on the language) that converts a string
-to camelCase, that is, all words must have their first letter capitalized and
-spaces must be removed.
+Write a method (or function, depending on the language) that converts a string to camelCase,
+that is, all words must have their first letter capitalized and spaces must be removed.
 
 Examples (input --> output):
 
-"hello case" --> "HelloCase"
-"camel case word" --> "CamelCaseWord"
+    "hello case" --> "HelloCase"
+    "camel case word" --> "CamelCaseWord"
+
+Don't forget to rate this kata! Thanks :)
 */
-package kata
 
 func CamelCase(s string) string {
 }
--- camelcase-method/solution.go
+++ camelcase-method/solution.go
@@ -5,9 +5,7 @@ import "strings"
 func CamelCase(s string) string {
 	words := strings.Fields(s)
 	for i, w := range words {
-		if len(w) > 0 {
-			words[i] = strings.ToUpper(w[:1]) + w[1:]
-		}
+		words[i] = strings.ToUpper(w[:1]) + w[1:]
 	}
 	return strings.Join(words, "")
 }
