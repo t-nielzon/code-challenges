27afe23e16fa8a49fcb9f58a1560a0fe90131588
t-nielzon <t-nielzon@users.noreply.github.com>
2026-04-26 20:16:14 +0800

    Add go solution: which-x-for-that-sum

--- which-x-for-that-sum/solution.go
+++ which-x-for-that-sum/solution.go
@@ -3,7 +3,5 @@ package kata
 import "math"
 
 func Solve(m float64) float64 {
-	// sum k*x^k (k=1..inf) = x/(1-x)^2, so m = x/(1-x)^2
-	// solving mx^2 - (2m+1)x + m = 0 via quadratic formula
 	return (2*m + 1 - math.Sqrt(4*m+1)) / (2 * m)
 }
