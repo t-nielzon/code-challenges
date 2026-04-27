f694c1a9624194fe895243e32ac58f8cb7e40685
t-nielzon <t-nielzon@users.noreply.github.com>
2026-04-27 20:56:33 +0800

    Add go solution: phone-directory

--- phone-directory/solution.go
+++ phone-directory/solution.go
@@ -6,14 +6,30 @@ import (
 	"strings"
 )
 
-func Phone(dir, num string) string {
-	lines := strings.Split(dir, "\n")
-	var matches []string
+func phone(strng string, num string) string {
+	lines := strings.Split(strng, "\n")
+	phoneRe := regexp.MustCompile(`\+\d{1,2}-\d{3}-\d{3}-\d{4}`)
+	nameRe := regexp.MustCompile(`<([^>]+)>`)
 
+	var matches []string
+	target := "+" + num
 	for _, line := range lines {
-		if strings.Contains(line, "+"+num) {
-			matches = append(matches, line)
+		if !strings.Contains(line, target) {
+			continue
 		}
+		// confirm it's a full phone match, not partial
+		phones := phoneRe.FindAllString(line, -1)
+		found := false
+		for _, p := range phones {
+			if p == target {
+				found = true
+				break
+			}
+		}
+		if !found {
+			continue
+		}
+		matches = append(matches, line)
 	}
 
 	if len(matches) == 0 {
@@ -24,33 +40,33 @@ func Phone(dir, num string) string {
 	}
 
 	line := matches[0]
-
-	// extract name between < and >
-	nameRe := regexp.MustCompile(`<([^>]+)>`)
 	nameMatch := nameRe.FindStringSubmatch(line)
 	name := ""
-	if nameMatch != nil {
+	if len(nameMatch) > 1 {
 		name = nameMatch[1]
 	}
 
-	// remove the phone number (with leading +) from the line to get the address
-	addr := line
-	addr = strings.Replace(addr, "+"+num, "", 1)
-
-	// remove the name tag including angle brackets
+	// remove phone and name from line to get address
+	addr := phoneRe.ReplaceAllString(line, "")
 	addr = nameRe.ReplaceAllString(addr, "")
 
-	// replace underscores with spaces
-	addr = strings.ReplaceAll(addr, "_", " ")
-
-	// replace non-alphanumeric characters (except spaces, dots, and dashes) with spaces
-	addrRe := regexp.MustCompile(`[^a-zA-Z0-9 .\-]`)
-	addr = addrRe.ReplaceAllString(addr, " ")
+	// replace non-alphanumeric (except space, dot, comma maybe) with space
+	// keep letters, digits, spaces, and basic punctuation? Looking at example:
+	// "/+1-541-754-3010 156 Alphand_St. <J Steeve>" -> "156 Alphand St."
+	// so underscore becomes space, slash removed
+	var b strings.Builder
+	for _, r := range addr {
+		if (r >= 'a' && r <= 'z') || (r >= 'A' && r <= 'Z') || (r >= '0' && r <= '9') || r == '.' || r == ' ' {
+			b.WriteRune(r)
+		} else {
+			b.WriteRune(' ')
+		}
+	}
+	addr = b.String()
 
 	// collapse multiple spaces
 	spaceRe := regexp.MustCompile(`\s+`)
 	addr = spaceRe.ReplaceAllString(addr, " ")
-
 	addr = strings.TrimSpace(addr)
 
 	return fmt.Sprintf("Phone => %s, Name => %s, Address => %s", num, name, addr)
