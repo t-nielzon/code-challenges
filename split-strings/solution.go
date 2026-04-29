dc8ba964ac6d82abc0f18ed3212a7208e07a036a
t-nielzon <t-nielzon@users.noreply.github.com>
2026-04-29 16:34:47 +0800

    Add go solution: split-strings

--- split-strings/solution.go
+++ split-strings/solution.go
@@ -1,12 +1,12 @@
 package kata
 
-func Solution(str string) []string {
-	if len(str)%2 != 0 {
-		str += "_"
+func SplitInParts(s string) []string {
+	if len(s)%2 != 0 {
+		s += "_"
 	}
-	result := make([]string, 0, len(str)/2)
-	for i := 0; i < len(str); i += 2 {
-		result = append(result, str[i:i+2])
+	result := []string{}
+	for i := 0; i < len(s); i += 2 {
+		result = append(result, s[i:i+2])
 	}
 	return result
 }
