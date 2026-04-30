d7fdd99fedbecbaa427c06e59834c781ac88147f
t-nielzon <t-nielzon@users.noreply.github.com>
2026-04-30 12:09:08 +0800

    Add go solution: longest-common-subsequence

--- longest-common-subsequence/problem.go
+++ longest-common-subsequence/problem.go
@@ -1,23 +1,23 @@
 /*
-Write a function called LCS that accepts two sequences and returns the
-longest subsequence common to the passed in sequences.
+Longest Common Subsequence
 
-Subsequence:
-A subsequence is different from a substring. The terms of a subsequence
-need not be consecutive terms of the original sequence.
+Write a function called `LCS` that accepts two sequences and returns the longest
+subsequence common to the passed in sequences.
 
-Example subsequences of "abc" = "a", "b", "c", "ab", "ac", "bc" and "abc".
+A subsequence is different from a substring. The terms of a subsequence need not
+be consecutive terms of the original sequence.
 
-LCS examples:
+Examples:
   LCS( "abcdef", "abc" ) => returns "abc"
   LCS( "abcdef", "acf" ) => returns "acf"
   LCS( "132535365", "123456789" ) => returns "12356"
 
 Notes:
-- Both arguments will be strings
-- Return value must be a string
-- Return an empty string if there exists no common subsequence
+  - Both arguments will be strings
+  - Return value must be a string
+  - Return an empty string if there exists no common subsequence
 */
+
 package kata
 
 func LCS(s1, s2 string) string {
--- longest-common-subsequence/solution.go
+++ longest-common-subsequence/solution.go
@@ -1,7 +1,10 @@
 package kata
 
 func LCS(s1, s2 string) string {
-	m, n := len(s1), len(s2)
+	a := []rune(s1)
+	b := []rune(s2)
+	m, n := len(a), len(b)
+
 	dp := make([][]int, m+1)
 	for i := range dp {
 		dp[i] = make([]int, n+1)
@@ -9,7 +12,7 @@ func LCS(s1, s2 string) string {
 
 	for i := 1; i <= m; i++ {
 		for j := 1; j <= n; j++ {
-			if s1[i-1] == s2[j-1] {
+			if a[i-1] == b[j-1] {
 				dp[i][j] = dp[i-1][j-1] + 1
 			} else if dp[i-1][j] >= dp[i][j-1] {
 				dp[i][j] = dp[i-1][j]
@@ -19,11 +22,11 @@ func LCS(s1, s2 string) string {
 		}
 	}
 
-	result := make([]byte, 0, dp[m][n])
+	result := make([]rune, 0, dp[m][n])
 	i, j := m, n
 	for i > 0 && j > 0 {
-		if s1[i-1] == s2[j-1] {
-			result = append(result, s1[i-1])
+		if a[i-1] == b[j-1] {
+			result = append(result, a[i-1])
 			i--
 			j--
 		} else if dp[i-1][j] >= dp[i][j-1] {
@@ -36,5 +39,6 @@ func LCS(s1, s2 string) string {
 	for l, r := 0, len(result)-1; l < r; l, r = l+1, r-1 {
 		result[l], result[r] = result[r], result[l]
 	}
+
 	return string(result)
 }
