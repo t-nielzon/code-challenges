dd540ffd3ef7a982890e0910b7e53f1b4825cbe4
t-nielzon <t-nielzon@users.noreply.github.com>
2026-05-04 13:26:34 +0800

    Add go solution: easy-cyclists-training

--- easy-cyclists-training/problem.go
+++ easy-cyclists-training/problem.go
@@ -1,20 +1,39 @@
 /*
-Easy Cyclist's Training
-
-John has bought a bike but before going mountain biking he wants us to do a few simulations.
+John has bought a bike but before going moutain biking he wants us to do a few simulations.
 
 He gathered information:
 - His trip will consist of an ascent of dTot kilometers with an average slope of slope percent
-- We suppose that: there is no wind, John's mass is constant MASS = 80 (kg), his power
-  (generated at time t by pedaling and measured in watts) at the start of the trip is WATTS0 = 225 (watts)
-- We don't take account of the rolling resistance
-- When he starts climbing at t = 0 his initial speed (pushed start) is v0 (km/h)
-- His initial acceleration gamma is 0.
-- Our time step is DELTA_T = 1.0 / 60.0 (in minutes)
+- John's mass is constant MASS = 80 (kg), initial power WATTS0 = 225 (watts)
+- No wind, no rolling resistance
+- Initial speed v0 (km/h), initial acceleration gamma = 0
+- DELTA_T = 1.0 / 60.0 (in minutes)
+- He loses D_WATTS * DELTA_T of power at each DELTA_T
+- Acceleration components:
+    1) -GRAVITY_ACC * function(slope) where slope is a percentage
+    2) -DRAG * abs(v) * abs(v) / MASS
+    3) if watts and v are strictly positive: +G_THRUST * watts / (v * MASS)
+    4) if total acceleration is <= 1e-5 set it to 0
+- If v - 3.0 <= 1e-2 John gives up
 
-Write function temps(v0, slope, dTot) which returns as a rounded integer the time t in minutes
-needed to travel dTot. If John gives up return -1.
+Constants:
+GRAVITY_ACC = 9.81 * 3.6 * 60.0
+DRAG        = 60.0 * 0.3 / 3.6
+DELTA_T     = 1.0 / 60.0
+G_THRUST    = 60 * 3.6 * 3.6
+MASS        = 80.0
+WATTS0      = 225.0
+D_WATTS     = 0.5
+
+Task:
+Write function temps(v0, slope, dTot) which returns as a rounded integer
+the time t in minutes needed to travel dTot. If John gives up return -1.
+
+Examples:
+temps(30, 5, 30)  -> 114
+temps(30, 20, 30) -> -1
+temps(30, 8, 20)  -> 110
 */
+
 package kata
 
 func Temps(v0, slope, dTot float64) int {
--- easy-cyclists-training/solution.go
+++ easy-cyclists-training/solution.go
@@ -2,41 +2,40 @@ package kata
 
 import "math"
 
-const (
-	GRAVITY_ACC = 9.81 * 3.6 * 60.0
-	DRAG        = 60.0 * 0.3 / 3.6
-	DELTA_T     = 1.0 / 60.0
-	G_THRUST    = 60 * 3.6 * 3.6
-	MASS        = 80.0
-	WATTS0      = 225.0
-	D_WATTS     = 0.5
-)
-
 func Temps(v0, slope, dTot float64) int {
-	t := 0.0
+	const (
+		GRAVITY_ACC = 9.81 * 3.6 * 60.0
+		DRAG        = 60.0 * 0.3 / 3.6
+		DELTA_T     = 1.0 / 60.0
+		G_THRUST    = 60.0 * 3.6 * 3.6
+		MASS        = 80.0
+		WATTS0      = 225.0
+		D_WATTS     = 0.5
+	)
+
+	s := slope / 100.0
+	fSlope := s / math.Sqrt(1.0+s*s)
+
 	v := v0
 	d := 0.0
+	t := 0.0
 	watts := WATTS0
-	slopeRad := math.Sin(math.Atan(slope / 100.0))
 
 	for d < dTot {
-		gamma := -GRAVITY_ACC * slopeRad
-		gamma -= DRAG * math.Abs(v) * math.Abs(v) / MASS
+		gamma := -GRAVITY_ACC*fSlope - DRAG*math.Abs(v)*math.Abs(v)/MASS
 		if watts > 0 && v > 0 {
 			gamma += G_THRUST * watts / (v * MASS)
 		}
-		if gamma <= 1e-5 {
+		if math.Abs(gamma) <= 1e-5 {
 			gamma = 0
 		}
-
 		v = v + gamma*DELTA_T
-		d = d + v*DELTA_T/60.0
-		t += DELTA_T
-		watts -= D_WATTS * DELTA_T
-
 		if v-3.0 <= 1e-2 {
 			return -1
 		}
+		d = d + v*DELTA_T/60.0
+		t += DELTA_T
+		watts -= D_WATTS * DELTA_T
 	}
 
 	return int(math.Round(t))
