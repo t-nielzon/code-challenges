88ff89852e34e30fadebb08d611841d4e764046d
t-nielzon <t-nielzon@users.noreply.github.com>
2026-05-04 22:22:04 +0800

    Add go solution: simple-assembler-interpreter

--- simple-assembler-interpreter/solution.go
+++ simple-assembler-interpreter/solution.go
@@ -6,27 +6,32 @@ import (
 )
 
 func SimpleAssembler(program []string) map[string]int {
-	registers := make(map[string]int)
+	registers := map[string]int{}
 
-	value := func(token string) int {
-		if n, err := strconv.Atoi(token); err == nil {
+	value := func(s string) int {
+		if n, err := strconv.Atoi(s); err == nil {
 			return n
 		}
-		return registers[token]
+		return registers[s]
 	}
 
-	for i := 0; i < len(program); i++ {
+	for i := 0; i < len(program); {
 		parts := strings.Fields(program[i])
 		switch parts[0] {
 		case "mov":
 			registers[parts[1]] = value(parts[2])
+			i++
 		case "inc":
 			registers[parts[1]]++
+			i++
 		case "dec":
 			registers[parts[1]]--
+			i++
 		case "jnz":
 			if value(parts[1]) != 0 {
-				i += value(parts[2]) - 1
+				i += value(parts[2])
+			} else {
+				i++
 			}
 		}
 	}
