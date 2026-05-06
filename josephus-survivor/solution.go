905990d7a762ea1298c356108bd115318d51792c
t-nielzon <t-nielzon@users.noreply.github.com>
2026-05-06 18:59:30 +0800

    Add go solution: josephus-survivor

--- josephus-survivor/solution.go
+++ josephus-survivor/solution.go
@@ -1,7 +1,6 @@
 package kata
 
-func JosSurvivor(n, k int) int {
-	// iterative josephus formula: position is 0-indexed, convert to 1-indexed at the end
+func JosephusSurvivor(n, k int) int {
 	survivor := 0
 	for i := 2; i <= n; i++ {
 		survivor = (survivor + k) % i
