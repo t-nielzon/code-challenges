0a7739ffb58741979501400434373091e4f558a4
t-nielzon <t-nielzon@users.noreply.github.com>
2026-05-07 16:36:15 +0800

    Add go solution: not-very-secure

---
+++ not-very-secure/solution.go
@@ -0,0 +1,13 @@
+package kata
+
+func Alphanumeric(s string) bool {
+	if len(s) == 0 {
+		return false
+	}
+	for _, c := range s {
+		if !((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9')) {
+			return false
+		}
+	}
+	return true
+}
