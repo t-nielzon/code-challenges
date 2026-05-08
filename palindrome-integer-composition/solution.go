2f06f06f6b72bbcdc9f489bc9337b52553e53159
t-nielzon <t-nielzon@users.noreply.github.com>
2026-05-08 21:22:09 +0800

    Add go solution: palindrome-integer-composition

--- palindrome-integer-composition/solution.go
+++ palindrome-integer-composition/solution.go
@@ -2,28 +2,29 @@ package kata
 
 import "strconv"
 
-func Values(n int) int {
-	seen := make(map[int]bool)
-
-	for i := 1; i*i < n; i++ {
-		sum := 0
-		for j := i; sum < n; j++ {
-			sum += j * j
-			if j > i && sum < n && isPalindrome(sum) {
-				seen[sum] = true
-			}
-		}
-	}
-
-	return len(seen)
-}
-
-func isPalindrome(n int) bool {
-	s := strconv.Itoa(n)
+func isPalindrome(x uint64) bool {
+	s := strconv.FormatUint(x, 10)
 	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
 		if s[i] != s[j] {
 			return false
 		}
 	}
 	return true
+}
+
+func Values(n uint64) int {
+	found := make(map[uint64]struct{})
+	for i := uint64(1); i*i < n; i++ {
+		sum := i * i
+		for j := i + 1; ; j++ {
+			sum += j * j
+			if sum >= n {
+				break
+			}
+			if isPalindrome(sum) {
+				found[sum] = struct{}{}
+			}
+		}
+	}
+	return len(found)
 }
