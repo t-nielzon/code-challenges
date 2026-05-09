37f0ef4c227a7c96984bff29524ed38acddf2da2
t-nielzon <t-nielzon@users.noreply.github.com>
2026-05-09 18:19:46 +0800

    Add javascript solution: simpsons-rule-approximate-integration

---
+++ simpsons-rule-approximate-integration/solution.js
@@ -0,0 +1,18 @@
+function simpson(n) {
+    const f = (x) => 1.5 * Math.pow(Math.sin(x), 3);
+    const a = 0;
+    const b = Math.PI;
+    const h = (b - a) / n;
+
+    let sumOdd = 0;
+    for (let i = 1; i <= n / 2; i++) {
+        sumOdd += f(a + (2 * i - 1) * h);
+    }
+
+    let sumEven = 0;
+    for (let i = 1; i <= n / 2 - 1; i++) {
+        sumEven += f(a + 2 * i * h);
+    }
+
+    return ((b - a) / (3 * n)) * (f(a) + f(b) + 4 * sumOdd + 2 * sumEven);
+}
