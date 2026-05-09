644434d609a81cede26b26c1eac1a5d751da47cf
t-nielzon <t-nielzon@users.noreply.github.com>
2026-05-09 13:28:37 +0800

    Add go solution: playing-with-passphrases

--- playing-with-passphrases/problem.go
+++ playing-with-passphrases/problem.go
@@ -1,3 +1,5 @@
+package kata
+
 /*
 Everyone knows passphrases. One can choose passphrases from poems, songs, movies names and so on but frequently
 they can be guessed due to common cultural references.
@@ -10,8 +12,17 @@ choose a text in capital letters including or not digits and non alphabetic char
 3. keep such as non alphabetic and non digit characters,
 4. downcase each letter in odd position, upcase each letter in even position (the first character is in position 0),
 5. reverse the whole result.
+
+Example:
+
+your text: "BORN IN 2015!", shift 1
+
+1 + 2 + 3 -> "CPSO JO 7984!"
+
+4 "CpSo jO 7984!"
+
+5 "!4897 Oj oSpC"
 */
-package kata
 
 func PlayPass(s string, n int) string {
 }
--- playing-with-passphrases/solution.go
+++ playing-with-passphrases/solution.go
@@ -1,31 +1,30 @@
 package kata
 
-import (
-	"strings"
-	"unicode"
-)
-
 func PlayPass(s string, n int) string {
-	var buf []byte
-	for i, ch := range s {
-		var c byte
+	runes := []rune(s)
+	out := make([]rune, len(runes))
+	for i, r := range runes {
 		switch {
-		case unicode.IsLetter(ch):
-			shifted := (int(unicode.ToUpper(ch)-'A') + n) % 26
-			if i%2 == 0 {
-				c = byte('A' + shifted)
-			} else {
-				c = byte('a' + shifted)
+		case r >= 'A' && r <= 'Z':
+			shifted := rune((int(r-'A')+n)%26) + 'A'
+			if i%2 == 1 {
+				shifted = shifted - 'A' + 'a'
 			}
-		case unicode.IsDigit(ch):
-			c = byte('0' + (9 - (ch - '0')))
+			out[i] = shifted
+		case r >= 'a' && r <= 'z':
+			shifted := rune((int(r-'a')+n)%26) + 'a'
+			if i%2 == 0 {
+				shifted = shifted - 'a' + 'A'
+			}
+			out[i] = shifted
+		case r >= '0' && r <= '9':
+			out[i] = '9' - (r - '0')
 		default:
-			c = byte(ch)
+			out[i] = r
 		}
-		buf = append(buf, c)
 	}
-	for i, j := 0, len(buf)-1; i < j; i, j = i+1, j-1 {
-		buf[i], buf[j] = buf[j], buf[i]
+	for i, j := 0, len(out)-1; i < j; i, j = i+1, j-1 {
+		out[i], out[j] = out[j], out[i]
 	}
-	return strings.TrimRight(string(buf), "\x00")
+	return string(out)
 }
