4987f338c0d3a583629009e73b87afc26b74dc7b
t-nielzon <t-nielzon@users.noreply.github.com>
2026-05-09 10:54:01 +0800

    Add javascript solution: simple-fun-number-273-powerset

--- simple-fun-number-273-powerset/solution.js
+++ simple-fun-number-273-powerset/solution.js
@@ -1,21 +1,15 @@
 function powerset(nums) {
   const result = [];
-  
-  function dfs(index, current) {
-    if (index === nums.length) {
-      result.push([...current]);
+  const dfs = (i, current) => {
+    if (i === nums.length) {
+      result.push(current.slice());
       return;
     }
-    
-    // First, choose NOT to take the current element
-    dfs(index + 1, current);
-    
-    // Then, choose to take the current element
-    current.push(nums[index]);
-    dfs(index + 1, current);
+    dfs(i + 1, current);
+    current.push(nums[i]);
+    dfs(i + 1, current);
     current.pop();
-  }
-  
+  };
   dfs(0, []);
   return result;
 }
