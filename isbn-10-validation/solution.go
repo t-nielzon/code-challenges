543adff7e43959c108e7777f58bd3919baa8b1aa
t-nielzon <t-nielzon@users.noreply.github.com>
2026-05-09 18:18:48 +0800

    Add go solution: isbn-10-validation

--- isbn-10-validation/solution.go
+++ isbn-10-validation/solution.go
@@ -1,22 +1,20 @@
 package kata
 
-func IsValidISBN10(isbn string) bool {
+func ValidISBN10(isbn string) bool {
 	if len(isbn) != 10 {
 		return false
 	}
-
 	sum := 0
-	for i, ch := range isbn {
-		var val int
-		if ch >= '0' && ch <= '9' {
-			val = int(ch - '0')
-		} else if ch == 'X' && i == 9 {
-			val = 10
+	for i, c := range isbn {
+		var v int
+		if c >= '0' && c <= '9' {
+			v = int(c - '0')
+		} else if c == 'X' && i == 9 {
+			v = 10
 		} else {
 			return false
 		}
-		sum += val * (i + 1)
+		sum += v * (i + 1)
 	}
-
 	return sum%11 == 0
 }
