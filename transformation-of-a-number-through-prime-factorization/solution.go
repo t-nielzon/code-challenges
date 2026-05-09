f5e096acafea811b16a0e597a44926c672920a6b
t-nielzon <t-nielzon@users.noreply.github.com>
2026-05-09 21:01:41 +0800

    Add go solution: transformation-of-a-number-through-prime-factorization

--- transformation-of-a-number-through-prime-factorization/solution.go
+++ transformation-of-a-number-through-prime-factorization/solution.go
@@ -2,23 +2,29 @@ package kata
 
 func F(n int) int {
 	result := 1
-	x := n
-	for p := 2; p*p <= x; p++ {
-		if x%p == 0 {
+	hasExpGreaterThanOne := false
+	num := n
+	for p := 2; p*p <= num; p++ {
+		if num%p == 0 {
 			k := 0
-			for x%p == 0 {
-				x /= p
+			for num%p == 0 {
+				num /= p
 				k++
 			}
-			factor := k
-			for i := 0; i < k-1; i++ {
-				factor *= p
+			if k > 1 {
+				hasExpGreaterThanOne = true
+				factor := k
+				for i := 0; i < k-1; i++ {
+					factor *= p
+				}
+				result *= factor
 			}
-			result *= factor
 		}
 	}
-	if x > 1 {
-		result *= 1
+	if num > 1 {
+	}
+	if !hasExpGreaterThanOne {
+		return 1
 	}
 	return result
 }
