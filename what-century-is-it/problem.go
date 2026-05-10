740450fe503ceb99a0202517abb2427d3540df17
t-nielzon <t-nielzon@users.noreply.github.com>
2026-05-10 20:38:40 +0800

    Add go solution: what-century-is-it

--- what-century-is-it/problem.go
+++ what-century-is-it/problem.go
@@ -1,14 +1,13 @@
 /*
-Return the century of the input year. The input will always be a 4 digit
-string, so there is no need for validation.
+Return the century of the input year. The input will always be a 4 digit string, so there is no need for validation.
 
 Examples:
-  "1999" --> "20th"
-  "2011" --> "21st"
-  "2154" --> "22nd"
-  "2259" --> "23rd"
-  "1124" --> "12th"
-  "2000" --> "20th"
+"1999" --> "20th"
+"2011" --> "21st"
+"2154" --> "22nd"
+"2259" --> "23rd"
+"1124" --> "12th"
+"2000" --> "20th"
 */
 package kata
 
--- what-century-is-it/solution.go
+++ what-century-is-it/solution.go
@@ -1,17 +1,13 @@
 package kata
 
-import (
-	"fmt"
-	"strconv"
-)
+import "strconv"
 
 func WhatCentury(year string) string {
 	y, _ := strconv.Atoi(year)
-	century := (y + 99) / 100
-
+	c := (y + 99) / 100
 	suffix := "th"
-	if century%100 < 11 || century%100 > 13 {
-		switch century % 10 {
+	if c%100 < 11 || c%100 > 13 {
+		switch c % 10 {
 		case 1:
 			suffix = "st"
 		case 2:
@@ -20,6 +16,5 @@ func WhatCentury(year string) string {
 			suffix = "rd"
 		}
 	}
-
-	return fmt.Sprintf("%d%s", century, suffix)
+	return strconv.Itoa(c) + suffix
 }
