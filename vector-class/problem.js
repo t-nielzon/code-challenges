1453819f1b495c0a9fa94ac3630daffc8367b6a2
t-nielzon <t-nielzon@users.noreply.github.com>
2026-05-10 20:38:59 +0800

    Add javascript solution: vector-class

--- vector-class/problem.js
+++ vector-class/problem.js
@@ -1,11 +1,9 @@
 /*
- * Vector class
- *
  * Create a Vector object that supports addition, subtraction, dot products, and norms.
  *
- * var a = new Vector([1, 2, 3]);
- * var b = new Vector([3, 4, 5]);
- * var c = new Vector([5, 6, 7, 8]);
+ * a = new Vector([1, 2, 3])
+ * b = new Vector([3, 4, 5])
+ * c = new Vector([5, 6, 7, 8])
  *
  * a.add(b)      // should return a new Vector([4, 6, 8])
  * a.subtract(b) // should return a new Vector([-2, -2, -2])
@@ -13,37 +11,14 @@
  * a.norm()      // should return sqrt(1^2 + 2^2 + 3^2) = sqrt(14)
  * a.add(c)      // throws an error
  *
- * If you try to add, subtract, or dot two vectors with different lengths, you must throw an error!
+ * If you try to add, subtract, or dot two vectors with different lengths,
+ * you must throw an error!
  *
  * Also provide:
  * - a toString method, so that a.toString() === '(1,2,3)'
- * - an equals method, to check that two vectors that have the same components are equal
+ * - an equals method, to check that two vectors with the same components are equal
  */
 
-var Vector = function(components) {
-
-};
-
-Vector.prototype.add = function(other) {
-
-};
-
-Vector.prototype.subtract = function(other) {
-
-};
-
-Vector.prototype.dot = function(other) {
-
-};
-
-Vector.prototype.norm = function() {
-
-};
-
-Vector.prototype.toString = function() {
-
-};
-
-Vector.prototype.equals = function(other) {
-
-};
+function Vector(components) {
+  // your code here
+}
--- vector-class/solution.js
+++ vector-class/solution.js
@@ -1,31 +1,38 @@
-var Vector = function(components) {
-  this.components = components;
+function Vector(components) {
+  this.components = components.slice();
+  this.length = components.length;
+}
+
+Vector.prototype._checkLength = function (other) {
+  if (this.length !== other.length) {
+    throw new Error('Vectors must be of the same length');
+  }
 };
 
-Vector.prototype.add = function(other) {
-  if (this.components.length !== other.components.length) throw new Error('Vectors must have the same length');
+Vector.prototype.add = function (other) {
+  this._checkLength(other);
   return new Vector(this.components.map((v, i) => v + other.components[i]));
 };
 
-Vector.prototype.subtract = function(other) {
-  if (this.components.length !== other.components.length) throw new Error('Vectors must have the same length');
+Vector.prototype.subtract = function (other) {
+  this._checkLength(other);
   return new Vector(this.components.map((v, i) => v - other.components[i]));
 };
 
-Vector.prototype.dot = function(other) {
-  if (this.components.length !== other.components.length) throw new Error('Vectors must have the same length');
+Vector.prototype.dot = function (other) {
+  this._checkLength(other);
   return this.components.reduce((sum, v, i) => sum + v * other.components[i], 0);
 };
 
-Vector.prototype.norm = function() {
+Vector.prototype.norm = function () {
   return Math.sqrt(this.components.reduce((sum, v) => sum + v * v, 0));
 };
 
-Vector.prototype.toString = function() {
+Vector.prototype.toString = function () {
   return '(' + this.components.join(',') + ')';
 };
 
-Vector.prototype.equals = function(other) {
-  return this.components.length === other.components.length &&
-    this.components.every((v, i) => v === other.components[i]);
+Vector.prototype.equals = function (other) {
+  if (this.length !== other.length) return false;
+  return this.components.every((v, i) => v === other.components[i]);
 };
