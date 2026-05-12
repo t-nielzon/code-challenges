df5173571d7ecd6188d982823fbb49a91a2a8516
t-nielzon <t-nielzon@users.noreply.github.com>
2026-05-12 21:43:05 +0800

    Add go solution: crack-the-pin

--- crack-the-pin/problem.go
+++ crack-the-pin/problem.go
@@ -1,11 +1,10 @@
-package kata
-
 /*
 Given is a md5 hash of a five digits long PIN. It is given as string.
 
 Your task is to return the cracked PIN as string.
 */
+package kata
 
 func Crack(hash string) string {
-	return ""
+
 }
