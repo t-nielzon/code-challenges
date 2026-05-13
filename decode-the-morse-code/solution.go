2079308315f35d82d8f9752c251cd3681ced70d3
t-nielzon <t-nielzon@users.noreply.github.com>
2026-05-13 15:17:45 +0800

    Add go solution: decode-the-morse-code

--- decode-the-morse-code/solution.go
+++ decode-the-morse-code/solution.go
@@ -5,14 +5,17 @@ import "strings"
 func DecodeMorse(morseCode string) string {
 	morseCode = strings.TrimSpace(morseCode)
 	words := strings.Split(morseCode, "   ")
-	decoded := make([]string, len(words))
-	for i, word := range words {
-		letters := strings.Split(strings.TrimSpace(word), " ")
+	decodedWords := make([]string, 0, len(words))
+	for _, word := range words {
+		letters := strings.Split(word, " ")
+		var sb strings.Builder
 		for _, letter := range letters {
-			if letter != "" {
-				decoded[i] += MORSE_CODE[letter]
+			if letter == "" {
+				continue
 			}
+			sb.WriteString(MORSE_CODE[letter])
 		}
+		decodedWords = append(decodedWords, sb.String())
 	}
-	return strings.Join(decoded, " ")
+	return strings.Join(decodedWords, " ")
 }
