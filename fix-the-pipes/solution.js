b0dc3d47a6ccf78efd22bd036d6a2628e1784d18
t-nielzon <t-nielzon@users.noreply.github.com>
2026-05-16 11:09:07 +0800

    Add javascript solution: fix-the-pipes

--- fix-the-pipes/solution.js
+++ fix-the-pipes/solution.js
@@ -1,39 +1,40 @@
 function fixThePipes(map, start, end) {
-  const isStr = typeof map[0] === 'string';
-  const grid = map.map(r => isStr ? r.split('') : r.slice());
-  const rows = grid.length;
-  const cols = grid[0].length;
+  const rows = map.length;
+  const cols = map[0].length;
+  const grid = map.map(r => r.split(''));
 
-  let enterRow = start;
+  const colsInfo = [];
   for (let c = 0; c < cols; c++) {
-    const xRows = [];
+    let top = -1, bot = -1;
     for (let r = 0; r < rows; r++) {
-      if (grid[r][c] === 'x') xRows.push(r);
-    }
-    if (xRows.length === 0) continue;
-
-    const minR = xRows[0];
-    const maxR = xRows[xRows.length - 1];
-
-    let exitRow;
-    if (c === cols - 1) {
-      exitRow = end;
-    } else {
-      exitRow = (enterRow === minR) ? maxR : minR;
-    }
-
-    if (minR === maxR) {
-      grid[minR][c] = '━';
-    } else {
-      grid[minR][c] = (minR === enterRow) ? '┓' : '┏';
-      grid[maxR][c] = (maxR === enterRow) ? '┛' : '┗';
-      for (let r = minR + 1; r < maxR; r++) {
-        grid[r][c] = '┃';
+      if (grid[r][c] === 'x') {
+        if (top === -1) top = r;
+        bot = r;
       }
     }
-
-    enterRow = exitRow;
+    colsInfo.push({ top, bot });
   }
 
-  return isStr ? grid.map(r => r.join('')) : grid;
+  let entry = start;
+  for (let c = 0; c < cols; c++) {
+    const { top, bot } = colsInfo[c];
+    let exit;
+    if (top === bot) {
+      exit = top;
+      grid[top][c] = '━';
+    } else if (entry === top) {
+      exit = bot;
+      grid[top][c] = '┓';
+      for (let r = top + 1; r < bot; r++) grid[r][c] = '┃';
+      grid[bot][c] = '┗';
+    } else {
+      exit = top;
+      grid[bot][c] = '┛';
+      for (let r = top + 1; r < bot; r++) grid[r][c] = '┃';
+      grid[top][c] = '┏';
+    }
+    entry = exit;
+  }
+
+  return grid.map(r => r.join(''));
 }
