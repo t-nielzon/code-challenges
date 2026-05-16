ac26d4401bb4b5ed73de0f4a32ca91772e849ae5
t-nielzon <t-nielzon@users.noreply.github.com>
2026-05-16 11:09:24 +0800

    Add javascript solution: color-choice

--- color-choice/solution.js
+++ color-choice/solution.js
@@ -1,8 +1,8 @@
-function colorChoice(m, n) {
-  const M = BigInt(m);
-  const N = BigInt(n);
-  if (M === 1n) return 0;
+function chooseColour(m, n) {
+  if (m === 1) return 0;
   let c = 1n;
+  const N = BigInt(n);
+  const M = BigInt(m);
   for (let x = 1n; x <= N; x++) {
     c = c * (N - x + 1n) / x;
     if (c === M) return Number(x);
