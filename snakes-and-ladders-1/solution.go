18a47650f1d597cb7bb677755deafa7e710a1314
t-nielzon <t-nielzon@users.noreply.github.com>
2026-05-18 20:05:18 +0800

    Add go solution: snakes-and-ladders-1

--- snakes-and-ladders-1/solution.go
+++ snakes-and-ladders-1/solution.go
@@ -3,44 +3,23 @@ package kata
 import "fmt"
 
 type SnakesLadders struct {
-	players       [2]int
+	positions     [2]int
 	currentPlayer int
 	gameOver      bool
 }
 
+func NewSnakesLadders() *SnakesLadders {
+	return &SnakesLadders{currentPlayer: 0}
+}
+
 var ladders = map[int]int{
-	2:  38,
-	7:  14,
-	8:  31,
-	15: 26,
-	21: 42,
-	28: 84,
-	36: 44,
-	51: 67,
-	71: 91,
-	78: 98,
-	87: 94,
+	2: 38, 7: 14, 8: 31, 15: 26, 21: 42, 28: 84,
+	36: 44, 51: 67, 71: 91, 78: 98, 87: 94,
 }
 
 var snakes = map[int]int{
-	16:  6,
-	46:  25,
-	49:  11,
-	62:  19,
-	64:  60,
-	74:  53,
-	89:  68,
-	92:  88,
-	95:  75,
-	99:  80,
-}
-
-func NewSnakesLadders() *SnakesLadders {
-	return &SnakesLadders{
-		players:       [2]int{0, 0},
-		currentPlayer: 0,
-		gameOver:      false,
-	}
+	16: 6, 46: 25, 49: 11, 62: 19, 64: 60,
+	74: 53, 89: 68, 92: 88, 95: 75, 99: 80,
 }
 
 func (s *SnakesLadders) Play(die1, die2 int) string {
@@ -48,22 +27,19 @@ func (s *SnakesLadders) Play(die1, die2 int) string {
 		return "Game over!"
 	}
 
-	move := die1 + die2
-	pos := s.players[s.currentPlayer] + move
-
+	p := s.currentPlayer
+	pos := s.positions[p] + die1 + die2
 	if pos > 100 {
-		pos = 100 - (pos - 100)
+		pos = 200 - pos
 	}
-
-	if dest, ok := ladders[pos]; ok {
-		pos = dest
-	} else if dest, ok := snakes[pos]; ok {
-		pos = dest
+	if v, ok := ladders[pos]; ok {
+		pos = v
+	} else if v, ok := snakes[pos]; ok {
+		pos = v
 	}
+	s.positions[p] = pos
 
-	s.players[s.currentPlayer] = pos
-
-	playerNum := s.currentPlayer + 1
+	playerNum := p + 1
 
 	if pos == 100 {
 		s.gameOver = true
