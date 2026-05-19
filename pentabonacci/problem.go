2a3ceeea9f16d7f35323dce01c8ff7531b317c88
t-nielzon <t-nielzon@users.noreply.github.com>
2026-05-19 11:35:43 +0800

    Add go solution: pentabonacci

--- pentabonacci/problem.go
+++ pentabonacci/problem.go
@@ -1,6 +1,4 @@
 /*
-Pentabonacci
-
 We have the following sequence:
 
 f(0) = 0
@@ -10,12 +8,19 @@ f(3) = 2
 f(4) = 4
 f(n) = f(n-1) + f(n-2) + f(n-3) + f(n-4) + f(n-5)
 
-Your task is to give the number of total values for the odd terms of the
-sequence up to the n-th term (included).
+Your task is to give the number of total values for the odd terms of the sequence
+up to the n-th term (included). (The number n (of n-th term) will be given as a
+nonnegative integer)
 
-The values 1 (one) is the only that is duplicated in the sequence and should
+The value 1 (one) is the only one that is duplicated in the sequence and should
 be counted only once.
+
+E.g.
+count_odd_pentaFib(5)  -----> 1  // terms up to 5: 0, 1, 1, 2, 4, 8
+count_odd_pentaFib(10) -----> 3
+count_odd_pentaFib(15) -----> 5
 */
+
 package kata
 
 func CountOddPentaFib(n int) int {
--- pentabonacci/solution.go
+++ pentabonacci/solution.go
@@ -1,41 +1,35 @@
 package kata
 
+import "math/big"
+
 func CountOddPentaFib(n int) int {
-	if n < 0 {
+	if n <= 0 {
 		return 0
 	}
-
-	// generate pentabonacci terms up to index n
-	f := make([]uint64, max(5, n+1))
-	f[0] = 0
-	f[1] = 1
-	f[2] = 1
-	f[3] = 2
-	f[4] = 4
-	for i := 5; i <= n; i++ {
-		f[i] = f[i-1] + f[i-2] + f[i-3] + f[i-4] + f[i-5]
+	if n <= 2 {
+		return 1
 	}
 
-	count := 0
-	seenOne := false
-	for i := 0; i <= n; i++ {
-		if f[i]%2 == 1 {
-			if f[i] == 1 {
-				if !seenOne {
-					seenOne = true
-					count++
-				}
-			} else {
-				count++
-			}
+	a := big.NewInt(0)
+	b := big.NewInt(1)
+	c := big.NewInt(1)
+	d := big.NewInt(2)
+	e := big.NewInt(4)
+
+	count := 1
+	tmp := new(big.Int)
+
+	for i := 5; i <= n; i++ {
+		tmp.Add(a, b)
+		tmp.Add(tmp, c)
+		tmp.Add(tmp, d)
+		tmp.Add(tmp, e)
+		a, b, c, d, e = b, c, d, e, tmp
+		tmp = new(big.Int)
+		if e.Bit(0) == 1 {
+			count++
 		}
 	}
-	return count
-}
 
-func max(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
+	return count
 }
