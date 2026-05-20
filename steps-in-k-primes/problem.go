2259ecfae3917b9496e7aa1bca41c07802027251
t-nielzon <t-nielzon@users.noreply.github.com>
2026-05-20 10:03:05 +0800

    Add go solution: steps-in-k-primes

--- steps-in-k-primes/problem.go
+++ steps-in-k-primes/problem.go
@@ -10,22 +10,27 @@ k = 2 -> 4, 6, 9, 10, 14, 15, 21, 22, …
 k = 3 -> 8, 12, 18, 20, 27, 28, 30, …
 k = 5 -> 32, 48, 72, 80, 108, 112, …
 
+The k-prime numbers are not regularly spaced. For example: between 2 and 50 we have the following 2-primes:
+[4, 6, 9, 10, 14, 15, 21, 22, 25, 26, 33, 34, 35, 38, 39, 46, 49]
+
+The steps between two k-primes of this list are 2, 3, 1, 4, 1, 6, 1, 3, 1, 7, 1, 1, 3, 1, 7, 3.
+
+Task:
 Write a function KprimesStep(k, step, start, nd) with parameters:
 - k (integer > 0) which indicates the type of k-primes we are looking for,
 - step (integer > 0) which indicates the step we want to find between two k-primes,
 - start (integer >= 0) which gives the start of the search (start inclusive),
 - nd (integer >= start) which gives the end of the search (nd inclusive)
 
-This function should return an array of all the pairs of k-prime numbers spaced with
-a step of step between the limits start, nd. This array can be empty.
+This function should return an array of all the pairs (or tuples) of k-prime numbers spaced with a step of step
+between the limits start, nd. This array can be empty.
 
 Examples:
-KprimesStep(2, 2, 0, 50)             => [[4, 6], [33, 35]]
+KprimesStep(2, 2, 0, 50) => [[4, 6], [33, 35]]
 KprimesStep(6, 14, 2113665, 2113889) => [[2113722, 2113736]]
-KprimesStep(2, 10, 0, 50)            => [[4, 14], [15, 25], [25, 35], [39, 49]]
-KprimesStep(5, 20, 0, 50)            => [][]uint64{}
+KprimesStep(2, 10, 0, 50) => [[4, 14], [15, 25], [25, 35], [39, 49]]
+KprimesStep(5, 20, 0, 50) => []
 */
 
-func KprimesStep(k, step, start, nd uint64) [][]uint64 {
-
+func KprimesStep(k, step, start, nd int) [][]int {
 }
--- steps-in-k-primes/solution.go
+++ steps-in-k-primes/solution.go
@@ -1,32 +1,28 @@
 package kata
 
-func KprimesStep(k, step, start, nd uint64) [][]uint64 {
-	result := [][]uint64{}
-	countFactors := func(n uint64) uint64 {
-		var c uint64
-		for p := uint64(2); p*p <= n; p++ {
-			for n%p == 0 {
-				n /= p
-				c++
-			}
+func countPrimeFactors(n int) int {
+	count := 0
+	for d := 2; d*d <= n; d++ {
+		for n%d == 0 {
+			count++
+			n /= d
 		}
-		if n > 1 {
-			c++
-		}
-		return c
 	}
-	var prev uint64
-	hasPrev := false
+	if n > 1 {
+		count++
+	}
+	return count
+}
+
+func KprimesStep(k, step, start, nd int) [][]int {
+	result := [][]int{}
+	var prev = -1
 	for n := start; n <= nd; n++ {
-		if n < 2 {
-			continue
-		}
-		if countFactors(n) == k {
-			if hasPrev && n-prev == step {
-				result = append(result, []uint64{prev, n})
+		if countPrimeFactors(n) == k {
+			if prev != -1 && n-prev == step {
+				result = append(result, []int{prev, n})
 			}
 			prev = n
-			hasPrev = true
 		}
 	}
 	return result
