7e42079006981f9eb138bc7909d1538592d76145
t-nielzon <t-nielzon@users.noreply.github.com>
2026-05-21 15:20:37 +0800

    Add javascript solution: integers-recreation-two

--- integers-recreation-two/solution.js
+++ integers-recreation-two/solution.js
@@ -1,8 +1,11 @@
 function prod2sum(a, b, c, d) {
+  // brahmagupta-fibonacci identity:
+  // (a² + b²)(c² + d²) = (ac - bd)² + (ad + bc)² = (ac + bd)² + (ad - bc)²
   const pairs = [
     [Math.abs(a * c - b * d), Math.abs(a * d + b * c)],
-    [Math.abs(a * d - b * c), Math.abs(a * c + b * d)]
+    [Math.abs(a * c + b * d), Math.abs(a * d - b * c)]
   ].map(p => p.sort((x, y) => x - y));
+
   const seen = new Set();
   const result = [];
   for (const p of pairs) {
@@ -12,5 +15,5 @@ function prod2sum(a, b, c, d) {
       result.push(p);
     }
   }
-  return result.sort((x, y) => x[0] - y[0]);
+  return result.sort((x, y) => x[0] - y[0] || x[1] - y[1]);
 }
