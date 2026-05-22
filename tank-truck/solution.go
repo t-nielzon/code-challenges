6a03de17d2bba5d940bf2f7316f815ef7ade93ff
t-nielzon <t-nielzon@users.noreply.github.com>
2026-05-22 16:41:39 +0800

    Add go solution: tank-truck

--- tank-truck/solution.go
+++ tank-truck/solution.go
@@ -2,10 +2,10 @@ package kata
 
 import "math"
 
-func tankvol(h, d, vt int) int {
+func Tankvol(h, d, vt int) int {
 	r := float64(d) / 2.0
-	hf := float64(h)
-	length := float64(vt) / (math.Pi * r * r)
-	area := r*r*math.Acos((r-hf)/r) - (r-hf)*math.Sqrt(2*r*hf-hf*hf)
-	return int(math.Floor(area * length))
+	hh := float64(h)
+	area := r*r*math.Acos((r-hh)/r) - (r-hh)*math.Sqrt(2*r*hh-hh*hh)
+	ratio := area / (math.Pi * r * r)
+	return int(math.Floor(ratio * float64(vt)))
 }
