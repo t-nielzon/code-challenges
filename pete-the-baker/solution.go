dad1b02851dc6b0d207544c20ce23e5b64cf43ee
t-nielzon <t-nielzon@users.noreply.github.com>
2026-05-23 15:44:52 +0800

    Add go solution: pete-the-baker

--- pete-the-baker/solution.go
+++ pete-the-baker/solution.go
@@ -2,11 +2,14 @@ package kata
 
 func Cakes(recipe, available map[string]int) int {
 	min := -1
-	for ingredient, needed := range recipe {
-		have := available[ingredient]
-		count := have / needed
-		if min == -1 || count < min {
-			min = count
+	for ingredient, amount := range recipe {
+		have, ok := available[ingredient]
+		if !ok || amount <= 0 {
+			return 0
+		}
+		n := have / amount
+		if min == -1 || n < min {
+			min = n
 		}
 	}
 	if min == -1 {
