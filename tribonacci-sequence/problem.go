559088a2847ddf406cf9659ee0d42bcce930abf8
t-nielzon <t-nielzon@users.noreply.github.com>
2026-05-28 09:22:50 +0800

    Add go solution: tribonacci-sequence

--- tribonacci-sequence/problem.go
+++ tribonacci-sequence/problem.go
@@ -1,28 +1,26 @@
-/*
-Well met with Fibonacci bigger brother, AKA Tribonacci.
-
-As the name may already reveal, it works basically like a Fibonacci, but summing
-the last 3 (instead of 2) numbers of the sequence to generate the next.
-
-So, if we are to start our Tribonacci sequence with [1, 1, 1] as a starting
-input (AKA signature), we have this sequence:
-
-[1, 1, 1, 3, 5, 9, 17, 31, ...]
-
-But what if we started with [0, 0, 1] as a signature?
-
-[0, 0, 1, 1, 2, 4, 7, 13, 24, ...]
-
-You need to create a fibonacci function that given a signature array/list,
-returns the first n elements - signature included - of the so seeded sequence.
-
-Signature will always contain 3 numbers; n will always be a non-negative number;
-if n == 0, then return an empty array and be ready for anything else which is
-not clearly specified ;)
-*/
+// Well met with Fibonacci bigger brother, AKA Tribonacci.
+//
+// As the name may already reveal, it works basically like a Fibonacci, but
+// summing the last 3 (instead of 2) numbers of the sequence to generate the
+// next.
+//
+// So, if we are to start our Tribonacci sequence with [1, 1, 1] as a starting
+// input (AKA signature), we have this sequence:
+//
+//	[1, 1, 1, 3, 5, 9, 17, 31, ...]
+//
+// But what if we started with [0, 0, 1] as a signature? We would get:
+//
+//	[0, 0, 1, 1, 2, 4, 7, 13, 24, ...]
+//
+// You need to create a function that given a signature array/list, returns the
+// first n elements - signature included - of the so seeded sequence.
+//
+// Signature will always contain 3 numbers; n will always be a non-negative
+// number; if n == 0, then return an empty array.
 
 package kata
 
-func Tribonacci(signature [3]float64, n int) []float64 {
-
+func Tribonacci(signature [3]int, n int) []int {
+	return nil
 }
--- tribonacci-sequence/solution.go
+++ tribonacci-sequence/solution.go
@@ -1,15 +1,13 @@
 package kata
 
-func Tribonacci(signature [3]float64, n int) []float64 {
-	if n == 0 {
-		return []float64{}
-	}
-	result := make([]float64, n)
-	for i := 0; i < n && i < 3; i++ {
-		result[i] = signature[i]
-	}
-	for i := 3; i < n; i++ {
-		result[i] = result[i-1] + result[i-2] + result[i-3]
+func Tribonacci(signature [3]int, n int) []int {
+	result := make([]int, 0, n)
+	for i := 0; i < n; i++ {
+		if i < 3 {
+			result = append(result, signature[i])
+		} else {
+			result = append(result, result[i-1]+result[i-2]+result[i-3])
+		}
 	}
 	return result
 }
