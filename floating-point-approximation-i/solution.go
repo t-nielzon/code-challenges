586f63ec2ff8fdc33d30e019d6f7f134381f87ab
t-nielzon <t-nielzon@users.noreply.github.com>
2026-06-04 08:49:37 +0800

    Add go solution: floating-point-approximation-i

--- floating-point-approximation-i/solution.go
+++ floating-point-approximation-i/solution.go
@@ -2,6 +2,8 @@ package kata
 
 import "math"
 
+// rationalize sqrt(1+x)-1 by multiplying by (sqrt(1+x)+1) to avoid the
+// catastrophic cancellation of subtracting two nearly equal numbers
 func F(x float64) float64 {
 	return x / (math.Sqrt(1+x) + 1)
 }
