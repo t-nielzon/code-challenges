f81f414b0d7d80202345027b36f8598dd7227ef7
t-nielzon <t-nielzon@users.noreply.github.com>
2026-06-04 08:49:18 +0800

    Add go solution: perimeter-of-squares-in-a-rectangle

--- perimeter-of-squares-in-a-rectangle/solution.go
+++ perimeter-of-squares-in-a-rectangle/solution.go
@@ -2,14 +2,22 @@ package kata
 
 import "math/big"
 
-func Perimeter(n uint64) *big.Int {
-	a := big.NewInt(0)
-	b := big.NewInt(1)
+// the side lengths are fibonacci numbers, so the total perimeter is
+// 4 times the sum of the first n+1 fibonacci numbers. that sum equals
+// fib(n+2) - 1, giving 4 * (fib(n+2) - 1). big.Int is used since n can
+// be large enough to overflow native integer types.
+func Perimeter(n *big.Int) *big.Int {
+	count := new(big.Int).Add(n, big.NewInt(2)) // iterate up to fib(n+2)
+
+	a := big.NewInt(0) // fib(0)
+	b := big.NewInt(1) // fib(1)
 	sum := big.NewInt(0)
-	for i := uint64(0); i <= n; i++ {
-		sum.Add(sum, b)
-		a.Add(a, b)
-		a, b = b, a
+
+	for i := big.NewInt(0); i.Cmp(count) < 0; i.Add(i, big.NewInt(1)) {
+		sum.Add(sum, a)
+		a, b = b, new(big.Int).Add(a, b)
 	}
+
+	// sum now holds fib(0)+...+fib(n+1) = fib(n+2)-1
 	return sum.Mul(sum, big.NewInt(4))
 }
