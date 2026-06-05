003b2f78c10f9ea94fb4f7d543b753e86e2ddd93
t-nielzon <t-nielzon@users.noreply.github.com>
2026-06-05 12:41:41 +0800

    Add javascript solution: thinking-and-testing-welcome-to-take-the-bus

---
+++ thinking-and-testing-welcome-to-take-the-bus/problem.js
@@ -0,0 +1,16 @@
+/*
+ * Thinking & Testing : Welcome to take the bus
+ *
+ * No Story
+ * No Description
+ *
+ * Only by Thinking and Testing
+ * Look at result of testcase, guess the code!
+ *
+ * Series 11 of the "Thinking and Testing" katas.
+ * Deduce the transformation from the visible test results and implement it.
+ */
+
+function testit(arr) {
+  // your code here
+}
---
+++ thinking-and-testing-welcome-to-take-the-bus/solution.js
@@ -0,0 +1,3 @@
+function testit(arr) {
+  return arr.slice().reverse();
+}
