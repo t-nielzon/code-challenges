849c7990c16447269d49e490fe1301a40f65c26f
t-nielzon <t-nielzon@users.noreply.github.com>
2026-06-06 18:29:27 +0800

    Add javascript solution: tribonacci-sequence

--- tribonacci-sequence/problem.js
+++ tribonacci-sequence/problem.js
@@ -1,29 +1,21 @@
 /*
  * Tribonacci Sequence
- * Difficulty: 6 kyu
  *
- * Well met with Fibonacci bigger brother, AKA Tribonacci.
+ * It works basically like a Fibonacci, but summing the last 3 (instead of 2)
+ * numbers of the sequence to generate the next.
  *
- * As the name may already reveal, it works basically like a Fibonacci, but
- * summing the last 3 (instead of 2) numbers of the sequence to generate the
- * next.
- *
- * So, if we are to start our Tribonacci sequence with [1, 1, 1] as a starting
- * input (AKA signature), we have this sequence:
- *
+ * Starting with the signature [1, 1, 1]:
  *   [1, 1, 1, 3, 5, 9, 17, 31, ...]
  *
- * But what if we started with [0, 0, 1] as a signature? We would get:
- *
+ * Starting with the signature [0, 0, 1]:
  *   [0, 0, 1, 1, 2, 4, 7, 13, 24, ...]
  *
- * You need to create a function that given a signature array/list, returns
- * the first n elements - signature included - of the so seeded sequence.
+ * Given a signature array (always 3 numbers), return the first n elements -
+ * signature included - of the so seeded sequence.
  *
- * Signature will always contain 3 numbers; n will always be a non-negative
- * number; if n == 0, then return an empty array.
+ * n will always be a non-negative number; if n == 0, return an empty array.
  */
 
 function tribonacci(signature, n) {
-
+  // your code here
 }
--- tribonacci-sequence/solution.js
+++ tribonacci-sequence/solution.js
@@ -1,8 +1,9 @@
 function tribonacci(signature, n) {
-  if (n === 0) return [];
   const result = signature.slice(0, n);
+
   for (let i = 3; i < n; i++) {
     result.push(result[i - 1] + result[i - 2] + result[i - 3]);
   }
+
   return result;
 }
