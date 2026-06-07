24812b7a89b4237a88ff5fbd7924e111a653cc11
t-nielzon <t-nielzon@users.noreply.github.com>
2026-06-07 21:47:32 +0800

    Add javascript solution: seven-ate-nine

--- seven-ate-nine/problem.js
+++ seven-ate-nine/problem.js
@@ -1,4 +1,6 @@
 /*
+ * Seven "ate" nine!
+ *
  * Seven is a hungry number and its favourite food is number 9. Whenever it spots 9
  * through the hoops of 8, it eats it! Well, not anymore, because you are
  * going to help the 9 by locating that particular sequence (7,8,9) in an array of digits
@@ -6,6 +8,6 @@
  * (If 9 is not in danger, just return the same array)
  */
 
-function sevenAteNine(arr) {
-
+function sevenAte9(arr) {
+  // your code here
 }
--- seven-ate-nine/solution.js
+++ seven-ate-nine/solution.js
@@ -1,7 +1,8 @@
-function sevenAteNine(arr) {
+function sevenAte9(arr) {
   const result = [];
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] === 7 && arr[i + 1] === 8 && arr[i + 2] === 9) {
+      // skip the 7 here; it will be placed after the 9
       result.push(8, 9, 7);
       i += 2;
     } else {
