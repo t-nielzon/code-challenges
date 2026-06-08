7e88b111dfeff21125cf5b3032abbe8ac0db0e19
t-nielzon <t-nielzon@users.noreply.github.com>
2026-06-08 19:30:25 +0800

    Add go solution: playing-with-passphrases

--- playing-with-passphrases/solution.go
+++ playing-with-passphrases/solution.go
@@ -1,30 +1,27 @@
 package kata
 
+import "strings"
+
 func PlayPass(s string, n int) string {
 	runes := []rune(s)
 	out := make([]rune, len(runes))
-	for i, r := range runes {
+
+	for i, c := range runes {
 		switch {
-		case r >= 'A' && r <= 'Z':
-			shifted := rune((int(r-'A')+n)%26) + 'A'
-			if i%2 == 1 {
-				shifted = shifted - 'A' + 'a'
-			}
-			out[i] = shifted
-		case r >= 'a' && r <= 'z':
-			shifted := rune((int(r-'a')+n)%26) + 'a'
-			if i%2 == 0 {
-				shifted = shifted - 'a' + 'A'
-			}
-			out[i] = shifted
-		case r >= '0' && r <= '9':
-			out[i] = '9' - (r - '0')
-		default:
-			out[i] = r
+		case c >= 'A' && c <= 'Z':
+			c = 'A' + (c-'A'+rune(n))%26
+		case c >= '0' && c <= '9':
+			c = '0' + (9 - (c - '0'))
 		}
+
+		if i%2 == 1 {
+			c = []rune(strings.ToLower(string(c)))[0]
+		} else {
+			c = []rune(strings.ToUpper(string(c)))[0]
+		}
+
+		out[len(runes)-1-i] = c
 	}
-	for i, j := 0, len(out)-1; i < j; i, j = i+1, j-1 {
-		out[i], out[j] = out[j], out[i]
-	}
+
 	return string(out)
 }
