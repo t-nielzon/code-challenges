ca79d5e4f542eea159d97fe5ac8e6a78db9f2873
t-nielzon <t-nielzon@users.noreply.github.com>
2026-06-09 12:10:11 +0800

    Add go solution: snakes-and-ladders-1

--- snakes-and-ladders-1/problem.go
+++ snakes-and-ladders-1/problem.go
@@ -1,50 +1,55 @@
-/*
-## Introduction
-
-Snakes and Ladders is an ancient Indian board game regarded today as a worldwide classic.
-It is played by two or more players on a game board with numbered, gridded squares.
-A number of "ladders" and "snakes" are pictured on the board, each connecting two specific squares.
-
-## Task
-
-Your task is to create a simple class called `SnakesLadders`. The test cases will call the method
-`play(die1, die2)` independently of the state of the game or the player turn. The arguments
-`die1` and `die2` are the dice thrown in a turn and are both integers between 1 and 6.
-The player will make a number of steps equal to the sum of `die1` and `die2`,
-moving one square per step.
-
-## Rules
-
-1. There are two players, and both start off the board on square 0.
-2. Player 1 starts and alternates with player 2.
-3. You follow the numbers up the board in order from 1 to 100.
-4. If the values of both dice are the same, that player will have another turn after the current turn ends.
-5. Climb up ladders.
-6. Slide down snakes.
-7. Land exactly on the last square to win. If you roll too high, your player "bounces" off square 100.
-8. If the player rolls a double and lands on the finish square (100), the player wins
-   and does not take another turn.
-
-## Returns
-
-Return `"Player n Wins!"` where `n` is the winning player.
-Return `"Game over!"` if a move is attempted after any player has won.
-Otherwise, return `"Player n is on square x"`.
-*/
+// ## Introduction
+//
+// Snakes and Ladders is an ancient Indian board game regarded today as a
+// worldwide classic. It is played by two or more players on a game board with
+// numbered, gridded squares. A number of "ladders" and "snakes" are pictured on
+// the board, each connecting two specific squares.
+//
+// ## Task
+//
+// Your task is to create a simple class called `SnakesLadders`. The test cases
+// will call the method `play(die1, die2)` independently of the state of the game
+// or the player turn. The arguments `die1` and `die2` are the dice thrown in a
+// turn and are both integers between 1 and 6. The player will make a number of
+// steps equal to the sum of `die1` and `die2`, moving one square per step.
+//
+// ## Rules
+//
+// 1. There are two players, and both start off the board on square 0.
+// 2. Player 1 starts and alternates with player 2.
+// 3. You follow the numbers up the board in order from 1 to 100.
+// 4. If the values of both dice are the same, that player will have another turn
+//    after the current turn ends.
+// 5. Climb up ladders. If you land exactly on a square that shows the bottom of a
+//    ladder, you move all the way up to the top (even if you roll a double).
+// 6. Slide down snakes. If you land exactly on the top of a snake, you slide all
+//    the way down to the bottom (even if you roll a double).
+// 7. Land exactly on the last square to win. If you roll too high, your player
+//    "bounces" off square 100 and continues moving backward for the remaining
+//    steps. You can only win by rolling the exact number needed.
+// 8. If the player rolls a double and lands on the finish square (100) after
+//    taking all steps, the player wins and does not take another turn.
+//
+// ## Returns
+//
+// Return "Player n Wins!" where n is the winning player who has landed on
+// square 100 after taking all steps in their turn.
+// Return "Game over!" if a move is attempted after any player has won.
+// Otherwise, return "Player n is on square x", where n is the current player and
+// x is the square they are currently on.
 
 package kata
 
 type SnakesLadders struct {
-	positions     [2]int
-	currentPlayer int
-	gameOver      bool
+	positions [2]int
+	turn      int
+	gameOver  bool
 }
 
 func NewSnakesLadders() *SnakesLadders {
-	return &SnakesLadders{currentPlayer: 0}
+	return &SnakesLadders{}
 }
 
-func (s *SnakesLadders) Play(die1, die2 int) string {
-	// TODO: implement
+func (s *SnakesLadders) play(die1, die2 int) string {
 	return ""
 }
--- snakes-and-ladders-1/solution.go
+++ snakes-and-ladders-1/solution.go
@@ -3,54 +3,54 @@ package kata
 import "fmt"
 
 type SnakesLadders struct {
-	positions     [2]int
-	currentPlayer int
-	gameOver      bool
+	positions [2]int
+	turn      int
+	gameOver  bool
 }
 
 func NewSnakesLadders() *SnakesLadders {
-	return &SnakesLadders{currentPlayer: 0}
+	return &SnakesLadders{}
 }
 
-var ladders = map[int]int{
+// jumps maps the bottom of a ladder (or top of a snake) to its destination square.
+var jumps = map[int]int{
+	// ladders
 	2: 38, 7: 14, 8: 31, 15: 26, 21: 42, 28: 84,
 	36: 44, 51: 67, 71: 91, 78: 98, 87: 94,
+	// snakes
+	16: 6, 46: 25, 49: 11, 62: 19, 64: 60, 74: 53,
+	89: 68, 92: 88, 95: 75, 99: 80,
 }
 
-var snakes = map[int]int{
-	16: 6, 46: 25, 49: 11, 62: 19, 64: 60,
-	74: 53, 89: 68, 92: 88, 95: 75, 99: 80,
-}
-
-func (s *SnakesLadders) Play(die1, die2 int) string {
+func (s *SnakesLadders) play(die1, die2 int) string {
 	if s.gameOver {
 		return "Game over!"
 	}
 
-	p := s.currentPlayer
-	pos := s.positions[p] + die1 + die2
+	player := s.turn
+	pos := s.positions[player] + die1 + die2
+
+	// bounce backward off the final square when overshooting
 	if pos > 100 {
 		pos = 200 - pos
 	}
-	if v, ok := ladders[pos]; ok {
-		pos = v
-	} else if v, ok := snakes[pos]; ok {
-		pos = v
-	}
-	s.positions[p] = pos
 
-	playerNum := p + 1
+	// landing exactly on the foot of a ladder or head of a snake relocates the piece
+	if dest, ok := jumps[pos]; ok {
+		pos = dest
+	}
+
+	s.positions[player] = pos
 
 	if pos == 100 {
 		s.gameOver = true
-		return fmt.Sprintf("Player %d Wins!", playerNum)
+		return fmt.Sprintf("Player %d Wins!", player+1)
 	}
 
-	result := fmt.Sprintf("Player %d is on square %d", playerNum, pos)
-
+	// doubles grant the same player another turn; otherwise pass to the other player
 	if die1 != die2 {
-		s.currentPlayer = 1 - s.currentPlayer
+		s.turn = 1 - s.turn
 	}
 
-	return result
+	return fmt.Sprintf("Player %d is on square %d", player+1, pos)
 }
