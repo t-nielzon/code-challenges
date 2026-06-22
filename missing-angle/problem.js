e6d5037ff1b1a2de454e8e23c03da55c9d808838
t-nielzon <t-nielzon@users.noreply.github.com>
2026-06-22 11:02:04 +0800

    Add javascript solution: missing-angle

--- missing-angle/problem.js
+++ missing-angle/problem.js
@@ -11,11 +11,12 @@
  *   |_______\
  *      a
  *
- * Your challenge is to write a function that calculates the angle θ in degrees.
- * You will be given three arguments representing each side: o, h and a.
- * One of the arguments equals zero. Use the length of the two other sides to calculate θ.
+ * Your challenge is to write a function that calculates the angle `θ` in
+ * degrees. You will be given three arguments representing each side: `o`, `h`
+ * and `a`. One of the arguments equals zero. Use the length of the two other
+ * sides to calculate `θ`.
  */
 
-function missingAngle(h, o, a) {
+function missingAngle(h, a, o) {
 
 }
--- missing-angle/solution.js
+++ missing-angle/solution.js
@@ -1,5 +1,7 @@
-function missingAngle(h, o, a) {
-  if (h === 0) return Math.atan(o / a) * 180 / Math.PI;
-  if (o === 0) return Math.acos(a / h) * 180 / Math.PI;
-  return Math.asin(o / h) * 180 / Math.PI;
+function missingAngle(h, a, o) {
+  const toDeg = (rad) => rad * 180 / Math.PI;
+
+  if (h === 0) return toDeg(Math.atan(o / a));
+  if (a === 0) return toDeg(Math.asin(o / h));
+  return toDeg(Math.acos(a / h));
 }
