1c5e3626eceb99393e72f2b22581f8ee4a74cbdc
t-nielzon <t-nielzon@users.noreply.github.com>
2026-06-25 09:15:33 +0800

    Add go solution: playing-with-digits

--- playing-with-digits/solution.go
+++ playing-with-digits/solution.go
@@ -1,20 +1,18 @@
 package kata
 
-import "strconv"
+import "math"
 
 func DigPow(n, p int) int {
-	digits := strconv.Itoa(n)
-	sum := 0
-	exp := p
-	for _, c := range digits {
-		d := int(c - '0')
-		term := 1
-		for i := 0; i < exp; i++ {
-			term *= d
-		}
-		sum += term
-		exp++
+	digits := []int{}
+	for x := n; x > 0; x /= 10 {
+		digits = append([]int{x % 10}, digits...)
 	}
+
+	sum := 0
+	for i, d := range digits {
+		sum += int(math.Pow(float64(d), float64(p+i)))
+	}
+
 	if sum%n == 0 {
 		return sum / n
 	}
