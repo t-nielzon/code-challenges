466b239bf5d158465cb7d4eba21e2bd75e718a5e
t-nielzon <t-nielzon@users.noreply.github.com>
2026-06-27 11:00:35 +0800

    Add go solution: pascalcase-method

---
+++ pascalcase-method/problem.go
@@ -0,0 +1,17 @@
+// Write a method (or function, depending on the language) that converts a
+// string to PascalCase, that is, all words must have their first letter
+// capitalized and spaces must be removed.
+//
+// Examples (input --> output):
+//
+//   "hello case"       --> "HelloCase"
+//   "pascal case word" --> "PascalCaseWord"
+//
+// Note: for historical reasons, the function is named camelCase() in some
+// languages, but it should actually perform conversion to PascalCase.
+
+package kata
+
+func PascalCase(s string) string {
+	return ""
+}
---
+++ pascalcase-method/solution.go
@@ -0,0 +1,11 @@
+package kata
+
+import "strings"
+
+func PascalCase(s string) string {
+	words := strings.Fields(s)
+	for i, w := range words {
+		words[i] = strings.ToUpper(w[:1]) + w[1:]
+	}
+	return strings.Join(words, "")
+}
