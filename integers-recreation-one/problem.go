ee3ab43c12038ec22863c9176337cdcd3af73b81
t-nielzon <t-nielzon@users.noreply.github.com>
2026-06-28 09:57:48 +0800

    Add go solution: integers-recreation-one

--- integers-recreation-one/problem.go
+++ integers-recreation-one/problem.go
@@ -1,18 +1,15 @@
 /*
+Integers: Recreation One (5 kyu)
+
 1, 246, 2, 123, 3, 82, 6, 41 are the divisors of number 246.
-
 Squaring these divisors we get: 1, 60516, 4, 15129, 9, 6724, 36, 1681.
-
 The sum of these squares is 84100 which is 290 * 290.
 
 Task:
 Find all integers between m and n (m and n are integers with 1 <= m <= n)
-such that the sum of their squared divisors is itself a square.
+such that the sum of their squared divisors is itself a perfect square.
 
-We will return an array of subarrays or of tuples.
-
-The subarrays will have two elements: first the number the squared divisors
-of which is a square and then the sum of the squared divisors.
+Return a slice of [2]int pairs where each pair is [number, sum_of_squared_divisors].
 
 Example:
   m =  1, n = 250 --> [[1, 1], [42, 2500], [246, 84100]]
@@ -21,5 +18,5 @@ Example:
 
 package kata
 
-func ListSquared(m, n uint64) [][2]uint64 {
+func ListSquared(m, n int) [][2]int {
 }
--- integers-recreation-one/solution.go
+++ integers-recreation-one/solution.go
@@ -2,24 +2,27 @@ package kata
 
 import "math"
 
-func ListSquared(m, n uint64) [][2]uint64 {
-	result := [][2]uint64{}
-	for i := m; i <= n; i++ {
-		var sum uint64 = 0
-		sqrtI := uint64(math.Sqrt(float64(i)))
-		for d := uint64(1); d <= sqrtI; d++ {
-			if i%d == 0 {
-				sum += d * d
-				other := i / d
-				if other != d {
-					sum += other * other
-				}
-			}
-		}
-		s := uint64(math.Sqrt(float64(sum)))
-		if s*s == sum {
-			result = append(result, [2]uint64{i, sum})
+func ListSquared(m, n int) [][2]int {
+	result := [][2]int{}
+	for num := m; num <= n; num++ {
+		sum := sumSquaredDivisors(num)
+		sqrt := int(math.Round(math.Sqrt(float64(sum))))
+		if sqrt*sqrt == sum {
+			result = append(result, [2]int{num, sum})
 		}
 	}
 	return result
+}
+
+func sumSquaredDivisors(n int) int {
+	sum := 0
+	for i := 1; i*i <= n; i++ {
+		if n%i == 0 {
+			sum += i * i
+			if i != n/i {
+				sum += (n / i) * (n / i)
+			}
+		}
+	}
+	return sum
 }
