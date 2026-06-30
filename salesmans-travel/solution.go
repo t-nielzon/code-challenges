20433a8e740dbb5f7e2f94c507b8bd2034461163
t-nielzon <t-nielzon@users.noreply.github.com>
2026-06-30 09:48:24 +0800

    Add go solution: salesmans-travel

--- salesmans-travel/solution.go
+++ salesmans-travel/solution.go
@@ -2,25 +2,28 @@ package kata
 
 import "strings"
 
-func Travel(r string, zipcode string) string {
-	if r == "" {
-		return zipcode + ":/"
-	}
+func travel(r string, zipcode string) string {
 	var streets []string
 	var numbers []string
-	for _, addr := range strings.Split(r, ",") {
-		if strings.HasSuffix(addr, zipcode) {
-			body := strings.TrimSuffix(addr, " "+zipcode)
+
+	if r != "" {
+		for _, addr := range strings.Split(r, ",") {
+			if !strings.HasSuffix(addr, zipcode) {
+				continue
+			}
+
+			body := strings.TrimSuffix(addr, zipcode)
+			body = strings.TrimRight(body, " ")
+
 			parts := strings.SplitN(body, " ", 2)
 			if len(parts) < 2 {
 				continue
 			}
+
 			numbers = append(numbers, parts[0])
 			streets = append(streets, parts[1])
 		}
 	}
-	if len(streets) == 0 {
-		return zipcode + ":/"
-	}
+
 	return zipcode + ":" + strings.Join(streets, ",") + "/" + strings.Join(numbers, ",")
 }
