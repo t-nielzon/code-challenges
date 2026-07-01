4eb1da1bf270b817cf107acc90e01107e826f258
t-nielzon <t-nielzon@users.noreply.github.com>
2026-07-01 11:42:26 +0800

    Add go solution: financing-plan-on-planet-xy140z-n

--- financing-plan-on-planet-xy140z-n/solution.go
+++ financing-plan-on-planet-xy140z-n/solution.go
@@ -1,7 +1,5 @@
 package kata
 
-func Finance(n int) int64 {
-	// each cell (w,d) where 0 ≤ w ≤ d ≤ n contributes w+d, which sums to n(n+1)(n+2)/2
-	m := int64(n)
-	return m * (m + 1) * (m + 2) / 2
+func finance(n uint64) uint64 {
+	return n * (n + 1) * (n + 2) / 2
 }
