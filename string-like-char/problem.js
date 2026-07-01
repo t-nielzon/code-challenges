84afc7e91eb546bbcb992d1b56b38af222435173
t-nielzon <t-nielzon@users.noreply.github.com>
2026-07-01 11:42:51 +0800

    Add javascript solution: string-like-char

--- string-like-char/problem.js
+++ string-like-char/problem.js
@@ -1,16 +1,31 @@
 /*
- * In some programming languages, strings internally are implemented like an array of chars.
+ * String like [Char]
  *
- * Add to the String.prototype the next Array.prototype methods:
- *  - map, join, filter, forEach, some, every, reduce, reduceRight, sort, reverse
+ * In some programming languages, strings internally are implemented like an
+ * array of chars, which allows applying array methods to strings.
  *
- * Also implement mutable methods that return the resulting string instead of the
- * value the equivalent array method returns:
- *  - push, pop, shift, unshift, splice
+ * Sadly, in JavaScript there is no Char type, and strings do not have most of
+ * the methods of arrays.
  *
- * Example:
+ * The objective of this kata is to add to the String.prototype the following
+ * Array.prototype methods:
+ *
+ *   map, join, filter, forEach, some, every, reduce, reduceRight, sort, reverse
+ *
+ * Only methods that satisfy these two conditions were included:
+ *   - There is not already a method for the String class with that name.
+ *   - Since strings are immutable, the method must not modify the array
+ *     (sort and reverse are exceptions: they mutate but also return the value).
+ *
+ * We also implement the mutable methods below, but instead of returning what
+ * the array equivalent returns, they return the resulting string:
+ *
+ *   push, pop, shift, unshift, splice
+ *
+ * Examples:
  *   "Hello".push(" Peter"); // "Hello Peter"
  *   "Hello".pop();          // "Hell"
  */
 
-// Add the methods to String.prototype here.
+// implementation is done by extending String.prototype; no single stub function.
+// String.prototype.map = function (callback, thisArg) { };
--- string-like-char/solution.js
+++ string-like-char/solution.js
@@ -1,31 +1,79 @@
-[
-  'map', 'filter', 'forEach', 'some', 'every',
-  'reduce', 'reduceRight', 'sort', 'reverse', 'join'
-].forEach(function (method) {
-  String.prototype[method] = function () {
-    return Array.prototype[method].apply(this.split(''), arguments);
+(function () {
+  // treat the string as an array of chars for delegation to Array.prototype
+  function toArray(str) {
+    return str.split('');
+  }
+
+  // pure (non-mutating) methods return exactly what Array.prototype returns
+  String.prototype.map = function (callback, thisArg) {
+    return toArray(this).map(callback, thisArg);
   };
-});
 
-String.prototype.push = function () {
-  return this + Array.prototype.slice.call(arguments).join('');
-};
+  String.prototype.join = function (separator) {
+    return toArray(this).join(separator);
+  };
 
-String.prototype.pop = function () {
-  return this.slice(0, -1);
-};
+  String.prototype.filter = function (callback, thisArg) {
+    return toArray(this).filter(callback, thisArg);
+  };
 
-String.prototype.shift = function () {
-  return this.slice(1);
-};
+  String.prototype.forEach = function (callback, thisArg) {
+    return toArray(this).forEach(callback, thisArg);
+  };
 
-String.prototype.unshift = function () {
-  return Array.prototype.slice.call(arguments).join('') + this;
-};
+  String.prototype.some = function (callback, thisArg) {
+    return toArray(this).some(callback, thisArg);
+  };
 
-String.prototype.splice = function (start, deleteCount) {
-  var arr = this.split('');
-  var args = [start, deleteCount].concat(Array.prototype.slice.call(arguments, 2));
-  Array.prototype.splice.apply(arr, args);
-  return arr.join('');
-};
+  String.prototype.every = function (callback, thisArg) {
+    return toArray(this).every(callback, thisArg);
+  };
+
+  String.prototype.reduce = function () {
+    return Array.prototype.reduce.apply(toArray(this), arguments);
+  };
+
+  String.prototype.reduceRight = function () {
+    return Array.prototype.reduceRight.apply(toArray(this), arguments);
+  };
+
+  // sort and reverse mutate the array but also return it; join back to a string
+  String.prototype.sort = function (compareFn) {
+    return toArray(this).sort(compareFn).join('');
+  };
+
+  String.prototype.reverse = function () {
+    return toArray(this).reverse().join('');
+  };
+
+  // mutable methods return the resulting string instead of the array's return value
+  String.prototype.push = function () {
+    var arr = toArray(this);
+    arr.push.apply(arr, arguments);
+    return arr.join('');
+  };
+
+  String.prototype.pop = function () {
+    var arr = toArray(this);
+    arr.pop();
+    return arr.join('');
+  };
+
+  String.prototype.shift = function () {
+    var arr = toArray(this);
+    arr.shift();
+    return arr.join('');
+  };
+
+  String.prototype.unshift = function () {
+    var arr = toArray(this);
+    arr.unshift.apply(arr, arguments);
+    return arr.join('');
+  };
+
+  String.prototype.splice = function () {
+    var arr = toArray(this);
+    Array.prototype.splice.apply(arr, arguments);
+    return arr.join('');
+  };
+})();
