e2d6ec7fa0caa04445e66e9d537ecfc6c0f2e8cf
t-nielzon <t-nielzon@users.noreply.github.com>
2026-07-03 11:06:51 +0800

    Add go solution: statistics-for-an-athletic-association

--- statistics-for-an-athletic-association/problem.go
+++ statistics-for-an-athletic-association/problem.go
@@ -3,36 +3,38 @@
 // all race results of every team who has run.
 // For example here is a string showing the individual results of a team of 5 runners:
 //
-//     "01|15|59, 1|47|6, 01|17|20, 1|32|34, 2|3|17"
+//   "01|15|59, 1|47|6, 01|17|20, 1|32|34, 2|3|17"
 //
 // Each part of the string is of the form: h|m|s
 // where h, m, s (h for hour, m for minutes, s for seconds) are positive or null
-// integer (represented as strings) with one or two digits. Substrings in the input
-// string are separated by ", " or ",".
+// integer (represented as strings) with one or two digits. Substrings in the
+// input string are separated by ", " or ",".
 //
-// To compare the results of the teams you are asked for giving three statistics:
-// range, average and median.
+// To compare the results of the teams you are asked for giving
+// three statistics: range, average and median.
 //
-//   - Range: difference between the lowest and highest values.
-//   - Mean or Average: add together all of the numbers and divide the sum by the count.
-//   - Median: the number separating the higher half from the lower half. For an odd
-//     number of observations, the middle one; for an even number, the mean of the two
-//     middle values.
+// - Range: difference between the lowest and highest values.
+//   In {4, 6, 9, 3, 7} the lowest value is 3, and the highest is 9,
+//   so the range is 9 - 3 = 6.
 //
-// Your task is to return a string giving these 3 values. For the example given above,
-// the string result will be:
+// - Mean or Average: To calculate mean, add together all of the numbers
+//   and then divide the sum by the total count of numbers.
 //
-//     "Range: 00|47|18 Average: 01|35|15 Median: 01|32|34"
+// - Median: the number separating the higher half of a data sample from the
+//   lower half. For an odd number of observations, pick the middle one.
+//   For an even number, the median is the mean of the two middle values.
 //
-// of the form:
+// Your task is to return a string giving these 3 values. For the example above:
 //
-//     "Range: hh|mm|ss Average: hh|mm|ss Median: hh|mm|ss"
+//   "Range: 00|47|18 Average: 01|35|15 Median: 01|32|34"
 //
+// of the form: "Range: hh|mm|ss Average: hh|mm|ss Median: hh|mm|ss"
 // where hh, mm, ss are integers (represented by strings) with each 2 digits.
 //
 // Remarks:
-//  1. if a result in seconds is ab.xy... it will be given truncated as ab.
-//  2. if the given string is "" you will return "".
+//   1. if a result in seconds is ab.xy... it will be given truncated as ab.
+//   2. if the given string is "" you will return ""
+
 package kata
 
 func Stat(strg string) string {
--- statistics-for-an-athletic-association/solution.go
+++ statistics-for-an-athletic-association/solution.go
@@ -8,35 +8,36 @@ import (
 )
 
 func Stat(strg string) string {
-	if strg == "" {
+	if strings.TrimSpace(strg) == "" {
 		return ""
 	}
 
-	parts := strings.Split(strings.ReplaceAll(strg, ", ", ","), ",")
+	parts := strings.Split(strg, ",")
 	secs := make([]int, 0, len(parts))
 	for _, p := range parts {
-		hms := strings.Split(strings.TrimSpace(p), "|")
-		h, _ := strconv.Atoi(strings.TrimSpace(hms[0]))
-		m, _ := strconv.Atoi(strings.TrimSpace(hms[1]))
-		s, _ := strconv.Atoi(strings.TrimSpace(hms[2]))
+		fields := strings.Split(strings.TrimSpace(p), "|")
+		h, _ := strconv.Atoi(strings.TrimSpace(fields[0]))
+		m, _ := strconv.Atoi(strings.TrimSpace(fields[1]))
+		s, _ := strconv.Atoi(strings.TrimSpace(fields[2]))
 		secs = append(secs, h*3600+m*60+s)
 	}
-	sort.Ints(secs)
 
+	sort.Ints(secs)
 	n := len(secs)
+
 	rng := secs[n-1] - secs[0]
 
 	sum := 0
 	for _, v := range secs {
 		sum += v
 	}
-	avg := sum / n
+	avg := sum / n // truncated
 
 	var median int
 	if n%2 == 1 {
 		median = secs[n/2]
 	} else {
-		median = (secs[n/2-1] + secs[n/2]) / 2
+		median = (secs[n/2-1] + secs[n/2]) / 2 // truncated
 	}
 
 	return fmt.Sprintf("Range: %s Average: %s Median: %s",
