f930a2e6b91efce05aeec0631cd4f86cfe7463ea
t-nielzon <t-nielzon@users.noreply.github.com>
2026-07-07 02:08:34 +0800

    Add go solution: the-supermarket-queue

--- the-supermarket-queue/solution.go
+++ the-supermarket-queue/solution.go
@@ -1,32 +1,26 @@
-package kata
+package main
 
 func QueueTime(customers []int, n int) int {
-	if len(customers) == 0 {
+	if n == 0 || len(customers) == 0 {
 		return 0
 	}
 	
-	// track when each till will be free
 	tills := make([]int, n)
 	
-	// assign each customer to the till that becomes free earliest
-	for _, customer := range customers {
-		// find the till with minimum free time
+	for _, time := range customers {
 		minIdx := 0
 		for i := 1; i < n; i++ {
 			if tills[i] < tills[minIdx] {
 				minIdx = i
 			}
 		}
-		
-		// add customer to that till
-		tills[minIdx] += customer
+		tills[minIdx] += time
 	}
 	
-	// return the maximum time (when the last till finishes)
-	maxTime := tills[0]
-	for i := 1; i < n; i++ {
-		if tills[i] > maxTime {
-			maxTime = tills[i]
+	maxTime := 0
+	for _, t := range tills {
+		if t > maxTime {
+			maxTime = t
 		}
 	}
 	
