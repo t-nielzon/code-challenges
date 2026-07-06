98e65848891acc578fa308683c99a81c7e801b31
t-nielzon <t-nielzon@users.noreply.github.com>
2026-07-06 13:21:28 +0800

    Add javascript solution: numerical-palindrome-number-3-dot-5

---
+++ numerical-palindrome-number-3-dot-5/solution.js
@@ -0,0 +1,28 @@
+function numPalindrome(num) {
+  if (!Number.isInteger(num) || num < 0) {
+    return "Not valid";
+  }
+  
+  const str = String(num);
+  const palindromes = new Set();
+  
+  for (let i = 0; i < str.length; i++) {
+    for (let j = i + 2; j <= str.length; j++) {
+      const substring = str.substring(i, j);
+      
+      if (substring[0] === '0' || substring[substring.length - 1] === '0') {
+        continue;
+      }
+      
+      if (substring === substring.split('').reverse().join('')) {
+        palindromes.add(parseInt(substring));
+      }
+    }
+  }
+  
+  if (palindromes.size === 0) {
+    return "No palindromes found";
+  }
+  
+  return Array.from(palindromes).sort((a, b) => a - b);
+}
