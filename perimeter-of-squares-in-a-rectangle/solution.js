325a8b2cb5d612f58bf060390a2a0f1d6fef4fa9
t-nielzon <t-nielzon@users.noreply.github.com>
2026-07-11 14:48:34 +0800

    Add javascript solution: perimeter-of-squares-in-a-rectangle

--- perimeter-of-squares-in-a-rectangle/solution.js
+++ perimeter-of-squares-in-a-rectangle/solution.js
@@ -1,10 +1,7 @@
 function perimeter(n) {
-  let a = 1n;
-  let b = 1n;
-  let sum = 1n;
-  for (let i = 1; i <= n; i++) {
-    sum += b;
-    [a, b] = [b, a + b];
+  const fib = [1, 1];
+  for (let i = 2; i <= n; i++) {
+    fib.push(fib[i - 1] + fib[i - 2]);
   }
-  return 4n * sum;
+  return 4 * fib.slice(0, n + 1).reduce((sum, val) => sum + val, 0);
 }
