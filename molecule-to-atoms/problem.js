6e65c6dcb2dc57894479fc1eda92842c15361ee3
t-nielzon <t-nielzon@users.noreply.github.com>
2026-07-12 13:44:16 +0800

    Add javascript solution: molecule-to-atoms

--- molecule-to-atoms/problem.js
+++ molecule-to-atoms/problem.js
@@ -1,29 +1,17 @@
 /*
- * Molecule to atoms
- *
- * For a given chemical formula represented by a string, count the number of
- * atoms of each element contained in the molecule and return an object.
- *
- * For example:
- *
- *   var water = 'H2O';
- *   parseMolecule(water); // return {H: 2, O: 1}
- *
- *   var magnesiumHydroxide = 'Mg(OH)2';
- *   parseMolecule(magnesiumHydroxide); // return {Mg: 1, O: 2, H: 2}
- *
- *   var fremySalt = 'K4[ON(SO3)2]2';
- *   parseMolecule(fremySalt); // return {K: 4, O: 14, N: 2, S: 4}
- *
- * Some formulas have brackets in them. The index outside the brackets tells
- * you that you have to multiply count of each atom inside the bracket by this
- * index. For example, in Fe(NO3)2 you have one iron atom, two nitrogen atoms
- * and six oxygen atoms.
- *
- * Note that brackets may be round, square or curly and can also be nested.
- * Index after the braces is optional.
+ * For a given chemical formula represented by a string, count the number of atoms 
+ * of each element contained in the molecule and return an object.
+ * 
+ * Examples:
+ * parseMolecule('H2O')           // {H: 2, O: 1}
+ * parseMolecule('Mg(OH)2')       // {Mg: 1, O: 2, H: 2}
+ * parseMolecule('K4[ON(SO3)2]2') // {K: 4, O: 14, N: 2, S: 4}
+ * 
+ * Brackets (round, square, curly) can be nested.
+ * Index after brackets tells you to multiply atom counts inside.
+ * Index after elements tells you the atom count.
  */
 
 function parseMolecule(formula) {
-  // your code here
+  
 }
--- molecule-to-atoms/solution.js
+++ molecule-to-atoms/solution.js
@@ -1,55 +1,49 @@
 function parseMolecule(formula) {
-  const tokens = formula.match(/[A-Z][a-z]*|\d+|[()\[\]{}]/g) || [];
-  let pos = 0;
-
-  const opening = { '(': ')', '[': ']', '{': '}' };
-  const closing = { ')': '(', ']': '[', '}': '{' };
-
-  function readNumber() {
-    if (pos < tokens.length && /^\d+$/.test(tokens[pos])) {
-      return parseInt(tokens[pos++], 10);
-    }
-    return 1;
-  }
-
-  function parseGroup() {
-    const counts = {};
-
-    function add(element, n) {
-      counts[element] = (counts[element] || 0) + n;
-    }
-
-    while (pos < tokens.length) {
-      const token = tokens[pos];
-
-      if (closing[token]) {
-        break;
-      } else if (opening[token]) {
-        pos++; // consume opening bracket
-        const inner = parseGroup();
-        const close = tokens[pos++]; // consume closing bracket
-        if (!close || opening[token] !== close) {
-          throw new Error('Not a valid molecule');
-        }
-        const multiplier = readNumber();
-        for (const element in inner) {
-          add(element, inner[element] * multiplier);
-        }
-      } else if (/^[A-Z][a-z]*$/.test(token)) {
-        pos++; // consume element
-        const n = readNumber();
-        add(token, n);
-      } else {
-        throw new Error('Not a valid molecule');
+  const stack = [{}];
+  let i = 0;
+  
+  while (i < formula.length) {
+    const char = formula[i];
+    
+    if (char === '(' || char === '[' || char === '{') {
+      stack.push({});
+      i++;
+    } else if (char === ')' || char === ']' || char === '}') {
+      const top = stack.pop();
+      i++;
+      
+      let numStr = '';
+      while (i < formula.length && /\d/.test(formula[i])) {
+        numStr += formula[i];
+        i++;
       }
+      const multiplier = numStr ? parseInt(numStr) : 1;
+      
+      for (const element in top) {
+        const parent = stack[stack.length - 1];
+        parent[element] = (parent[element] || 0) + top[element] * multiplier;
+      }
+    } else if (/[A-Z]/.test(char)) {
+      let element = char;
+      i++;
+      while (i < formula.length && /[a-z]/.test(formula[i])) {
+        element += formula[i];
+        i++;
+      }
+      
+      let numStr = '';
+      while (i < formula.length && /\d/.test(formula[i])) {
+        numStr += formula[i];
+        i++;
+      }
+      const count = numStr ? parseInt(numStr) : 1;
+      
+      const current = stack[stack.length - 1];
+      current[element] = (current[element] || 0) + count;
+    } else {
+      i++;
     }
-
-    return counts;
   }
-
-  const result = parseGroup();
-  if (pos !== tokens.length) {
-    throw new Error('Not a valid molecule');
-  }
-  return result;
+  
+  return stack[0];
 }
