f58506bef73b303358443c1b6219bdc15159133f
t-nielzon <t-nielzon@users.noreply.github.com>
2026-07-16 09:32:21 +0800

    Add javascript solution: molecule-to-atoms

--- molecule-to-atoms/problem.js
+++ molecule-to-atoms/problem.js
@@ -1,15 +1,14 @@
 /*
- * For a given chemical formula represented by a string, count the number of atoms 
+ * For a given chemical formula represented by a string, count the number of atoms
  * of each element contained in the molecule and return an object.
- * 
+ *
  * Examples:
- * parseMolecule('H2O')           // {H: 2, O: 1}
- * parseMolecule('Mg(OH)2')       // {Mg: 1, O: 2, H: 2}
- * parseMolecule('K4[ON(SO3)2]2') // {K: 4, O: 14, N: 2, S: 4}
- * 
- * Brackets (round, square, curly) can be nested.
- * Index after brackets tells you to multiply atom counts inside.
- * Index after elements tells you the atom count.
+ * parseMolecule('H2O');           // {H: 2, O: 1}
+ * parseMolecule('Mg(OH)2');       // {Mg: 1, O: 2, H: 2}
+ * parseMolecule('K4[ON(SO3)2]2'); // {K: 4, O: 14, N: 2, S: 4}
+ *
+ * Brackets may be round (), square [], or curly {} and can be nested.
+ * Index after brackets is optional.
  */
 
 function parseMolecule(formula) {
--- molecule-to-atoms/solution.js
+++ molecule-to-atoms/solution.js
@@ -1,27 +1,27 @@
 function parseMolecule(formula) {
   const stack = [{}];
   let i = 0;
-  
+
   while (i < formula.length) {
     const char = formula[i];
-    
+
     if (char === '(' || char === '[' || char === '{') {
       stack.push({});
       i++;
     } else if (char === ')' || char === ']' || char === '}') {
-      const top = stack.pop();
       i++;
-      
-      let numStr = '';
+      let num = '';
       while (i < formula.length && /\d/.test(formula[i])) {
-        numStr += formula[i];
+        num += formula[i];
         i++;
       }
-      const multiplier = numStr ? parseInt(numStr) : 1;
-      
-      for (const element in top) {
-        const parent = stack[stack.length - 1];
-        parent[element] = (parent[element] || 0) + top[element] * multiplier;
+      const multiplier = num ? parseInt(num) : 1;
+
+      const popped = stack.pop();
+      const current = stack[stack.length - 1];
+
+      for (const [element, count] of Object.entries(popped)) {
+        current[element] = (current[element] || 0) + count * multiplier;
       }
     } else if (/[A-Z]/.test(char)) {
       let element = char;
@@ -30,20 +30,20 @@ function parseMolecule(formula) {
         element += formula[i];
         i++;
       }
-      
-      let numStr = '';
+
+      let num = '';
       while (i < formula.length && /\d/.test(formula[i])) {
-        numStr += formula[i];
+        num += formula[i];
         i++;
       }
-      const count = numStr ? parseInt(numStr) : 1;
-      
+      const count = num ? parseInt(num) : 1;
+
       const current = stack[stack.length - 1];
       current[element] = (current[element] || 0) + count;
     } else {
       i++;
     }
   }
-  
+
   return stack[0];
 }
