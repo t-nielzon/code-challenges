02d8d78b6aa350ed4dfcdd4d24b7ff6ce793479e
t-nielzon <t-nielzon@users.noreply.github.com>
2026-07-18 03:51:30 +0800

    Add javascript solution: simple-events

--- simple-events/problem.js
+++ simple-events/problem.js
@@ -1,14 +1,19 @@
-/**
- * Simple Events
- * 
- * Your goal is to write an Event constructor function, which can be used to make event objects.
- * 
- * An event object should work like this:
- * - it has a .subscribe() method, which takes a function and stores it as its handler
- * - it has an .unsubscribe() method, which takes a function and removes it from its handlers
- * - it has an .emit() method, which takes an arbitrary number of arguments and calls all the stored functions with these arguments
- */
+/*
+  Your goal is to write an Event constructor function, which can be used to make event objects.
+
+  An event object should work like this:
+  - it has a .subscribe() method, which takes a function and stores it as its handler
+  - it has an .unsubscribe() method, which takes a function and removes it from its handlers
+  - it has an .emit() method, which takes an arbitrary number of arguments and calls all the stored functions with these arguments
+
+  Simplifications:
+  - all functions are called with correct arguments (e.g. only functions will be passed to unsubscribe)
+  - you should not worry about the order of handlers' execution
+  - the handlers will not attempt to modify an event object (e.g. add or remove handlers)
+  - the context of handlers' execution is not important
+  - each handler will be subscribed at most once at any given moment of time
+*/
 
 function Event() {
-
+  
 }
