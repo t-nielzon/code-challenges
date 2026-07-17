51877edbf0c389ce48758700e0120727db4bfaa6
t-nielzon <t-nielzon@users.noreply.github.com>
2026-07-18 03:51:11 +0800

    Add go solution: find-last-fibonacci-digit-hardcore-version

---
+++ find-last-fibonacci-digit-hardcore-version/problem.go
@@ -0,0 +1,21 @@
+/*
+Find last Fibonacci digit [hardcore version]
+
+Return the last digit of the nth element in the Fibonacci sequence
+(starting with 1, 1, not 0, 1).
+
+For large values of n (up to 1,000,000), brute force calculation is impractical.
+
+Examples:
+LastFibDigit(1) == 1
+LastFibDigit(2) == 1
+LastFibDigit(3) == 2
+LastFibDigit(1000) == 5
+LastFibDigit(1000000) == 5
+*/
+package main
+
+func LastFibDigit(n int64) int {
+	// Implementation here
+	return 0
+}
