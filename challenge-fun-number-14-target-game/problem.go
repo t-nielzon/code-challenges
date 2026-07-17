19ca5079bc1bfb3a43544ee1130079e93622b36f
t-nielzon <t-nielzon@users.noreply.github.com>
2026-07-17 11:21:51 +0800

    Add go solution: challenge-fun-number-14-target-game

--- challenge-fun-number-14-target-game/problem.go
+++ challenge-fun-number-14-target-game/problem.go
@@ -1,35 +1,26 @@
-// ## Task
-// In your favorite game, you must shoot a target with a water-gun to gain
-// points. Each target can be worth a different amount of points.
-//
-// You are guaranteed to hit every target that you try to hit. You cannot hit
-// consecutive targets though because targets are only visible for one second
-// (one at a time) and it takes you a full second to reload your water-gun after
-// shooting (you start the game already loaded).
-//
-// Given an array `vals` with the order of each target's point value, determine
-// the maximum number of points that you can win.
-//
-// ## Example
-// For `vals = [1, 2, 3, 4]`, the result should be `6`.
-// (shoot vals[1]=2 and vals[3]=4)
-//
-// For `vals = [5, 5, 5, 5, 5]`, the result should be `15`.
-// (shoot the 1st, 3rd and 5th)
-//
-// Note that the value can be zero or negative, don't shoot them.
-//
-// For `vals = [0, 0, -1, -1]`, the result should be `0`.
-// For `vals = [5, -2, -9, -4]`, the result should be `5`.
-//
-// ## Input/Output
-// - [input] integer array `vals`
-//   The point values (negative or non-negative) of the targets (in order).
-// - [output] an integer
-//   The maximum number of points that you can score.
+/*
+In your favorite game, you must shoot a target with a water-gun to gain points.
+Each target can be worth a different amount of points.
 
-package kata
+You are guaranteed to hit every target that you try to hit. You cannot hit
+consecutive targets though because targets are only visible for one second
+(one at a time) and it takes you a full second to reload your water-gun after
+shooting (you start the game already loaded).
 
+Given an array vals with the order of each target's point value, determine the
+maximum number of points that you can win.
+
+Example:
+For vals = [1, 2, 3, 4], the result should be 6.
+Your optimal strategy would be to let the first one pass and shoot the second
+one with value 2 and the 4th one with value 4.
+
+For vals = [5, 5, 5, 5, 5], the result should be 15.
+Your optimal strategy would be to shoot the 1st, 3rd and 5th value.
+
+For vals = [0, 0, -1, -1], the result should be 0.
+
+For vals = [5, -2, -9, -4], the result should be 5.
+*/
 func TargetGame(vals []int) int {
-	// your code here
 }
--- challenge-fun-number-14-target-game/solution.go
+++ challenge-fun-number-14-target-game/solution.go
@@ -1,16 +1,36 @@
-package kata
-
-// classic house-robber: at each target keep the best score achievable
-// either by skipping it (prev) or shooting it plus the best from two back.
 func TargetGame(vals []int) int {
-	prev, prevPrev := 0, 0
-	for _, v := range vals {
-		take := prevPrev + v
-		if take < prev {
-			take = prev
-		}
-		prevPrev = prev
-		prev = take
+	n := len(vals)
+	if n == 0 {
+		return 0
 	}
-	return prev
+
+	if n == 1 {
+		if vals[0] < 0 {
+			return 0
+		}
+		return vals[0]
+	}
+
+	dp := make([]int, n)
+
+	dp[0] = vals[0]
+	if dp[0] < 0 {
+		dp[0] = 0
+	}
+
+	dp[1] = vals[1]
+	if dp[1] < dp[0] {
+		dp[1] = dp[0]
+	}
+
+	for i := 2; i < n; i++ {
+		option1 := dp[i-1]
+		option2 := vals[i] + dp[i-2]
+		dp[i] = option1
+		if option2 > option1 {
+			dp[i] = option2
+		}
+	}
+
+	return dp[n-1]
 }
