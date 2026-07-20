2742236f3285a0f0667c97ef9c53e36af4a30a6c
t-nielzon <t-nielzon@users.noreply.github.com>
2026-07-20 10:45:47 +0800

    Add javascript solution: simple-events

--- simple-events/solution.js
+++ simple-events/solution.js
@@ -2,14 +2,19 @@ function Event() {
   this.handlers = [];
 }
 
-Event.prototype.subscribe = function(fn) {
-  this.handlers.push(fn);
+Event.prototype.subscribe = function(handler) {
+  this.handlers.push(handler);
 };
 
-Event.prototype.unsubscribe = function(fn) {
-  this.handlers = this.handlers.filter(handler => handler !== fn);
+Event.prototype.unsubscribe = function(handler) {
+  const index = this.handlers.indexOf(handler);
+  if (index !== -1) {
+    this.handlers.splice(index, 1);
+  }
 };
 
 Event.prototype.emit = function(...args) {
-  this.handlers.forEach(handler => handler(...args));
+  for (const handler of this.handlers) {
+    handler(...args);
+  }
 };
