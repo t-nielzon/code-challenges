df1f08b9cc5e4cf4fa8c59098cb637a9f1896480
t-nielzon <t-nielzon@users.noreply.github.com>
2026-07-24 08:38:17 +0800

    Add javascript solution: zipwith-ii-lists

--- zipwith-ii-lists/solution.js
+++ zipwith-ii-lists/solution.js
@@ -1,12 +1,10 @@
-function Node(value, next = null) {
-  this.value = value;
-  this.next = next;
-}
-
 function zipWith(fn, list1, list2) {
   if (list1 === null || list2 === null) {
     return null;
   }
   
-  return new Node(fn(list1.value, list2.value), zipWith(fn, list1.next, list2.next));
+  return new Node(
+    fn(list1.value, list2.value),
+    zipWith(fn, list1.next, list2.next)
+  );
 }
