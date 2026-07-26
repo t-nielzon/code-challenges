39d73740664b74f14e234c1c1b96d87ea944d5db
t-nielzon <t-nielzon@users.noreply.github.com>
2026-07-27 07:50:24 +0800

    Add go solution: midi-files-variable-length-quantity

--- midi-files-variable-length-quantity/problem.go
+++ midi-files-variable-length-quantity/problem.go
@@ -1,11 +1,14 @@
-package main
+package kata
 
 /*
 # Introduction
 
-In MIDI files, timestamps are stored as integers representing the number of "ticks" since the previous event in the file.
+In MIDI files, timestamps are stored as integers representing the number of "ticks"
+since the previous event in the file.
 
-Because these timestamps are relative to one another, most values tend to be fairly small. In order to save space, MIDI files use a special integer encoding format called a Variable Length Quantity (VLQ).
+Because these timestamps are relative to one another, most values tend to be fairly
+small. In order to save space, MIDI files use a special integer encoding format
+called a Variable Length Quantity (VLQ).
 
 A MIDI VLQ stores an integer using one or more bytes:
 - The lower 7 bits of each byte contain the actual data
@@ -14,38 +17,45 @@ A MIDI VLQ stores an integer using one or more bytes:
   - 0 means this is the final byte
 
 For example:
-0x7F -> 01111111
-Since the highest bit is 0, this is the final byte.
-Removing the continuation bit leaves: 1111111 = 127
+  0x7F -> 01111111
 
-Values larger than 127 require multiple bytes.
-For example: [0x81, 0x00] in binary becomes 10000001 00000000
+Since the highest bit is 0, this is the final byte. Removing the continuation bit
+leaves: 1111111 = 127
+
+Values larger than 127 require multiple bytes. For example:
+  [0x81, 0x00] in binary becomes: 10000001 00000000
+
 Removing the continuation bits gives: 0000001 0000000 = 128
 
 # Task
 
 Implement two functions:
-- encode(n): convert an integer into a MIDI VLQ byte sequence
-- decode(data): decode only the first complete MIDI VLQ contained in the input
+- Encode: convert an integer into a MIDI VLQ byte sequence
+- Decode: decode only the first complete MIDI VLQ in the input, ignoring bytes after
 
-All test values will satisfy: 0 <= n <= 0x0FFFFFFF
+All test values satisfy: 0 <= n <= 0x0FFFFFFF
 
-Examples:
-encode(0)    == [0x00]
-encode(127)  == [0x7F]
-encode(128)  == [0x81, 0x00]
-encode(8192) == [0xC0, 0x00]
+# Examples
 
-decode([0x00])                   == 0
-decode([0x7F])                   == 127
-decode([0x81, 0x00])             == 128
-decode([0xFF, 0xFF, 0xFF, 0x7F]) == 268435455
+Encode:
+  Encode(0)    == [0x00]
+  Encode(127)  == [0x7F]
+  Encode(128)  == [0x81, 0x00]
+  Encode(8192) == [0xC0, 0x00]
+
+Decode:
+  Decode([0x00])                   == 0
+  Decode([0x7F])                   == 127
+  Decode([0x81, 0x00])             == 128
+  Decode([0xFF, 0xFF, 0xFF, 0x7F]) == 268435455
 */
 
 func Encode(n int) []byte {
 	// TODO
+	return nil
 }
 
 func Decode(data []byte) int {
 	// TODO
+	return 0
 }
--- midi-files-variable-length-quantity/solution.go
+++ midi-files-variable-length-quantity/solution.go
@@ -1,30 +1,38 @@
-package main
+package kata
 
 func Encode(n int) []byte {
-	if n == 0 {
-		return []byte{0}
+	if n <= 127 {
+		return []byte{byte(n)}
 	}
 
-	var bytes []byte
+	var result []byte
+
 	for n > 0 {
-		bytes = append([]byte{byte(n & 0x7F)}, bytes...)
+		result = append(result, byte(n&0x7F))
 		n >>= 7
 	}
 
-	for i := 0; i < len(bytes)-1; i++ {
-		bytes[i] |= 0x80
+	for i, j := 0, len(result)-1; i < j; i, j = i+1, j-1 {
+		result[i], result[j] = result[j], result[i]
 	}
 
-	return bytes
+	for i := 0; i < len(result)-1; i++ {
+		result[i] |= 0x80
+	}
+
+	return result
 }
 
 func Decode(data []byte) int {
 	result := 0
+
 	for _, b := range data {
 		result = (result << 7) | int(b&0x7F)
-		if (b & 0x80) == 0 {
+
+		if b&0x80 == 0 {
 			break
 		}
 	}
+
 	return result
 }
