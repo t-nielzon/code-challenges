ff78af250c244b1d421f9961e0c99c4571babdf2
t-nielzon <t-nielzon@users.noreply.github.com>
2026-07-26 08:24:37 +0800

    Add go solution: steps-in-k-primes

--- steps-in-k-primes/problem.go
+++ steps-in-k-primes/problem.go
@@ -1,4 +1,4 @@
-package kata
+package main
 
 /*
 A natural number is called k-prime if it has exactly k prime factors, counted with multiplicity.
@@ -16,21 +16,23 @@ The k-prime numbers are not regularly spaced. For example: between 2 and 50 we h
 The steps between two k-primes of this list are 2, 3, 1, 4, 1, 6, 1, 3, 1, 7, 1, 1, 3, 1, 7, 3.
 
 Task:
-Write a function KprimesStep(k, step, start, nd) with parameters:
-- k (integer > 0) which indicates the type of k-primes we are looking for,
-- step (integer > 0) which indicates the step we want to find between two k-primes,
-- start (integer >= 0) which gives the start of the search (start inclusive),
+We will write a function kprimes_step(k, step, start, nd) with parameters:
+- k (integer > 0) which indicates the type of k-primes we are looking for
+- step (integer > 0) which indicates the step we want to find between two k-primes
+- start (integer >= 0) which gives the start of the search (start inclusive)
 - nd (integer >= start) which gives the end of the search (nd inclusive)
 
 This function should return an array of all the pairs (or tuples) of k-prime numbers spaced with a step of step
 between the limits start, nd. This array can be empty.
 
 Examples:
-KprimesStep(2, 2, 0, 50) => [[4, 6], [33, 35]]
-KprimesStep(6, 14, 2113665, 2113889) => [[2113722, 2113736]]
-KprimesStep(2, 10, 0, 50) => [[4, 14], [15, 25], [25, 35], [39, 49]]
-KprimesStep(5, 20, 0, 50) => []
+kprimes_step(2, 2, 0, 50) => [[4, 6], [33, 35]]
+kprimes_step(6, 14, 2113665, 2113889) => [[2113722, 2113736]]
+kprimes_step(2, 10, 0, 50) => [[4, 14], [15, 25], [25, 35], [39, 49]]
+kprimes_step(5, 20, 0, 50) => []
 */
 
-func KprimesStep(k, step, start, nd int) [][]int {
+func kprimes_step(k, step, start, nd int) [][]int {
+	// TODO: implement
+	return nil
 }
--- steps-in-k-primes/solution.go
+++ steps-in-k-primes/solution.go
@@ -1,29 +1,40 @@
-package kata
+package main
 
 func countPrimeFactors(n int) int {
+	if n <= 1 {
+		return 0
+	}
 	count := 0
-	for d := 2; d*d <= n; d++ {
-		for n%d == 0 {
+	// divide out all 2s
+	for n%2 == 0 {
+		count++
+		n /= 2
+	}
+	// check odd factors from 3 onwards
+	for i := 3; i*i <= n; i += 2 {
+		for n%i == 0 {
 			count++
-			n /= d
+			n /= i
 		}
 	}
+	// if n > 1, then it's a prime factor itself
 	if n > 1 {
 		count++
 	}
 	return count
 }
 
-func KprimesStep(k, step, start, nd int) [][]int {
-	result := [][]int{}
-	var prev = -1
-	for n := start; n <= nd; n++ {
-		if countPrimeFactors(n) == k {
-			if prev != -1 && n-prev == step {
-				result = append(result, []int{prev, n})
+func kprimes_step(k, step, start, nd int) [][]int {
+	var result [][]int
+	
+	for num := start; num <= nd; num++ {
+		if countPrimeFactors(num) == k {
+			nextNum := num + step
+			if nextNum <= nd && countPrimeFactors(nextNum) == k {
+				result = append(result, []int{num, nextNum})
 			}
-			prev = n
 		}
 	}
+	
 	return result
 }
