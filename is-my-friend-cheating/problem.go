59fc6244ed2601c6b1ca70eff229fb2d34591a66
t-nielzon <t-nielzon@users.noreply.github.com>
2026-07-27 07:49:43 +0800

    Add go solution: is-my-friend-cheating

--- is-my-friend-cheating/problem.go
+++ is-my-friend-cheating/problem.go
@@ -1,19 +1,19 @@
+package main
+
 /*
 A friend of mine takes the sequence of all numbers from 1 to n (where n > 0).
 Within that sequence, he chooses two numbers, a and b.
-He says that the product of a and b should be equal to the sum of all numbers
-in the sequence, excluding a and b.
+He says that the product of a and b should be equal to the sum of all numbers in the sequence, excluding a and b.
 Given a number n, could you tell me the numbers he excluded from the sequence?
 
 The function takes the parameter: n (n is always strictly greater than 0)
-and returns an array of [2]int with all (a, b) which are the possible removed
-numbers in the sequence 1 to n, sorted in increasing order of "a".
+and returns an array of the form: [[a, b], ...] with all (a, b) which are the possible removed numbers in the sequence 1 to n.
+The array will be sorted in increasing order of the "a".
 
-Returns nil if no possible numbers are found.
+If no possible numbers are found, return nil.
 
-Example: removNb(26) should return [[15, 21], [21, 15]]
+Examples:
+RemoveNb(26) should return [[15, 21], [21, 15]]
 */
-package kata
-
 func RemoveNb(n int) [][2]int {
 }
--- is-my-friend-cheating/solution.go
+++ is-my-friend-cheating/solution.go
@@ -1,19 +1,42 @@
-package kata
+package main
+
+import "sort"
 
 func RemoveNb(n int) [][2]int {
-	sum := n * (n + 1) / 2
+	sumAll := int64(n) * int64(n+1) / 2
+	target := sumAll + 1
+	
 	var result [][2]int
-	for a := 1; a <= n; a++ {
-		// a*b = sum - a - b  =>  b = (sum - a) / (a + 1)
-		if (sum-a)%(a+1) == 0 {
-			b := (sum - a) / (a + 1)
-			if b >= 1 && b <= n {
-				result = append(result, [2]int{a, b})
+	
+	// Find all divisors by iterating up to sqrt(target)
+	for d := int64(1); d*d <= target; d++ {
+		if target%d == 0 {
+			// Check divisor d: (d-1, target/d - 1)
+			a := d - 1
+			b := target/d - 1
+			if a >= 1 && a <= int64(n) && b >= 1 && b <= int64(n) && a != b {
+				result = append(result, [2]int{int(a), int(b)})
+			}
+			
+			// Check divisor target/d (if different from d): (target/d - 1, d - 1)
+			if d != target/d {
+				d2 := target / d
+				a2 := d2 - 1
+				b2 := d - 1
+				if a2 >= 1 && a2 <= int64(n) && b2 >= 1 && b2 <= int64(n) && a2 != b2 {
+					result = append(result, [2]int{int(a2), int(b2)})
+				}
 			}
 		}
 	}
+	
 	if len(result) == 0 {
 		return nil
 	}
+	
+	sort.Slice(result, func(i, j int) bool {
+		return result[i][0] < result[j][0]
+	})
+	
 	return result
 }
