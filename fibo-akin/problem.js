c84b2a59fe41373fb541c3619408cfd698165120
t-nielzon <t-nielzon@users.noreply.github.com>
2026-07-26 08:24:56 +0800

    Add javascript solution: fibo-akin

--- fibo-akin/problem.js
+++ fibo-akin/problem.js
@@ -1,21 +1,21 @@
-/*
- * Fibo akin
- *
+/**
+ * Fibo akin Kata - 5 kyu
+ * 
  * Be u(n) a sequence beginning with:
- *   u[1] = 1, u[2] = 1, u[3] = 2, u[4] = 3, u[5] = 3, u[6] = 4,
- *   u[7] = 5, u[8] = 5, u[9] = 6, u[10] = 6, u[11] = 6, u[12] = 8, ...
- *
- * u[n] = u[n - u[n-1]] + u[n - u[n-2]]
- *
- * Task:
- *  1) lengthSupUK(n, k): number of terms u[i] >= k with 1 <= i <= n.
- *  2) comp(n): number of times a term of u is less than its predecessor up to u[n].
+ * u[1] = 1, u[2] = 1, u[3] = 2, u[4] = 3, u[5] = 3, u[6] = 4,
+ * u[7] = 5, u[8] = 5, u[9] = 6, u[10] = 6, u[11] = 6, u[12] = 8,
+ * u[13] = 8, u[14] = 8, u[15] = 10, u[16] = 9, u[17] = 10, u[18] = 11,
+ * u[19] = 11, u[20] = 12, u[21] = 12, u[22] = 12, u[23] = 12, etc...
+ * 
+ * The recurrence relation is: u[n] = u[n - u[n-1]] + u[n - u[n-2]]
+ * 
+ * For example:
+ * - u[8] = u[8 - u[7]] + u[8 - u[6]] = u[8 - 5] + u[8 - 4] = u[3] + u[4] = 2 + 3 = 5
+ * - u[13] = u[13 - u[12]] + u[13 - u[11]] = u[13 - 8] + u[13 - 6] = u[5] + u[7] = 3 + 5 = 8
  */
 
-function lengthSupUK(n, k) {
-
+function length_sup_u_k(n, k) {
 }
 
 function comp(n) {
-
 }
--- fibo-akin/solution.js
+++ fibo-akin/solution.js
@@ -1,27 +1,35 @@
-function buildU(n) {
+function length_sup_u_k(n, k) {
   const u = new Array(n + 1);
   u[1] = 1;
-  if (n >= 2) u[2] = 1;
+  u[2] = 1;
+  
   for (let i = 3; i <= n; i++) {
     u[i] = u[i - u[i - 1]] + u[i - u[i - 2]];
   }
-  return u;
-}
-
-function lengthSupUK(n, k) {
-  const u = buildU(n);
+  
   let count = 0;
   for (let i = 1; i <= n; i++) {
-    if (u[i] >= k) count++;
+    if (u[i] >= k) {
+      count++;
+    }
   }
   return count;
 }
 
 function comp(n) {
-  const u = buildU(n);
+  const u = new Array(n + 1);
+  u[1] = 1;
+  u[2] = 1;
+  
+  for (let i = 3; i <= n; i++) {
+    u[i] = u[i - u[i - 1]] + u[i - u[i - 2]];
+  }
+  
   let count = 0;
   for (let i = 2; i <= n; i++) {
-    if (u[i] < u[i - 1]) count++;
+    if (u[i] < u[i - 1]) {
+      count++;
+    }
   }
   return count;
 }
