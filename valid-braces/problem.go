9389da3e24637f00405505cc3bc41f7161554b95
t-nielzon <t-nielzon@users.noreply.github.com>
2026-08-01 07:56:56 +0800

    Add go solution: valid-braces

---
+++ valid-braces/problem.go
@@ -0,0 +1,20 @@
+package main
+
+/*
+Write a function that takes a string of braces, and determines if the order of the braces is valid.
+It should return true if the string is valid, and false if it's invalid.
+
+A string of braces is considered valid if all braces are matched with the correct brace.
+
+Examples:
+"(){}[]"   =>  true
+"([{}])"   =>  true
+"(}"       =>  false
+"[(])"     =>  false
+"[({})](]" =>  false
+*/
+
+func ValidBraces(s string) bool {
+	// TODO: implement
+	return false
+}
---
+++ valid-braces/solution.go
@@ -0,0 +1,29 @@
+package main
+
+func ValidBraces(s string) bool {
+	stack := []rune{}
+	
+	for _, ch := range s {
+		switch ch {
+		case '(', '[', '{':
+			stack = append(stack, ch)
+		case ')':
+			if len(stack) == 0 || stack[len(stack)-1] != '(' {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		case ']':
+			if len(stack) == 0 || stack[len(stack)-1] != '[' {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		case '}':
+			if len(stack) == 0 || stack[len(stack)-1] != '{' {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		}
+	}
+	
+	return len(stack) == 0
+}
