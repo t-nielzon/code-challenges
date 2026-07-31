2496d47a4162d4c563b092b416fef12b75d179fc
t-nielzon <t-nielzon@users.noreply.github.com>
2026-07-31 08:19:57 +0800

    Add javascript solution: buddy-pairs

--- buddy-pairs/problem.js
+++ buddy-pairs/problem.js
@@ -1,17 +1,27 @@
 /*
- * Buddy Pairs
- *
- * The divisors of a positive integer n are said to be proper when you consider
- * only the divisors other than n itself. Let s(n) be the sum of these proper
- * divisors of n. Two positive integers (n, m) are a buddy pair if:
- *   s(m) = n + 1 and s(n) = m + 1
- *
- * Given two positive integers start and limit, return the first pair [n, m]
- * of buddy pairs such that n is between start (inclusive) and limit (inclusive);
- * m can be greater than limit and must be greater than n.
- *
+ * Buddy pairs
+ * 
+ * The divisors of a positive integer n are said to be proper when considering
+ * only the divisors other than n itself.
+ * 
+ * Let s(n) be the sum of these proper divisors of n.
+ * 
+ * Call buddy two positive integers such that the sum of the proper divisors
+ * of each number is one more than the other number:
+ * (n, m) are a pair of buddy if s(m) = n + 1 and s(n) = m + 1
+ * 
+ * For example 48 & 75 is such a pair:
+ * - Divisors of 48 are: 1, 2, 3, 4, 6, 8, 12, 16, 24 --> sum: 76 = 75 + 1
+ * - Divisors of 75 are: 1, 3, 5, 15, 25 --> sum: 49 = 48 + 1
+ * 
+ * Task: Given two positive integers start and limit, the function buddy(start, limit)
+ * should return the first pair (n m) of buddy pairs such that n is between start
+ * (inclusive) and limit (inclusive); m can be greater than limit and has to be
+ * greater than n.
+ * 
  * If there is no buddy pair satisfying the conditions, return "Nothing".
  */
+
 function buddy(start, limit) {
-  // your code here
+  
 }
--- buddy-pairs/solution.js
+++ buddy-pairs/solution.js
@@ -1,21 +1,25 @@
 function buddy(start, limit) {
-  const sumProperDivisors = (n) => {
+  function sumOfDivisors(n) {
     let sum = 1;
-    const sqrt = Math.floor(Math.sqrt(n));
-    for (let i = 2; i <= sqrt; i++) {
+    for (let i = 2; i * i <= n; i++) {
       if (n % i === 0) {
         sum += i;
-        if (i !== n / i) sum += n / i;
+        if (i !== n / i) {
+          sum += n / i;
+        }
       }
     }
     return sum;
-  };
-
+  }
+  
   for (let n = start; n <= limit; n++) {
-    const m = sumProperDivisors(n) - 1;
-    if (m > n && sumProperDivisors(m) - 1 === n) {
+    const sn = sumOfDivisors(n);
+    const m = sn - 1;
+    
+    if (m > n && sumOfDivisors(m) === n + 1) {
       return [n, m];
     }
   }
+  
   return "Nothing";
 }
