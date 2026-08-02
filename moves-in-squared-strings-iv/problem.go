25f9f16b396e02a8d3182c1f061e82bc4c2018f2
t-nielzon <t-nielzon@users.noreply.github.com>
2026-08-02 19:42:09 +0800

    Add go solution: moves-in-squared-strings-iv

--- moves-in-squared-strings-iv/problem.go
+++ moves-in-squared-strings-iv/problem.go
@@ -1,28 +1,36 @@
-/*
-You are given a string of n lines, each substring being n characters long.
+package main
 
-Transformations:
-- diag_2_sym: Symmetry with respect to the main cross diagonal
-- rot_90_counter: Counterclockwise rotation 90 degrees
-- selfie_diag2_counterclock: initial + diag_2_sym + rot_90_counter joined by |
-- oper(fct, s): high-order function applying fct to s
-
-Example:
-  s = "abcd\nefgh\nijkl\nmnop"
-  oper(diag_2_sym, s) => "plhd\nokgc\nnjfb\nmiea"
-  oper(rot_90_counter, s) => "dhlp\ncgko\nbfjn\naeim"
-  oper(selfie_diag2_counterclock, s) => "abcd|plhd|dhlp\nefgh|okgc|cgko\nijkl|njfb|bfjn\nmnop|miea|aeim"
-*/
-package kata
+// You are given a string of n lines, each substring being n characters long.
+// For example: s = "abcd\nefgh\nijkl\nmnop"
+//
+// We will study some transformations of this square of strings:
+//
+// - Symmetry with respect to the antidiagonal: Diag2Sym
+//   Diag2Sym("abcd\nefgh\nijkl\nmnop") => "plhd\nokgc\nnjfb\nmiea"
+//
+// - Counterclockwise rotation 90 degrees: Rot90Counter
+//   Rot90Counter("abcd\nefgh\nijkl\nmnop") => "dhlp\ncgko\nbfjn\naeim"
+//
+// - SelfieDiag2Counterclock
+//   It is initial string + string obtained by symmetry with respect to the antidiagonal + counterclockwise rotation 90 degrees.
+//   SelfieDiag2Counterclock("abcd\nefgh\nijkl\nmnop") => "abcd|plhd|dhlp\nefgh|okgc|cgko\nijkl|njfb|bfjn\nmnop|miea|aeim"
+//
+// Task:
+// Write the functions Diag2Sym, Rot90Counter, SelfieDiag2Counterclock
+// and the high-order function Oper(fct, s) where fct is a function to apply to string s
 
 func Diag2Sym(s string) string {
+	return ""
 }
 
 func Rot90Counter(s string) string {
+	return ""
 }
 
 func SelfieDiag2Counterclock(s string) string {
+	return ""
 }
 
 func Oper(fct func(string) string, s string) string {
+	return ""
 }
--- moves-in-squared-strings-iv/solution.go
+++ moves-in-squared-strings-iv/solution.go
@@ -1,44 +1,65 @@
-package kata
+package main
 
 import "strings"
 
 func Diag2Sym(s string) string {
 	lines := strings.Split(s, "\n")
 	n := len(lines)
-	result := make([]string, n)
-	for i := 0; i < n; i++ {
-		row := make([]byte, n)
-		for j := 0; j < n; j++ {
-			row[j] = lines[n-1-j][n-1-i]
-		}
-		result[i] = string(row)
+	
+	result := make([][]rune, n)
+	for i := range result {
+		result[i] = make([]rune, n)
 	}
-	return strings.Join(result, "\n")
+	
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
+			result[n-1-j][n-1-i] = rune(lines[i][j])
+		}
+	}
+	
+	return linesToString(result)
 }
 
 func Rot90Counter(s string) string {
 	lines := strings.Split(s, "\n")
 	n := len(lines)
-	result := make([]string, n)
-	for i := 0; i < n; i++ {
-		row := make([]byte, n)
-		for j := 0; j < n; j++ {
-			row[j] = lines[j][n-1-i]
-		}
-		result[i] = string(row)
+	
+	result := make([][]rune, n)
+	for i := range result {
+		result[i] = make([]rune, n)
 	}
-	return strings.Join(result, "\n")
+	
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
+			result[n-1-j][i] = rune(lines[i][j])
+		}
+	}
+	
+	return linesToString(result)
 }
 
 func SelfieDiag2Counterclock(s string) string {
-	orig := strings.Split(s, "\n")
-	d := strings.Split(Diag2Sym(s), "\n")
-	r := strings.Split(Rot90Counter(s), "\n")
-	result := make([]string, len(orig))
-	for i := range orig {
-		result[i] = orig[i] + "|" + d[i] + "|" + r[i]
+	diag2 := Diag2Sym(s)
+	rot90 := Rot90Counter(s)
+	
+	origLines := strings.Split(s, "\n")
+	diag2Lines := strings.Split(diag2, "\n")
+	rot90Lines := strings.Split(rot90, "\n")
+	
+	var result []string
+	for i := 0; i < len(origLines); i++ {
+		result = append(result, origLines[i]+"|"+diag2Lines[i]+"|"+rot90Lines[i])
 	}
+	
 	return strings.Join(result, "\n")
+}
+
+func linesToString(grid [][]rune) string {
+	var lines []string
+	for i := range grid {
+		lines = append(lines, string(grid[i]))
+	}
+	return strings.Join(lines, "\n")
 }
 
 func Oper(fct func(string) string, s string) string {
