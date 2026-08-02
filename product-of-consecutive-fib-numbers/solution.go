932838e3b2e162959802315c5e224ed76667e4f9
t-nielzon <t-nielzon@users.noreply.github.com>
2026-08-03 07:58:49 +0800

    Add go solution: product-of-consecutive-fib-numbers

--- product-of-consecutive-fib-numbers/solution.go
+++ product-of-consecutive-fib-numbers/solution.go
@@ -1,12 +1,15 @@
-package kata
+// solution.go
+package main
 
-func ProductFib(prod uint64) [3]uint64 {
-	var a, b uint64 = 0, 1
+func ProductFib(prod int64) [3]interface{} {
+	a, b := int64(0), int64(1)
+	
 	for a*b < prod {
 		a, b = b, a+b
 	}
+	
 	if a*b == prod {
-		return [3]uint64{a, b, 1}
+		return [3]interface{}{a, b, true}
 	}
-	return [3]uint64{a, b, 0}
+	return [3]interface{}{a, b, false}
 }
