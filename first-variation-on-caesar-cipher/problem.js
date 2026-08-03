92a9547bf87cf627a39e59ff4085556262ae3ae2
t-nielzon <t-nielzon@users.noreply.github.com>
2026-08-03 08:00:47 +0800

    Add javascript solution: first-variation-on-caesar-cipher

--- first-variation-on-caesar-cipher/problem.js
+++ first-variation-on-caesar-cipher/problem.js
@@ -1,35 +1,26 @@
 /*
-The action of a Caesar cipher is to replace each plaintext letter (plaintext letters are from 'a' to 'z' or from 'A' to 'Z') with a different one a fixed number of places up or down the alphabet.
-
-This program performs a variation of the Caesar shift. The shift increases by 1 for each character (on each iteration).
-
-If the shift is initially 1, the first character of the message to be encoded will be shifted by 1, the second character will be shifted by 2, etc...
-
-Coding: Parameters and return of function "movingShift"
-- param s: a string to be coded
-- param shift: an integer giving the initial shift
-
-The function "movingShift" first codes the entire string and then returns an array of strings containing the coded string in 5 parts.
-
-If possible the message will be equally divided by message length between the five runners. If this is not possible, parts 1 to 5 will have subsequently non-increasing lengths, such that parts 1 to 4 are at least as long as when evenly divided, but at most 1 longer. If the last part is the empty string this empty string must be shown in the resulting array.
-
-Decoding: parameters and return of function "demovingShift"
-1) an array of strings: s (possibly resulting from "movingShift", with 5 strings)
-2) an int shift
-
-"demovingShift" returns a string.
-
-Example:
-u = "I should have known that you would have a perfect answer for me!!!"
-movingShift(u, 1) returns:
-v = ["J vltasl rlhr ", "zdfog odxr ypw", " atasl rlhr p ", "gwkzzyq zntyhv", " lvz wp!!!"]
-demovingShift(v, 1) returns u.
-*/
+ * The action of a Caesar cipher is to replace each plaintext letter with a different one
+ * a fixed number of places up or down the alphabet.
+ * 
+ * This program performs a variation where the shift increases by 1 for each character.
+ * If the shift is initially 1, the first character is shifted by 1, the second by 2, etc.
+ * 
+ * movingShift(s, shift):
+ *   - Encodes a string with increasing shift
+ *   - Returns an array of 5 strings (the encoded message split into parts)
+ *   - Parts have non-increasing lengths; parts 1-4 are between floor(length/5) and ceil(length/5)
+ *   - params: s (string to encode), shift (initial shift value)
+ * 
+ * demovingShift(arr, shift):
+ *   - Decodes an array of strings back to the original
+ *   - params: arr (array of 5 strings from movingShift), shift (initial shift value)
+ *   - returns: the decoded string
+ */
 
 function movingShift(s, shift) {
-
+  
 }
 
-function demovingShift(s, shift) {
-
+function demovingShift(arr, shift) {
+  
 }
--- first-variation-on-caesar-cipher/solution.js
+++ first-variation-on-caesar-cipher/solution.js
@@ -1,30 +1,67 @@
-function shiftChar(ch, shift) {
-  const code = ch.charCodeAt(0);
-  if (code >= 65 && code <= 90) {
-    return String.fromCharCode(((code - 65 + shift) % 26 + 26) % 26 + 65);
-  }
-  if (code >= 97 && code <= 122) {
-    return String.fromCharCode(((code - 97 + shift) % 26 + 26) % 26 + 97);
-  }
-  return ch;
-}
-
 function movingShift(s, shift) {
-  const encoded = s.split('').map((ch, i) => shiftChar(ch, shift + i)).join('');
-  const len = encoded.length;
-  const base = Math.floor(len / 5);
-  const extra = len % 5;
-  const parts = [];
-  let idx = 0;
-  for (let i = 0; i < 5; i++) {
-    const size = base + (i < extra ? 1 : 0);
-    parts.push(encoded.slice(idx, idx + size));
-    idx += size;
+  let encoded = "";
+  let currentShift = shift;
+  
+  for (let char of s) {
+    if (/[a-z]/.test(char)) {
+      let charCode = char.charCodeAt(0) - 'a'.charCodeAt(0);
+      charCode = (charCode + currentShift) % 26;
+      encoded += String.fromCharCode(charCode + 'a'.charCodeAt(0));
+    } else if (/[A-Z]/.test(char)) {
+      let charCode = char.charCodeAt(0) - 'A'.charCodeAt(0);
+      charCode = (charCode + currentShift) % 26;
+      encoded += String.fromCharCode(charCode + 'A'.charCodeAt(0));
+    } else {
+      encoded += char;
+    }
+    currentShift++;
   }
+  
+  let length = encoded.length;
+  let baseHigh = Math.ceil(length / 5);
+  let baseLow = Math.floor(length / 5);
+  
+  let part1 = baseHigh;
+  let part2 = baseHigh;
+  let part3 = baseHigh;
+  let remaining = length - part1 - part2 - part3;
+  
+  let part4 = Math.max(baseLow, Math.min(baseHigh, remaining));
+  remaining -= part4;
+  
+  let part5 = remaining;
+  
+  let parts = [];
+  let index = 0;
+  for (let size of [part1, part2, part3, part4, part5]) {
+    parts.push(encoded.substring(index, index + size));
+    index += size;
+  }
+  
   return parts;
 }
 
-function demovingShift(s, shift) {
-  const joined = s.join('');
-  return joined.split('').map((ch, i) => shiftChar(ch, -(shift + i))).join('');
+function demovingShift(arr, shift) {
+  let encoded = arr.join("");
+  let decoded = "";
+  let currentShift = shift;
+  
+  for (let char of encoded) {
+    if (/[a-z]/.test(char)) {
+      let charCode = char.charCodeAt(0) - 'a'.charCodeAt(0);
+      let shiftMod = currentShift % 26;
+      charCode = ((charCode - shiftMod) % 26 + 26) % 26;
+      decoded += String.fromCharCode(charCode + 'a'.charCodeAt(0));
+    } else if (/[A-Z]/.test(char)) {
+      let charCode = char.charCodeAt(0) - 'A'.charCodeAt(0);
+      let shiftMod = currentShift % 26;
+      charCode = ((charCode - shiftMod) % 26 + 26) % 26;
+      decoded += String.fromCharCode(charCode + 'A'.charCodeAt(0));
+    } else {
+      decoded += char;
+    }
+    currentShift++;
+  }
+  
+  return decoded;
 }
