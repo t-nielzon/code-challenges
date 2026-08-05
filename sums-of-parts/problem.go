463def3a9989b59a9d398201fca83b5effb6fd57
t-nielzon <t-nielzon@users.noreply.github.com>
2026-08-06 07:29:16 +0800

    Add go solution: sums-of-parts

--- sums-of-parts/problem.go
+++ sums-of-parts/problem.go
@@ -1,38 +1,17 @@
 // Sums of Parts
+// 
+// Given an array, return a list of the sums of all its parts.
+// For each index in the array, compute the sum from that index to the end.
+// Also include the sum of an empty array (0) at the end of the result.
 //
-// Let us consider this example (array written in general format):
+// Example:
+// [0, 1, 3, 6, 10] -> [20, 20, 19, 16, 10, 0]
+// [1, 2, 3, 4, 5, 6] -> [21, 20, 18, 15, 11, 6, 0]
 //
-// ls = [0, 1, 3, 6, 10]
-//
-// Its following parts:
-//
-// ls = [0, 1, 3, 6, 10]
-// ls = [1, 3, 6, 10]
-// ls = [3, 6, 10]
-// ls = [6, 10]
-// ls = [10]
-// ls = []
-//
-// The corresponding sums are (put together in a list):
-// [20, 20, 19, 16, 10, 0]
-//
-// The function PartsSums will take as parameter a list ls and return a
-// list of the sums of its parts as defined above.
-//
-// Other Examples:
-//
-// ls = [1, 2, 3, 4, 5, 6]
-// PartsSums(ls) -> [21, 20, 18, 15, 11, 6, 0]
-//
-// ls = [744125, 935, 407, 454, 430, 90, 144, 6710213, 889, 810, 2579358]
-// PartsSums(ls) -> [10037855, 9293730, 9292795, 9292388, 9291934, 9291504,
-//                   9291414, 9291270, 2581057, 2580168, 2579358, 0]
-//
-// Notes:
-// - Take a look at performance: some lists have thousands of elements.
+// Performance note: optimize for arrays with thousands of elements.
 
 package kata
 
 func PartsSums(ls []int) []int {
-	return nil
+	
 }
--- sums-of-parts/solution.go
+++ sums-of-parts/solution.go
@@ -1,14 +1,12 @@
 package kata
 
 func PartsSums(ls []int) []int {
-	n := len(ls)
-	result := make([]int, n+1)
+	result := make([]int, len(ls)+1)
 	sum := 0
-	// build from the end so each entry is the running suffix sum
-	for i := n - 1; i >= 0; i-- {
+	for i := len(ls) - 1; i >= 0; i-- {
 		sum += ls[i]
 		result[i] = sum
 	}
-	result[n] = 0
+	result[len(ls)] = 0
 	return result
 }
