b1e77067c6d0e4e2846a6ad7c10849f442caaa57
t-nielzon <t-nielzon@users.noreply.github.com>
2026-08-06 10:12:59 +0800

    Add go solution: bankers-plan

--- bankers-plan/problem.go
+++ bankers-plan/problem.go
@@ -1,21 +1,23 @@
+package main
+
 /*
 John has some amount of money of which he wants to deposit a part f0 to the bank at the beginning
 of year 1. He wants to withdraw each year for his living an amount c0.
 
 Here is his banker plan:
-
 - deposit f0 at beginning of year 1
 - his bank account has an interest rate of p percent per year, constant over the years
-- John can withdraw each year c0, taking it whenever he wants in the year; he must take account of
-  an inflation of i percent per year in order to keep his quality of living. i is supposed to stay
-  constant over the years.
-- all amounts f0..fn-1, c0..cn-1 are truncated by the bank to their integral part
+- John can withdraw each year c0, taking it whenever he wants in the year; he must take account of an inflation of i percent per year in order to keep his quality of living. i is supposed to stay constant over the years.
+- all amounts f0..f(n-1), c0..c(n-1) are truncated by the bank to their integral part
 - Given f0, p, c0, i the banker guarantees that John will be able to go on that way until the nth year.
 
-Given parameters f0, p, c0, n, i build a function fortune which returns true if John can make a
-living until the nth year and false if it is not possible.
-*/
-package kata
+John wants to know if the banker's plan is right or wrong.
+Given parameters f0, p, c0, n, i build a function fortune which returns true if John can make a living until the nth year
+and false if it is not possible.
 
-func Fortune(f0 int, p float64, c0 float64, n int, i float64) bool {
+Note: Don't forget to convert the percent parameters as percentages in the body of your function: if a parameter percent is 2 you have to convert it to 0.02.
+*/
+
+func fortune(f0 float64, p float64, c0 float64, n int, i float64) bool {
+	return false
 }
--- bankers-plan/solution.go
+++ bankers-plan/solution.go
@@ -1,19 +1,21 @@
-package kata
+package main
 
-import "math"
-
-func Fortune(f0 int, p float64, c0 float64, n int, i float64) bool {
-	f := float64(f0)
+func fortune(f0 float64, p float64, c0 float64, n int, i float64) bool {
+	f := f0
 	c := c0
-	pRate := p / 100.0
-	iRate := i / 100.0
-
+	
+	// Convert percentages to decimal
+	p /= 100
+	i /= 100
+	
 	for year := 2; year <= n; year++ {
-		f = math.Floor(f + f*pRate - c)
-		c = math.Floor(c + c*iRate)
+		f = float64(int64(f*(1+p) - c))
+		c = float64(int64(c * (1 + i)))
+		
 		if f < 0 {
 			return false
 		}
 	}
+	
 	return true
 }
