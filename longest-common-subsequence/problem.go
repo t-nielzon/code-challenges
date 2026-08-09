81a152c534a8cf4d6c6e55889c453f2787eaaa66
t-nielzon <t-nielzon@users.noreply.github.com>
2026-08-09 21:03:43 +0800

    Add go solution: longest-common-subsequence

--- longest-common-subsequence/problem.go
+++ longest-common-subsequence/problem.go
@@ -1,28 +1,28 @@
 /*
-Longest Common Subsequence
+Write a function called LCS that accepts two sequences and returns
+the longest subsequence common to the passed in sequences.
 
-Write a function called `LCS` that accepts two sequences and returns the longest
-subsequence common to the passed in sequences.
-
-Subsequence:
-A subsequence is different from a substring. The terms of a subsequence need not
-be consecutive terms of the original sequence.
+Subsequence: A subsequence is different from a substring.
+The terms of a subsequence need not be consecutive terms of the original sequence.
 
 Example subsequence:
 Subsequences of "abc" = "a", "b", "c", "ab", "ac", "bc" and "abc".
 
 LCS examples:
-	LCS( "abcdef", "abc" )           => returns "abc"
-	LCS( "abcdef", "acf" )           => returns "acf"
-	LCS( "132535365", "123456789" )  => returns "12356"
+LCS( "abcdef", "abc" ) => returns "abc"
+LCS( "abcdef", "acf" ) => returns "acf"
+LCS( "132535365", "123456789" ) => returns "12356"
 
 Notes:
-  - Both arguments will be strings
-  - Return value must be a string
-  - Return an empty string if there exists no common subsequence
-  - All tests will only have a single longest common subsequence
+- Both arguments will be strings
+- Return value must be a string
+- Return an empty string if there exists no common subsequence
+- Both arguments will have one or more characters
+- All tests will only have a single longest common subsequence
 */
-package kata
+package main
 
 func LCS(s1, s2 string) string {
+	// TODO: implement
+	return ""
 }
--- longest-common-subsequence/solution.go
+++ longest-common-subsequence/solution.go
@@ -1,37 +1,47 @@
-package kata
+package main
 
 func LCS(s1, s2 string) string {
-	a, b := []byte(s1), []byte(s2)
-	n, m := len(a), len(b)
+	n, m := len(s1), len(s2)
+
+	// dp[i][j] = length of lcs of s1[0:i] and s2[0:j]
 	dp := make([][]int, n+1)
 	for i := range dp {
 		dp[i] = make([]int, m+1)
 	}
+
+	// fill dp table
 	for i := 1; i <= n; i++ {
 		for j := 1; j <= m; j++ {
-			if a[i-1] == b[j-1] {
+			if s1[i-1] == s2[j-1] {
 				dp[i][j] = dp[i-1][j-1] + 1
-			} else if dp[i-1][j] >= dp[i][j-1] {
-				dp[i][j] = dp[i-1][j]
 			} else {
-				dp[i][j] = dp[i][j-1]
+				dp[i][j] = max(dp[i-1][j], dp[i][j-1])
 			}
 		}
 	}
-	res := make([]byte, 0, dp[n][m])
-	for i, j := n, m; i > 0 && j > 0; {
-		if a[i-1] == b[j-1] {
-			res = append(res, a[i-1])
+
+	// backtrack to build result
+	i, j := n, m
+	var result []byte
+
+	for i > 0 && j > 0 {
+		if s1[i-1] == s2[j-1] {
+			result = append([]byte{s1[i-1]}, result...)
 			i--
 			j--
-		} else if dp[i-1][j] >= dp[i][j-1] {
+		} else if dp[i-1][j] > dp[i][j-1] {
 			i--
 		} else {
 			j--
 		}
 	}
-	for l, r := 0, len(res)-1; l < r; l, r = l+1, r-1 {
-		res[l], res[r] = res[r], res[l]
+
+	return string(result)
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
 	}
-	return string(res)
+	return b
 }
