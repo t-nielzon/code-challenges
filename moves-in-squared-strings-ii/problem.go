2a93242deb2a3d59b3a4d5c9788d52e8593d6494
t-nielzon <t-nielzon@users.noreply.github.com>
2026-08-09 09:57:58 +0800

    Add go solution: moves-in-squared-strings-ii

--- moves-in-squared-strings-ii/problem.go
+++ moves-in-squared-strings-ii/problem.go
@@ -1,25 +1,30 @@
+/*
+You are given a string of n lines, each substring being n characters long.
+
+We will study some transformations of this square of strings.
+
+rot(s):
+Clock rotation 180 degrees.
+Example: rot("abcd\nefgh\nijkl\nmnop") => "ponm\nlkji\nhgfe\ndcba"
+
+selfie_and_rot(s):
+It is an initial string combined with its 180-degree clock-rotated version,
+interspersed with dots proportional to the length of the segments.
+Example: selfie_and_rot("abcd\nefgh\nijkl\nmnop") => 
+"abcd....\nefgh....\nijkl....\nmnop....\n....ponm\n....lkji\n....hgfe\n....dcba"
+
+oper(fct, s):
+High-order function where fct is the function to apply to the string s.
+fct will be one of rot or selfie_and_rot
+*/
+
 package main
 
-// Moves in squared strings (II)
-//
-// You are given a string of n lines, each substring being n characters long.
-// Example: s = "abcd\nefgh\nijkl\nmnop"
-//
-// Rot(s): Clock rotation 180 degrees.
-// Example: Rot("abcd\nefgh\nijkl\nmnop") => "ponm\nlkji\nhgfe\ndcba"
-//
-// SelfieAndRot(s): Initial string combined with its 180-degree rotated version,
-// interspersed with dots proportional to the length of the line segments.
-// Example: SelfieAndRot("abcd\nefgh\nijkl\nmnop") => "abcd....\nefgh....\nijkl....\nmnop....\n....ponm\n....lkji\n....hgfe\n....dcba"
-//
-// Oper(fct, s): Higher-order function where fct is a function to apply to string s.
-// fct will be one of Rot or SelfieAndRot.
-
-func Rot(s string) string {
+func rot(s string) string {
 }
 
-func SelfieAndRot(s string) string {
+func selfie_and_rot(s string) string {
 }
 
-func Oper(fct func(string) string, s string) string {
+func oper(fct func(string) string, s string) string {
 }
--- moves-in-squared-strings-ii/solution.go
+++ moves-in-squared-strings-ii/solution.go
@@ -1,42 +1,52 @@
 package main
 
-import (
-	"strings"
-)
+import "strings"
 
-func Rot(s string) string {
+func rot(s string) string {
 	lines := strings.Split(s, "\n")
-	for i, j := 0, len(lines)-1; i < j; i, j = i+1, j-1 {
+	
+	// reverse order of lines and reverse each line for 180 degree rotation
+	for i := 0; i < len(lines)/2; i++ {
+		j := len(lines) - 1 - i
 		lines[i], lines[j] = lines[j], lines[i]
 	}
+	
 	for i := range lines {
 		runes := []rune(lines[i])
-		for j, k := 0, len(runes)-1; j < k; j, k = j+1, k-1 {
+		for j := 0; j < len(runes)/2; j++ {
+			k := len(runes) - 1 - j
 			runes[j], runes[k] = runes[k], runes[j]
 		}
 		lines[i] = string(runes)
 	}
+	
 	return strings.Join(lines, "\n")
 }
 
-func SelfieAndRot(s string) string {
+func selfie_and_rot(s string) string {
+	if s == "" {
+		return ""
+	}
+	
 	lines := strings.Split(s, "\n")
-	rotated := Rot(s)
-	rotatedLines := strings.Split(rotated, "\n")
+	n := len(lines[0])
 	
-	dotCount := len(lines[0])
-	dots := strings.Repeat(".", dotCount)
+	rotated := rot(s)
+	rotLines := strings.Split(rotated, "\n")
 	
-	result := []string{}
+	var result []string
+	
 	for _, line := range lines {
-		result = append(result, line+dots)
+		result = append(result, line+strings.Repeat(".", n))
 	}
-	for _, line := range rotatedLines {
-		result = append(result, dots+line)
+	
+	for _, line := range rotLines {
+		result = append(result, strings.Repeat(".", n)+line)
 	}
+	
 	return strings.Join(result, "\n")
 }
 
-func Oper(fct func(string) string, s string) string {
+func oper(fct func(string) string, s string) string {
 	return fct(s)
 }
