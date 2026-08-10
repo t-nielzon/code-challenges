b598cd3d26c8dc45ef0d8f6b931db2c5be0b4568
t-nielzon <t-nielzon@users.noreply.github.com>
2026-08-10 10:07:46 +0800

    Add javascript solution: are-all-elements-equal-infinity-version

--- are-all-elements-equal-infinity-version/solution.js
+++ are-all-elements-equal-infinity-version/solution.js
@@ -1,13 +1,15 @@
 function eqAll(list) {
-  let first;
-  let hasFirst = false;
-  for (const item of list) {
-    if (!hasFirst) {
-      first = item;
-      hasFirst = true;
-    } else if (item !== first) {
+  let first = null;
+  let isFirst = true;
+  
+  for (const element of list) {
+    if (isFirst) {
+      first = element;
+      isFirst = false;
+    } else if (element !== first) {
       return false;
     }
   }
+  
   return true;
 }
