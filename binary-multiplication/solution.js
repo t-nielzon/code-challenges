872d646d8304a335cc0adf94691deee3be92cc0c
t-nielzon <t-nielzon@users.noreply.github.com>
2026-08-11 07:22:22 +0800

    Add javascript solution: binary-multiplication

--- binary-multiplication/solution.js
+++ binary-multiplication/solution.js
@@ -1,16 +1,17 @@
-function egyptianMultiplication(a, b) {
-  let m = Math.max(a, b);
-  let n = Math.min(a, b);
-  
-  const result = [];
-  
-  while (m > 0) {
-    if (m % 2 === 1) {
-      result.push(n);
-    }
-    m = Math.floor(m / 2);
-    n *= 2;
+function egyptianMultiplication(m, n) {
+  if (m < n) {
+    [m, n] = [n, m];
   }
   
-  return result.reverse();
+  const list = [];
+  
+  while (m > 0) {
+    if (m % 2 !== 0) {
+      list.push(n);
+    }
+    m = Math.floor(m / 2);
+    n = n * 2;
+  }
+  
+  return list.reverse();
 }
