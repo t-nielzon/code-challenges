6722b33c083fa7b0df10350ab708a50fdbead7c8
t-nielzon <t-nielzon@users.noreply.github.com>
2026-08-13 09:10:07 +0800

    Add go solution: playing-on-a-chessboard

--- playing-on-a-chessboard/solution.go
+++ playing-on-a-chessboard/solution.go
@@ -1,8 +1,36 @@
-package kata
+package main
 
-func Game(n int) []int {
-	if n%2 == 0 {
-		return []int{n * n / 2}
+func game(n int) []interface{} {
+	if n == 0 {
+		return []interface{}{0}
 	}
-	return []int{n * n, 2}
+
+	numerator := int64(0)
+	denominator := int64(1)
+
+	for row := 1; row <= n; row++ {
+		for col := 1; col <= n; col++ {
+			fracNum := int64(col)
+			fracDen := int64(col + row)
+
+			numerator = numerator*fracDen + fracNum*denominator
+			denominator = denominator * fracDen
+
+			g := gcd(numerator, denominator)
+			numerator /= g
+			denominator /= g
+		}
+	}
+
+	if denominator == 1 {
+		return []interface{}{numerator}
+	}
+	return []interface{}{numerator, denominator}
+}
+
+func gcd(a, b int64) int64 {
+	for b != 0 {
+		a, b = b, a%b
+	}
+	return a
 }
