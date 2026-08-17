e729a52b0cca0f0d5f1c3279a1792f588a05c016
t-nielzon <t-nielzon@users.noreply.github.com>
2026-08-17 09:08:43 +0800

    Add javascript solution: the-deaf-rats-of-hamelin

--- the-deaf-rats-of-hamelin/problem.js
+++ the-deaf-rats-of-hamelin/problem.js
@@ -1,24 +1,24 @@
 /*
-The Pied Pied has been enlisted to play his magical tune and coax all the rats out of town.
+The Pied Piper has been enlisted to play his magical tune and coax all the rats out of town.
 But some of the rats are deaf and are going the wrong way!
 
-How many deaf rats are there?
+Task: How many deaf rats are there?
 
 Legend:
-- P = The Pied Piper
-- O~ = Rat going left
-- ~O = Rat going right
+* P = The Pied Piper
+* O~ = Rat going left
+* ~O = Rat going right
 
-Deaf rats are those moving away from the Pied Piper:
-- Rats left of P should go right (~O); those going left (O~) are deaf
-- Rats right of P should go left (O~); those going right (~O) are deaf
+Example:
+* ex1 ~O~O~O~O P has 0 deaf rats
+* ex2 P O~ O~ ~O O~ has 1 deaf rat
+* ex3 ~O~O~O~OP~O~OO~ has 2 deaf rats
 
-Examples:
-- ~O~O~O~O P has 0 deaf rats
-- P O~ O~ ~O O~ has 1 deaf rat
-- ~O~O~O~OP~O~OO~ has 2 deaf rats
+Rats are deaf if they go away from the Pied Piper:
+- Rats to the left of P going left (O~) are deaf
+- Rats to the right of P going right (~O) are deaf
 */
 
-function countDeafRats(string) {
+function deafRats(string) {
   
 }
--- the-deaf-rats-of-hamelin/solution.js
+++ the-deaf-rats-of-hamelin/solution.js
@@ -1,26 +1,9 @@
-function countDeafRats(string) {
-  const cleaned = string.replace(/\s/g, '');
-  const [left, right] = cleaned.split('P');
+function deafRats(string) {
+  const clean = string.replace(/\s/g, '');
+  const [left, right] = clean.split('P');
   
-  let deafCount = 0;
+  const deafLeft = (left.match(/O~/g) || []).length;
+  const deafRight = (right.match(/~O/g) || []).length;
   
-  // Left of P: deaf rats going left (O~)
-  if (left) {
-    for (let i = 0; i < left.length; i += 2) {
-      if (left.substr(i, 2) === 'O~') {
-        deafCount++;
-      }
-    }
-  }
-  
-  // Right of P: deaf rats going right (~O)
-  if (right) {
-    for (let i = 0; i < right.length; i += 2) {
-      if (right.substr(i, 2) === '~O') {
-        deafCount++;
-      }
-    }
-  }
-  
-  return deafCount;
+  return deafLeft + deafRight;
 }
