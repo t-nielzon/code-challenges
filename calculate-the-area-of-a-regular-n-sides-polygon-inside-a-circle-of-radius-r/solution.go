16a333a679c065d37c72cabaa258ee48b55997ab
t-nielzon <t-nielzon@users.noreply.github.com>
2026-08-20 14:43:25 +0800

    Add go solution: calculate-the-area-of-a-regular-n-sides-polygon-inside-a-circle-of-radius-r

--- calculate-the-area-of-a-regular-n-sides-polygon-inside-a-circle-of-radius-r/solution.go
+++ calculate-the-area-of-a-regular-n-sides-polygon-inside-a-circle-of-radius-r/solution.go
@@ -1,9 +1,10 @@
-package kata
+package main
 
 import "math"
 
 func AreaOfPolygonInsideCircle(circleRadius float64, numberOfSides int) float64 {
-	n := float64(numberOfSides)
-	area := 0.5 * n * circleRadius * circleRadius * math.Sin(2*math.Pi/n)
+	// area of regular polygon = (n * r^2 * sin(2*pi/n)) / 2
+	area := float64(numberOfSides) * circleRadius * circleRadius * math.Sin(2*math.Pi/float64(numberOfSides)) / 2
+	// round to 3 decimal places
 	return math.Round(area*1000) / 1000
 }
