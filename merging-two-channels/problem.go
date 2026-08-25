f07f7d8c5523c5cad566d01a3b9e9f61330533d6
t-nielzon <t-nielzon@users.noreply.github.com>
2026-08-25 16:07:23 +0800

    Add go solution: merging-two-channels

--- merging-two-channels/problem.go
+++ merging-two-channels/problem.go
@@ -1,16 +1,16 @@
-package main
-
 /*
 # Merging Two Channels
 
-Write a function `func Merge(a <-chan string, b <-chan string) <-chan string`,
-which takes two read-only channels and returns a new channel. All messages from
-channel `a` and `b` must be forwarded to the new channel. Once `a` and `b` are
-both closed, also the returned channel must be closed.
+## Introduction
+Go has a powerful built-in data type: **channel**. In some cases you want process messages from different data sources (e.g. UDP packages, TCP messages, HTTP requests) and you want to consume all in the same way. Then it might make sense to merge multiple channels into a single one. This is what we do here.
 
-The order of the forwarded messages doesn't matter, but you should consume from
-both incoming channels concurrently.
+## Task
+Write a function `func Merge(a <-chan string, b <-chan string) <-chan string`, which takes two read-only channels and returns a new channel. All messages from channel `a` and `b` must be forwarded to the new channel. Once `a` and `b` are both closed, also the returned channel must be closed.
+
+The order of the forwarded messages doesn't matter, but you should consume from both incoming channels concurrently.
 */
+
+package main
 
 func Merge(a <-chan string, b <-chan string) <-chan string {
 	// TODO: implement
--- merging-two-channels/solution.go
+++ merging-two-channels/solution.go
@@ -5,27 +5,27 @@ import "sync"
 func Merge(a <-chan string, b <-chan string) <-chan string {
 	out := make(chan string)
 	var wg sync.WaitGroup
-	
+
 	wg.Add(2)
-	
+
 	go func() {
-		defer wg.Done()
 		for msg := range a {
 			out <- msg
 		}
+		wg.Done()
 	}()
-	
+
 	go func() {
-		defer wg.Done()
 		for msg := range b {
 			out <- msg
 		}
+		wg.Done()
 	}()
-	
+
 	go func() {
 		wg.Wait()
 		close(out)
 	}()
-	
+
 	return out
 }
