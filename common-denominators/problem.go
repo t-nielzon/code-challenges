a125719b93fb9beb7ca9f7a4f26fc2e487fa2448
t-nielzon <t-nielzon@users.noreply.github.com>
2026-08-31 09:41:20 +0800

    Add go solution: common-denominators

--- common-denominators/problem.go
+++ common-denominators/problem.go
@@ -1,39 +1,21 @@
 package main
 
 /*
-Common Denominators
+Common denominators
 
 You will have a list of rationals in the form:
-{ {numer_1, denom_1}, ..., {numer_n, denom_n} }
-or
-[ [numer_1, denom_1], ..., [numer_n, denom_n] ]
-or
-[ (numer_1, denom_1), ..., (numer_n, denom_n) ]
+{numer_1, denom_1}, ..., {numer_n, denom_n}
 
-where all numbers are positive ints.
+All numbers are positive integers.
 
-You have to produce a result in the form:
-(N_1, D) ... (N_n, D)
-or
-[ [N_1, D] ... [N_n, D] ]
-or
-[ (N_1', D), ..., (N_n, D) ]
-or
-{{N_1, D} ... {N_n, D}}
-or
-"(N_1, D) ... (N_n, D)"
-
-depending on the language, in which D is as small as possible and
-
+You have to produce a result where D is as small as possible and:
 N_1/D == numer_1/denom_1 ... N_n/D == numer_n/denom_n.
 
 Example:
-convertFracs [(1, 2), (1, 3), (1, 4)] shouldBe [(6, 12), (4, 12), (3, 12)]
+ConvertFracs([][]int{{1, 2}, {1, 3}, {1, 4}}) should return [][]int{{6, 12}, {4, 12}, {3, 12}}
 
-Note:
-Newer translations have some reducible fractions. To be on the safe side it is better to do
-a bit more work by simplifying fractions even if they don't have to be.
+Note: Newer translations have reducible fractions. Simplify fractions to ensure D is minimal.
 */
 
-func ConvertFracs(fracs [][]int64) [][]int64 {
+func ConvertFracs(fracs [][]int) [][]int {
 }
--- common-denominators/solution.go
+++ common-denominators/solution.go
@@ -1,40 +1,40 @@
 package main
 
-func gcd(a, b int64) int64 {
+func gcd(a, b int) int {
 	for b != 0 {
 		a, b = b, a%b
 	}
 	return a
 }
 
-func lcm(a, b int64) int64 {
-	return a / gcd(a, b) * b
+func lcm(a, b int) int {
+	return a * b / gcd(a, b)
 }
 
-func ConvertFracs(fracs [][]int64) [][]int64 {
+func ConvertFracs(fracs [][]int) [][]int {
 	if len(fracs) == 0 {
-		return [][]int64{}
+		return [][]int{}
 	}
-	
-	// Reduce input fractions first
-	reduced := make([][]int64, len(fracs))
+
+	// Simplify all input fractions
+	simplified := make([][]int, len(fracs))
 	for i, frac := range fracs {
 		g := gcd(frac[0], frac[1])
-		reduced[i] = []int64{frac[0] / g, frac[1] / g}
+		simplified[i] = []int{frac[0] / g, frac[1] / g}
 	}
-	
-	// Find LCM of all denominators
-	var commonDenom int64 = reduced[0][1]
-	for i := 1; i < len(reduced); i++ {
-		commonDenom = lcm(commonDenom, reduced[i][1])
+
+	// Find LCM of all simplified denominators
+	commonDenom := simplified[0][1]
+	for i := 1; i < len(simplified); i++ {
+		commonDenom = lcm(commonDenom, simplified[i][1])
 	}
-	
+
 	// Convert all fractions to have the common denominator
-	result := make([][]int64, len(reduced))
-	for i, frac := range reduced {
-		num := frac[0] * (commonDenom / frac[1])
-		result[i] = []int64{num, commonDenom}
+	result := make([][]int, len(simplified))
+	for i, frac := range simplified {
+		newNumer := frac[0] * (commonDenom / frac[1])
+		result[i] = []int{newNumer, commonDenom}
 	}
-	
+
 	return result
 }
