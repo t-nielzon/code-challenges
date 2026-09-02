1e9d4601e5bd0275f1f59028417d6e66d3923b8a
t-nielzon <t-nielzon@users.noreply.github.com>
2026-09-02 08:12:13 +0800

    Add javascript solution: are-they-the-same

--- are-they-the-same/problem.js
+++ are-they-the-same/problem.js
@@ -1,8 +1,9 @@
-/**
- * Given two arrays `a` and `b` write a function `comp(a, b)` that checks whether 
- * the two arrays have the "same" elements, with the same multiplicities 
- * (the multiplicity of a member is the number of times it appears).
+/*
+ * Are they the "same"?
  * 
+ * Given two arrays `a` and `b` write a function `comp(a, b)` that checks 
+ * whether the two arrays have the "same" elements, with the same multiplicities 
+ * (the multiplicity of a member is the number of times it appears). 
  * "Same" means that the elements in `b` are the elements in `a` squared, 
  * regardless of the order.
  * 
@@ -11,15 +12,10 @@
  * b = [121, 14641, 20736, 361, 25921, 361, 20736, 361]
  * comp(a, b) returns true
  * 
- * a = [121, 144, 19, 161, 19, 144, 19, 11]
- * b = [132, 14641, 20736, 361, 25921, 361, 20736, 361]
- * comp(a, b) returns false
- * 
- * Remarks:
- * - a or b might be [] or {}
- * - a or b might be nil or null
- * - If a or b are nil/null, return false
+ * If a or b are null/undefined, return false.
+ * If a or b are not arrays, return false.
  */
 
-function comp(a, b) {
+function comp(a, b){
+  
 }
--- are-they-the-same/solution.js
+++ are-they-the-same/solution.js
@@ -1,10 +1,9 @@
-function comp(a, b) {
-  if (a == null || b == null) return false;
+function comp(a, b){
   if (!Array.isArray(a) || !Array.isArray(b)) return false;
   if (a.length !== b.length) return false;
   
   const squaredA = a.map(x => x * x).sort((x, y) => x - y);
-  const sortedB = [...b].sort((x, y) => x - y);
+  const sortedB = b.sort((x, y) => x - y);
   
   return squaredA.every((val, i) => val === sortedB[i]);
 }
