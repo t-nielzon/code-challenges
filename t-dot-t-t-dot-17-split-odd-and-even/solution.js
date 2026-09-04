5ed1630db5547d697494afcf1bc470a0343866d2
t-nielzon <t-nielzon@users.noreply.github.com>
2026-09-04 08:32:37 +0800

    Add javascript solution: t-dot-t-t-dot-17-split-odd-and-even

--- t-dot-t-t-dot-17-split-odd-and-even/solution.js
+++ t-dot-t-t-dot-17-split-odd-and-even/solution.js
@@ -1,18 +1,20 @@
-function splitOddAndEven(n) {
-  const digits = String(n).split('');
-  const groups = [];
-  let current = '';
-  let currentParity = null;
-  for (const d of digits) {
-    const parity = Number(d) % 2;
-    if (parity === currentParity) {
-      current += d;
+function splitOddEven(n) {
+  const str = String(n);
+  const result = [];
+  let current = str[0];
+  
+  for (let i = 1; i < str.length; i++) {
+    const currentDigit = parseInt(str[i]);
+    const lastDigit = parseInt(str[i - 1]);
+    
+    if ((currentDigit % 2) !== (lastDigit % 2)) {
+      result.push(parseInt(current));
+      current = str[i];
     } else {
-      if (current !== '') groups.push(current);
-      current = d;
-      currentParity = parity;
+      current += str[i];
     }
   }
-  if (current !== '') groups.push(current);
-  return groups.map(Number);
+  
+  result.push(parseInt(current));
+  return result;
 }
