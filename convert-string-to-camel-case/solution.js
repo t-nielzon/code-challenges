d6f336341218043b855dba27b1c447ea8ae87164
t-nielzon <t-nielzon@users.noreply.github.com>
2026-09-06 13:05:44 +0800

    Add javascript solution: convert-string-to-camel-case

---
+++ convert-string-to-camel-case/solution.js
@@ -0,0 +1,9 @@
+function toCamelCase(str) {
+  return str
+    .split(/[-_]/)
+    .map((word, index) => {
+      if (index === 0) return word;
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    })
+    .join('');
+}
