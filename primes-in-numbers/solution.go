a658ee457716b3c27bd54dad89322074311aecfa
t-nielzon <t-nielzon@users.noreply.github.com>
2026-09-07 10:29:40 +0800

    Add go solution: primes-in-numbers

--- primes-in-numbers/solution.go
+++ primes-in-numbers/solution.go
@@ -1,23 +1,44 @@
-package kata
+package main
 
 import "fmt"
 
 func PrimeFactors(n int) string {
 	result := ""
-	for p := 2; p*p <= n; p++ {
-		count := 0
-		for n%p == 0 {
-			n /= p
-			count++
-		}
+
+	addFactor := func(prime, count int) {
 		if count == 1 {
-			result += fmt.Sprintf("(%d)", p)
-		} else if count > 1 {
-			result += fmt.Sprintf("(%d**%d)", p, count)
+			result += fmt.Sprintf("(%d)", prime)
+		} else {
+			result += fmt.Sprintf("(%d**%d)", prime, count)
 		}
 	}
-	if n > 1 {
-		result += fmt.Sprintf("(%d)", n)
+
+	// check for factor 2
+	count := 0
+	for n%2 == 0 {
+		count++
+		n = n / 2
 	}
+	if count > 0 {
+		addFactor(2, count)
+	}
+
+	// check for odd factors from 3 onwards
+	for i := 3; i*i <= n; i += 2 {
+		count = 0
+		for n%i == 0 {
+			count++
+			n = n / i
+		}
+		if count > 0 {
+			addFactor(i, count)
+		}
+	}
+
+	// if n is still greater than 1, then it's a prime factor
+	if n > 1 {
+		addFactor(n, 1)
+	}
+
 	return result
 }
