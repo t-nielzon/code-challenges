721c4806171b52c23f2727284c2c51f380a6e30a
t-nielzon <t-nielzon@users.noreply.github.com>
2026-09-10 09:06:35 +0800

    Add go solution: steps-in-primes

--- steps-in-primes/solution.go
+++ steps-in-primes/solution.go
@@ -1,22 +1,28 @@
-package kata
+package main
 
 func Step(g, m, n int) []int {
-	isPrime := func(num int) bool {
-		if num < 2 {
-			return false
-		}
-		for i := 2; i*i <= num; i++ {
-			if num%i == 0 {
-				return false
-			}
-		}
-		return true
-	}
-
 	for i := m; i <= n-g; i++ {
 		if isPrime(i) && isPrime(i+g) {
 			return []int{i, i + g}
 		}
 	}
 	return nil
+}
+
+func isPrime(num int) bool {
+	if num < 2 {
+		return false
+	}
+	if num == 2 {
+		return true
+	}
+	if num%2 == 0 {
+		return false
+	}
+	for i := 3; i*i <= num; i += 2 {
+		if num%i == 0 {
+			return false
+		}
+	}
+	return true
 }
