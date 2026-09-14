647637c3e2a6dacf96f3a90e751e015c62ef57d5
t-nielzon <t-nielzon@users.noreply.github.com>
2026-09-14 09:48:30 +0800

    Add go solution: catalog

--- catalog/solution.go
+++ catalog/solution.go
@@ -1,23 +1,43 @@
-package kata
+package main
 
 import (
-	"regexp"
+	"encoding/xml"
+	"fmt"
 	"strings"
 )
 
-var prodRe = regexp.MustCompile(`<name>(.*?)</name><prx>(.*?)</prx><qty>(.*?)</qty>`)
+type item struct {
+	Name     string `xml:"name"`
+	Price    string `xml:"prx"`
+	Quantity string `xml:"qty"`
+}
+
+type catalog struct {
+	Items []item `xml:"prod"`
+}
 
 func Catalog(s, article string) string {
-	matches := prodRe.FindAllStringSubmatch(s, -1)
-	var lines []string
-	for _, m := range matches {
-		name, prx, qty := m[1], m[2], m[3]
-		if name == article || strings.HasSuffix(name, " "+article) {
-			lines = append(lines, name+" > prx: $"+prx+" qty: "+qty)
-		}
-	}
-	if len(lines) == 0 {
+	var cat catalog
+	
+	xmlStr := "<catalog>" + s + "</catalog>"
+	
+	err := xml.Unmarshal([]byte(xmlStr), &cat)
+	if err != nil {
 		return "Nothing"
 	}
-	return strings.Join(lines, "\n")
+	
+	var results []string
+	
+	for _, product := range cat.Items {
+		if strings.Contains(product.Name, article) {
+			line := fmt.Sprintf("%s > prx: $%s qty: %s", product.Name, product.Price, product.Quantity)
+			results = append(results, line)
+		}
+	}
+	
+	if len(results) == 0 {
+		return "Nothing"
+	}
+	
+	return strings.Join(results, "\n") + "\n"
 }
