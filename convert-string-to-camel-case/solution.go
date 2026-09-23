5da5ba2e2d2c9c7a78ba299b14f6cb07af2bb961
t-nielzon <t-nielzon@users.noreply.github.com>
2026-09-23 12:10:35 +0800

    Add go solution: convert-string-to-camel-case

--- convert-string-to-camel-case/solution.go
+++ convert-string-to-camel-case/solution.go
@@ -1,13 +1,27 @@
-package kata
+package main
 
-import "strings"
+import (
+	"strings"
+	"unicode"
+)
 
-func ToCamelCase(s string) string {
-	words := strings.FieldsFunc(s, func(r rune) bool {
-		return r == '-' || r == '_'
-	})
-	for i := 1; i < len(words); i++ {
-		words[i] = strings.ToUpper(words[i][:1]) + words[i][1:]
+func ToCamelCase(str string) string {
+	if str == "" {
+		return str
 	}
+
+	// Replace underscores with dashes to have a single delimiter
+	str = strings.ReplaceAll(str, "_", "-")
+
+	// Split by dashes
+	words := strings.Split(str, "-")
+
+	// Capitalize the first letter of all words except the first one
+	for i := 1; i < len(words); i++ {
+		if len(words[i]) > 0 {
+			words[i] = string(unicode.ToUpper(rune(words[i][0]))) + words[i][1:]
+		}
+	}
+
 	return strings.Join(words, "")
 }
