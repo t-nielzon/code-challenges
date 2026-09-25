b634fb20145221fda087ad1df8b09d6203a52f49
t-nielzon <t-nielzon@users.noreply.github.com>
2026-09-25 09:43:51 +0800

    Add go solution: fibo-akin

--- fibo-akin/solution.go
+++ fibo-akin/solution.go
@@ -1,20 +1,25 @@
 package main
 
-func lengthSupUK(n, k int) int {
+func lengthSupUK(n int, k int) int {
 	u := make([]int, n+1)
 	u[1] = 1
 	u[2] = 1
 	
-	for i := 3; i <= n; i++ {
-		u[i] = u[i-u[i-1]] + u[i-u[i-2]]
+	count := 0
+	if u[1] >= k {
+		count++
+	}
+	if u[2] >= k {
+		count++
 	}
 	
-	count := 0
-	for i := 1; i <= n; i++ {
+	for i := 3; i <= n; i++ {
+		u[i] = u[i-u[i-1]] + u[i-u[i-2]]
 		if u[i] >= k {
 			count++
 		}
 	}
+	
 	return count
 }
 
@@ -23,15 +28,14 @@ func comp(n int) int {
 	u[1] = 1
 	u[2] = 1
 	
+	count := 0
+	
 	for i := 3; i <= n; i++ {
 		u[i] = u[i-u[i-1]] + u[i-u[i-2]]
-	}
-	
-	count := 0
-	for i := 2; i <= n; i++ {
 		if u[i] < u[i-1] {
 			count++
 		}
 	}
+	
 	return count
 }
